4c39e45e0717f1f664d03dc126e14a46f4945b87
Mikael Salson <mikael.salson@lifl.fr>
2013-04-16 19:25:02 +0200

    Vidjil: should-to-tap now launches the command by itself. The .should_get file must provide the command to be launched

--- src/tests/should-to-tap.sh
+++ src/tests/should-to-tap.sh
@@ -1,85 +1,112 @@
 #!/bin/bash
 
-# This script also parse a file .should_get
-# In the .should_get format, every line starting with a # is a comment
-# every line starting with a $ is a description of the test
-# non empty lines, must be of the following form:
-# <info>:<regexp>
-# where <regexp> is a regular expression passed to grep 
-# (beware to escape backslashes).
-# info is the number of times this regular expression should occur in the file.
-# the info can be prefixed by a letter either s or f:
-# s: if the test fails, we skip it
-# f: even if the test fails, the script will not exit with an error code.
+if [ $# -eq 0 -o "$1" == "-h" -o "$1" == "--help" ]; then
+    echo "Usage: $0 <file.should_get>
 
-DATA_DIR=$(dirname $0)
+This script takes as input a .should_get file. Don't know what it is? Read this!
+In the .should_get format, every line starting with a # is a comment
+every line starting with a $ is a description of the test
+non empty lines, must be of the following form:
+<info>:<regexp>
+where <regexp> is a regular expression passed to grep 
+(beware to escape backslashes).
+info is the number of times this regular expression should occur in the file.
+the info can be prefixed by a letter either s or f:
+s: if the test fails, we skip it
+f: even if the test fails, the script will not exit with an error code.
 
-FILES=($DATA_DIR/result.sam)    # For each such file, a file with the same
-                                # name, suffixed by .should_get must exist
+The script must contain a !LAUNCH: line stating what command line to be
+launched (the working directory is the directory where the input file is).  A
+line starting with !LOG: is the filename used for redirecting STDOUT from the
+command line. By default it is the input filename where the extension is
+replaced by .log.
 
-FILES=(src/tests/stanford.log) ### Hum.
+The output is in TAP format and consists of a file whose name is the same
+as the input file, where .should_get is replaced by .tap
+" >&2
+    exit 1
+fi
 
-COMMENT=("Testing SNPs")
+file=$1
+DIR=$(dirname $file)
+BASE=$(basename $file)
+cd "$DIR"
 
-echo "==>" $0.tap
+TAP_FILE=${BASE%.*}.tap
+LOG_FILE=${BASE%.*}.log
+
+echo "==>" $DIR/$TAP_FILE
 
 {
 nb_tests=0
 # Count number of tests to be performed
-#for file in ${FILES[*]}; do
-#    nb_tests=$((nb_tests+`grep -Pc '^[^$#]' $file.should_get`))
-#done
-nb_tests=1
+nb_tests=`grep -Pc '^[^$#!]' $BASE`
 
 echo "1.."$nb_tests
 test_nb=1
-for (( i = 0; $i < ${#FILES[*]}; i++ )); do
-    error=0
-    file=${FILES[$i]}
-    not_ok=$?
-    line_nb=1
-    failed_lines=()
-    while read line; do
-        if [ ! -z "$line" ]; then
-            if [ ${line:0:1} == '$' ]; then
-                msg=${line:1}
-            else
-                # This is not a comment
-                if [ ${line:0:1} != '#' ]; then
-                    skip=0
-                    know_to_fail=0
-
-                    pattern=$(cut -d: -f2- <<< $line)
-                    nb_hits=$(cut -d: -f1 <<< $line)
-
-                    if [ ${nb_hits:0:1} == "s" ]; then
-                        skip=1  # We skip the test if it fails
-                        nb_hits=${nb_hits:1}
-                    elif [ ${nb_hits:0:1} == "f" ]; then
-                        know_to_fail=1 # We know the test fails, but don't fail globally
-                        nb_hits=${nb_hits:1}
-                    fi
-                    if [ $(grep -cE "$pattern" $file) -eq $nb_hits -o $skip -eq 1 ]; then
-                        echo -n "ok"
-                    else
-                        echo -n "not ok"
-                        if [ $know_to_fail -eq 0 ]; then
-                            error=1
-                        fi
-                    fi
-                    echo -n " "$test_nb" "
-                    if [ $skip -eq 1 ]; then
-                        echo -n "# SKIP "
-                    fi
-                    echo "- " $msg
-                    test_nb=$((test_nb+1))
+error=0
+not_ok=$?
+line_nb=1
+failed_lines=()
+launched=0                      # Did we launch the program yet?
+cmd=
+while read line; do
+    if [ ! -z "$line" ]; then
+        if  [ ${line:0:1} == "!" ]; then
+            line=${line:1}
+            type=${line%%:*}
+            if [ "$type" == "LAUNCH" ]; then
+                cmd=${line#*:}
+            elif [ "$type" == "LOG" ]; then
+                LOG_FILE=${line#*:}
+            fi
+        elif [ ${line:0:1} == '$' ]; then
+            msg=${line:1}
+        else
+            if [ $launched -eq 0 ]; then
+                if [ -z "$cmd" ]; then
+                    echo "Error: you must specify a !LAUNCH: line in $file" >&2
+                    exit 2
                 fi
+                echo "Launching $cmd" >&2
+                eval $cmd > $LOG_FILE
+                launched=1
+            fi
+                # This is not a comment
+            if [ ${line:0:1} != '#' ]; then
+                skip=0
+                know_to_fail=0
+
+                pattern=$(cut -d: -f2- <<< $line)
+                nb_hits=$(cut -d: -f1 <<< $line)
+
+                if [ ${nb_hits:0:1} == "s" ]; then
+                    skip=1  # We skip the test if it fails
+                    nb_hits=${nb_hits:1}
+                elif [ ${nb_hits:0:1} == "f" ]; then
+                    know_to_fail=1 # We know the test fails, but don't fail globally
+                    nb_hits=${nb_hits:1}
+                fi
+                if [ $(grep -cE "$pattern" $LOG_FILE) -eq $nb_hits -o $skip -eq 1 ]; then
+                    echo -n "ok"
+                else
+                    echo -n "not ok"
+                    if [ $know_to_fail -eq 0 ]; then
+                        error=1
+                    fi
+                fi
+                echo -n " "$test_nb" "
+                if [ $skip -eq 1 ]; then
+                    echo -n "# SKIP "
+                fi
+                echo "- " $msg
+                test_nb=$((test_nb+1))
             fi
         fi
-        line_nb=$((line_nb+1))
-    done < ${file}.should_get
-done
-} > $0.tap
+    fi
+    line_nb=$((line_nb+1))
+done < $BASE
+} > $TAP_FILE
 
 
 exit $error
