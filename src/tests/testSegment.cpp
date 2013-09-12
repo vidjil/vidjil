97d6923aa43f7f737466ee928574e74be3f7ef8c
Mikael Salson <mikael.salson@lifl.fr>
2013-09-12 16:56:02 +0200

    testSegment: reindent

--- src/tests/testSegment.cpp
+++ src/tests/testSegment.cpp
@@ -17,14 +17,14 @@ void testSegment()
   
   Fasta data("../../data/Stanford_S22.fasta", 1, " ");
       
-	//segmentation VJ
+  //segmentation VJ
   FineSegmenter s(data.read(2), seqV, seqJ, 0, 50, VDJ);
 	
-	TAP_TEST(s.isSegmented(), TEST_SEGMENT_POSITION, "is segmented (VJ)") ;
-
-	//segmentation D
-	s.FineSegmentD(seqV, seqD, seqJ);
-	
-	TAP_TEST(s.isSegmented(), TEST_SEGMENT_POSITION, "is segmented (VDJ)") ;
+  TAP_TEST(s.isSegmented(), TEST_SEGMENT_POSITION, "is segmented (VJ)") ;
+  
+  //segmentation D
+  s.FineSegmentD(seqV, seqD, seqJ);
+  
+  TAP_TEST(s.isSegmented(), TEST_SEGMENT_POSITION, "is segmented (VDJ)") ;
 }
 
