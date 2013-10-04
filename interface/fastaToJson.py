2a27303d299f3e83a497d7bec6443f20dc0f4cd6
Marc <marc1.duez@etudiant.univ-lille1.fr>
2013-10-04 14:50:21 +0200

    fastaTojson >> garde uniquement l'identifiant

--- interface/fastaToJson.py
+++ interface/fastaToJson.py
@@ -17,18 +17,24 @@ for ligne in fasta :
   
   if len(ligne) != 0 :
     if ligne[0]=='>' :
+      
       if len(sequence)!=0 :
 	table[identifiant]=sequence
       identifiant=ligne[1:]
+      
+      if identifiant.count('|') != 0 :
+	tmp=identifiant.split('|')
+	identifiant=tmp[1]
       sequence="";
     else :
       sequence+=ligne
-  table[identifiant]=sequence
+      
+  if len(sequence)!=0 :
+    table[identifiant]=sequence
+  
   
 fasta.close()
-
 print json.dumps(table, indent=2, sort_keys=True)
-
 with open(output_name, "w") as file :
   json.dump(table, file, indent=2, sort_keys=True)
 
