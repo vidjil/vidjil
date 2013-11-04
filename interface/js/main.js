6ab0f34c2783b62a6fc30614fd219ce99064b706
duez <duez@duez-OptiPlex-9010.(none)>
2013-11-04 14:13:21 +0100

    main.js

---
+++ interface/js/main.js
@@ -0,0 +1,16 @@
+
+/* Model
+ *
+ * */
+var m = new Model();
+
+
+/* views
+ * 
+ * */
+var graph = new Graph("visu2",m);
+var list = new List("listClones",m);
+var sp = new ScatterPlot("visu",m);
+var segment = new Segment("bot_container",m);
+
+initTag();//TODO a enlever
