19bc4f522425d9377fbaa240e558750526381844
Mikael Salson <mikael.salson@lifl.fr>
2014-01-06 18:09:20 +0100

    Vidjil: Add a Windows© class in Vidjil for dealing with windows and their         associated reads.

---
+++ src/core/windows.cpp
@@ -0,0 +1,113 @@
+#include "tools.h"
+#include "json.h"
+#include "windows.h"
+
+WindowsStorage::WindowsStorage(map<string, string> &labels):windows_labels(labels) {}
+
+map<junction, list<Sequence> > &WindowsStorage::getMap() {
+  return seqs_by_window;
+}
+
+list<pair <junction, int> > &WindowsStorage::getSortedList() {
+  return sort_all_windows;
+}
+
+size_t WindowsStorage::getNbReads(junction window) {
+  return seqs_by_window[window].size();
+}
+
+list<Sequence> &WindowsStorage::getReads(junction window) {
+  return seqs_by_window[window];
+}
+
+size_t WindowsStorage::size() {
+  return seqs_by_window.size();
+}
+
+void WindowsStorage::add(junction window, Sequence sequence) {
+  seqs_by_window[window].push_back(sequence);
+}
+
+pair <int, int> WindowsStorage::keepInterestingWindows(size_t min_reads_window) {
+  int removes = 0 ;
+  int nb_reads = 0 ;
+
+  for (map <junction, list<Sequence> >::iterator it = seqs_by_window.begin(); 
+       it != seqs_by_window.end(); ++it)
+    {
+      junction junc = it->first;
+      
+      // Is it supported by enough reads?
+      if (!(seqs_by_window[junc].size() >= min_reads_window) 
+          // Is it a labelled junction?
+          && !(windows_labels.find(junc) == windows_labels.end()))
+        {
+          seqs_by_window.erase(junc);
+          removes++ ;
+        }
+      else
+        nb_reads += seqs_by_window[junc].size();
+    }
+  return make_pair(removes, nb_reads);
+}
+
+void WindowsStorage::sort() {
+  for (map <junction, list<Sequence> >::const_iterator it = seqs_by_window.begin(); 
+       it != seqs_by_window.end(); ++it) 
+    {
+      sort_all_windows.push_back(make_pair(it->first, it->second.size()));
+    }
+
+  sort_all_windows.sort(pair_occurrence_sort<junction>);
+}
+
+ostream &WindowsStorage::printSortedWindows(ostream &os) {
+  int num_seq = 0 ;
+
+  for (list<pair <junction, int> >::const_iterator it = sort_all_windows.begin(); 
+       it != sort_all_windows.end(); ++it) 
+    {
+      num_seq++ ;
+
+      os << windowToStream(os, it->first, num_seq, it->second);
+    }
+  return os;
+}
+
+JsonArray WindowsStorage::sortedWindowsToJsonArray(map <junction, JsonList> json_data_segment,
+                                                   list< pair <float, int> > norm_list,
+                                                   int nb_segmented) {
+  JsonArray windowsArray;
+  int top = 1;
+    
+  for (list<pair <junction, int> >::const_iterator it = sort_all_windows.begin(); 
+       it != sort_all_windows.end(); ++it) 
+    {
+	   
+      JsonList windowsList;
+    
+      JsonArray normalization_ratios;
+      normalization_ratios.add( json_normalization(norm_list, it->second, 
+                                                   nb_segmented) );
+    
+      JsonArray json_size;
+      json_size.add(it->second);
+
+      windowsList.add("seg", json_data_segment[it->first]);
+      windowsList.add("window", it->first);
+      windowsList.add("size", json_size);
+      windowsList.add("ratios", normalization_ratios);
+      windowsList.add("top", top++);
+	 
+      windowsArray.add(windowsList);
+    }
+
+  return windowsArray;
+}
+
+ostream &WindowsStorage::windowToStream(ostream &os, junction window, int num_seq, 
+                                        size_t size) {
+  os << ">" << size << "--window--" << num_seq << " " << windows_labels[window] << endl ;
+  os << window << endl;
+  return os;
+}
---
+++ src/core/windows.h
@@ -0,0 +1,88 @@
+
+/** 
+ * A window is associated to a list of sequences. We deal with a list of
+ * windows. We'd like to sort it, to output it, to remove windows not appearing
+ * enough (apart from the ones that are labelled).
+ */
+
+#include <iostream>
+#include <map>
+#include <utility>
+#include "fasta.h"
+#include "cluster-junctions.h"
+
+using namespace std;
+
+
+class WindowsStorage {
+ private:
+  map<junction, list<Sequence> > seqs_by_window;
+  map<string, string> windows_labels;
+  list<pair <junction, int> > sort_all_windows;
+ public:
+  /**
+   * Build an empty storage, with the labels that correspond to specific
+   * windows that must be kept. The map<string, string> keys are DNA sequences
+   * corresponding to the window, while values are the name of the sequence.
+   */
+  WindowsStorage(map<string, string> &labels);
+
+  /**
+   * Return the map storing the elements
+   */
+  map<junction, list<Sequence> > &getMap();
+
+  /**
+   * @return the number of reads supporting a given window
+   */
+  size_t getNbReads(junction window);
+
+  /**
+   * @return the list of reads supporting a given window
+   */
+  list<Sequence> &getReads(junction window);
+
+  /**
+   * @return a list of windows together with the number of reads they appear in.
+   * @pre sort() must have been called
+   */
+  list<pair <junction, int> > &getSortedList();
+
+  /**
+   * The number of windows stored
+   */
+  size_t size();
+
+  /**
+   * Add a new window with its list of sequences
+   */
+  void add(junction window, Sequence sequence);
+
+  /**
+   * Only keep windows that are interesting.  Those windows are windows
+   * supported by at least min_reads_window reads as well as windows that are
+   * labelled.  
+   * @return the number of windows removed as well as the number of
+   * reads finally kept.
+   */
+  pair<int, int> keepInterestingWindows(size_t min_reads_window);
+
+  /**
+   * sort windows according to the number of reads they appear in
+   */
+  void sort();
+
+  /**
+   * Print the windows from the most abundant to the least abundant
+   */ 
+  ostream &printSortedWindows(ostream &os);
+
+  JsonArray sortedWindowsToJsonArray(map<junction, JsonList> json_data_segment,
+                                     list< pair <float, int> > norm_list,
+                                     int nb_segmented);
+
+  /**
+   * Display a window with its in size in a somewhat FASTA format
+   */
+  ostream &windowToStream(ostream &os, junction window, int num_seq, size_t size);
+};
