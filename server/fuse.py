52ab444ef32fc6102d6b7f7a1144ed1c6e42d835
Mathieu Giraud <giraud@lifl.fr>
2014-02-17 21:45:08 +0100

    fuse.py: header, GPLv3+

--- server/fuse.py
+++ server/fuse.py
@@ -1,3 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+### fuse.py - Vidjil utility to parse and regroup list of clones of different timepoints or origins
+
+#  This file is part of "Vidjil" <http://bioinfo.lifl.fr/vidjil>
+#  Copyright (C) 2011, 2012, 2013, 2014 by Bonsai bioinformatics at LIFL (UMR CNRS 8022, Université Lille) and Inria Lille
+#
+#  "Vidjil" is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  "Vidjil" is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with "Vidjil". If not, see <http://www.gnu.org/licenses/>
+
 import collections
 import json
 import argparse
