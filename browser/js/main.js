e632330d2401064502c7d74b32b7b3e61bfa4bd2
Marc Duez <Marc.Duez@lifl.fr>
2014-04-03 12:00:13 +0200

    web2py : model file db.py >> define/init database table

--- browser/js/main.js
+++ browser/js/main.js
@@ -1,4 +1,4 @@
-var DB_ADDRESS = "http://134.206.11.64:8000/Vidjil_test/default/";
+var DB_ADDRESS = "http://134.206.11.64:8000/Vidjil/default/";
 var CGI_ADDRESS = "http://127.0.1.1/cgi-bin/";
 
 /*load user config if exist
