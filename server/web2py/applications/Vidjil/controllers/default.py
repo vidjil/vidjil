52b121387dd157a636955330903cba84e3699beb
Marc Duez <Marc.Duez@lifl.fr>
2014-04-09 17:12:09 +0200

    server : store sequence filename

--- server/web2py/applications/Vidjil/controllers/default.py
+++ server/web2py/applications/Vidjil/controllers/default.py
@@ -111,10 +111,15 @@ def add_file_form():
         response.headers['Access-Control-Max-Age'] = 86400
         
     id = db.sequence_file.insert(data_file = request.vars.file )
+  
+    query = db( ( db.sequence_file.id==id)).select() 
+    for row in query :
+        name = db.sequence_file.data_file.retrieve(row.data_file)[0]
     
     db.sequence_file[id] = dict(sampling_date=request.vars['sampling_date'],
                                 info=request.vars['file_info'],
-                                patient_id=request.vars['patient_id'])
+                                patient_id=request.vars['patient_id'],
+                                filename=name)
     
     res = {"success": "true" }
     return gluon.contrib.simplejson.dumps(res, separators=(',',':'))
