b91437da1650733f9875de0f2ce4be3e0431a332
Mikael Salson <mikael.salson@lifl.fr>
2014-04-14 16:41:38 +0200

    CountAffectAnalyser: Tests

--- algo/tests/testAffectAnalyser.cpp
+++ algo/tests/testAffectAnalyser.cpp
@@ -10,16 +10,21 @@ void testAffectAnalyser1() {
   T<KAffect> *index = createIndex<T<KAffect> >(k, revcomp);
   
   KmerAffectAnalyser<KAffect> kaa(*index, "AAAACCCCCGGGGG");
+  CountKmerAffectAnalyser<KAffect> ckaa(*index, "AAAACCCCCGGGGG");
 
   for (int i = 2; i < nb_seq-1; i++) {
+    // i starts at 2 because AAAA is not found: there is an ambiguity with
+    // AAAA coming from AAAACAAAACAAAAC or AAAAAAAAAAAAAAA
     KAffect current_affect("", seq[2*i+1], 1);
     TAP_TEST(kaa.count(current_affect) == 0, TEST_AA_COUNT, "");
+    TAP_TEST(ckaa.count(current_affect) == 0, TEST_COUNT_AA_COUNT, ckaa.count(current_affect));
     TAP_TEST(kaa.first(current_affect) == (int)string::npos, TEST_AA_FIRST, "");
     TAP_TEST(kaa.last(current_affect) == (int)string::npos, TEST_AA_LAST, "");
   }
   for (int i = 0; i < 2; i++) {
     KAffect current_affect("", seq[2*i+1], 1);
     TAP_TEST(kaa.count(current_affect) == 2, TEST_AA_COUNT, "");
+    TAP_TEST(ckaa.count(current_affect) == 2, TEST_COUNT_AA_COUNT, "");
     TAP_TEST(kaa.getAffectation(kaa.first(current_affect)) == current_affect, TEST_AA_GET_AFFECT, "");
     TAP_TEST(kaa.getAffectation(kaa.last(current_affect)) == current_affect, TEST_AA_GET_AFFECT, "");
   }
@@ -27,15 +32,49 @@ void testAffectAnalyser1() {
   TAP_TEST((kaa.first(KAffect("", seq[2*(nb_seq-1)+1], 1)) 
           == kaa.last(KAffect("", seq[2*(nb_seq-1)+1], 1)))
            == 1, TEST_AA_FIRST, "");
+
   TAP_TEST(kaa.getAffectation(3).isUnknown(), TEST_AA_PREDICATES, "");
   TAP_TEST(kaa.getAffectation(8).isUnknown(), TEST_AA_PREDICATES, "");
   TAP_TEST(kaa.getAffectation(0).isAmbiguous(), TEST_AA_PREDICATES, "");
   
   TAP_TEST(kaa.getDistinctAffectations().size() == 5, TEST_AA_GET_DISTINCT_AFFECT, "");
 
+  KAffect cAffect = KAffect("", seq[1], 1);
+  KAffect gAffect = KAffect("", seq[3], 1);
+  TAP_TEST(ckaa.countBefore(cAffect, 4) == 0, TEST_COUNT_AA_COUNT_BEFORE, "");
+  TAP_TEST(ckaa.countBefore(cAffect, 5) == 1, TEST_COUNT_AA_COUNT_BEFORE, "");
+  TAP_TEST(ckaa.countAfter(cAffect, 4) == 1, TEST_COUNT_AA_COUNT_AFTER, "");
+  TAP_TEST(ckaa.countAfter(cAffect, 5) == 0, TEST_COUNT_AA_COUNT_AFTER, "");
+
+  TAP_TEST(ckaa.countAfter(gAffect, 4) == 2, TEST_COUNT_AA_COUNT_AFTER, "");
+  TAP_TEST(ckaa.countAfter(gAffect, 5) == 2, TEST_COUNT_AA_COUNT_AFTER, "");
+  TAP_TEST(ckaa.countBefore(gAffect, 4) == 0, TEST_COUNT_AA_COUNT_BEFORE, "");
+  TAP_TEST(ckaa.countBefore(gAffect, 5) == 0, TEST_COUNT_AA_COUNT_BEFORE, "");
+
+  TAP_TEST(ckaa.countBefore(cAffect, 9) == 2, TEST_COUNT_AA_COUNT_BEFORE, "");
+  TAP_TEST(ckaa.countBefore(cAffect, 10) == 2, TEST_COUNT_AA_COUNT_BEFORE, "");
+  TAP_TEST(ckaa.countAfter(cAffect, 9) == 0, TEST_COUNT_AA_COUNT_AFTER, "");
+  TAP_TEST(ckaa.countAfter(cAffect, 10) == 0, TEST_COUNT_AA_COUNT_AFTER, "");
+
+  TAP_TEST(ckaa.countAfter(gAffect, 9) == 1, TEST_COUNT_AA_COUNT_AFTER, "");
+  TAP_TEST(ckaa.countAfter(gAffect, 10) == 0, TEST_COUNT_AA_COUNT_AFTER, "");
+  TAP_TEST(ckaa.countBefore(gAffect, 9) == 0, TEST_COUNT_AA_COUNT_BEFORE, "");
+  TAP_TEST(ckaa.countBefore(gAffect, 10) == 1, TEST_COUNT_AA_COUNT_BEFORE, "");
+
+  TAP_TEST(ckaa.firstMax(cAffect, gAffect) == 6, TEST_COUNT_AA_FIRST_MAX, "");
+  TAP_TEST(ckaa.lastMax(cAffect, gAffect) == 8, TEST_COUNT_AA_LAST_MAX, ckaa.lastMax(cAffect, gAffect));
+
+  // Test affectation with two affects that are not in the sequence
+  KAffect aAffect = KAffect("", seq[5], 1);
+  KAffect tAffect = KAffect("", seq[7], 1);
+  TAP_TEST(ckaa.firstMax(aAffect, tAffect) == 0, TEST_COUNT_AA_FIRST_MAX, "");
+  TAP_TEST(ckaa.lastMax(aAffect, tAffect) == ckaa.count() - 1, 
+           TEST_COUNT_AA_LAST_MAX, "");
+
   delete index;
 }
 
+// Test with revcomp
 template<template <class> class T, class KAffect>
 void testAffectAnalyser2() {
   const int k = 5;
@@ -43,10 +82,14 @@ void testAffectAnalyser2() {
   T<KAffect> *index = createIndex<T<KAffect> >(k, revcomp);
   
   KmerAffectAnalyser<KAffect> kaa(*index, "TTTTTGGGGG");
+  CountKmerAffectAnalyser<KAffect> ckaa(*index, "TTTTTGGGGG");
   
   TAP_TEST(kaa.getAffectation(1) == KAffect("", seq[2*(nb_seq-1)+1], -1), TEST_AA_GET_AFFECT, "");
   TAP_TEST(kaa.count(kaa.getAffectation(1)) == 1, TEST_AA_GET_AFFECT, "");
+  TAP_TEST(ckaa.count(kaa.getAffectation(1)) == 1, TEST_COUNT_AA_COUNT, "");
   TAP_TEST(kaa.getAffectation(0) == kaa.getAffectation(10 - k), TEST_AA_GET_AFFECT, "");
+  TAP_TEST(kaa.getAffectation(0).isAmbiguous(), TEST_AA_PREDICATES, "");
+
   for (int i = 2; i < 10 - k; i++)
     TAP_TEST(kaa.getAffectation(i).isUnknown(), TEST_AA_PREDICATES, "");
 
