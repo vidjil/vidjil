8bc5bf0bb9fc2d2b056c1d4d3108805d816ca540
Marc Duez <Marc.Duez@lifl.fr>
2014-04-15 17:56:17 +0200

    server : add edit form for patient/ sequence file / config / standard file - add view file for each - controller function edit and edit_form for each - add "e" button nead editable data

--- server/web2py/applications/Vidjil/controllers/config.py
+++ server/web2py/applications/Vidjil/controllers/config.py
@@ -8,12 +8,14 @@ def index():
         response.headers['Access-Control-Max-Age'] = 86400
     return dict(message=T('config_list'))
 
+
 def add(): 
     if request.env.http_origin:
         response.headers['Access-Control-Allow-Origin'] = request.env.http_origin  
         response.headers['Access-Control-Allow-Credentials'] = 'true'
         response.headers['Access-Control-Max-Age'] = 86400
     return dict(message=T('add config'))
+
 
 #TODO check data
 def add_form(): 
@@ -23,11 +25,71 @@ def add_form():
         response.headers['Access-Control-Allow-Credentials'] = 'true'
         response.headers['Access-Control-Max-Age'] = 86400
     
-    db.config.insert(name=request.vars['config_name'],
-                    info=request.vars['config_info'],
-                    standard_id=request.vars['config_standard'],
-                    command=request.vars['config_command'],
-                    )
+        
+    error =""
+
+    if request.vars['config_name'] == "" :
+        error += "name needed, "
+    if request.vars['config_info'] == "" :
+        error += "info needed, "
+    if request.vars['config_standard'] == "" :
+        error += "standard needed, "
+    if request.vars['config_command'] == "" : 
+        error += "command needed, "  
+
+    if error=="" :
+        
+        db.config.insert(name=request.vars['config_name'],
+                        info=request.vars['config_info'],
+                        standard_id=request.vars['config_standard'],
+                        command=request.vars['config_command'],
+                        )
+
+        res = {"success": "true" }
+        return gluon.contrib.simplejson.dumps(res, separators=(',',':'))
+        
+    else :
+        res = {"success" : "false", "error" : error}
+        return gluon.contrib.simplejson.dumps(res, separators=(',',':'))
+
+
+def edit(): 
+    if request.env.http_origin:
+        response.headers['Access-Control-Allow-Origin'] = request.env.http_origin  
+        response.headers['Access-Control-Allow-Credentials'] = 'true'
+        response.headers['Access-Control-Max-Age'] = 86400
+    return dict(message=T('edit config'))
+
+
+def edit_form(): 
+    import gluon.contrib.simplejson, shutil, os.path
+    if request.env.http_origin:
+        response.headers['Access-Control-Allow-Origin'] = request.env.http_origin  
+        response.headers['Access-Control-Allow-Credentials'] = 'true'
+        response.headers['Access-Control-Max-Age'] = 86400
     
-    res = {"success": "true" }
-    return gluon.contrib.simplejson.dumps(res, separators=(',',':'))
+    error =""
+
+    if request.vars['config_name'] == "" :
+        error += "name needed, "
+    if request.vars['config_info'] == "" :
+        error += "info needed, "
+    if request.vars['config_standard'] == "" :
+        error += "standard needed, "
+    if request.vars['config_command'] == "" : 
+        error += "command needed, "  
+
+    if error=="" :
+
+        db.config[request.vars["id"]] = dict(name=request.vars['config_name'],
+                                            info=request.vars['config_info'],
+                                            standard_id=request.vars['config_standard'],
+                                            command=request.vars['config_command'],
+                                            )
+
+        res = {"success": "true" }
+        return gluon.contrib.simplejson.dumps(res, separators=(',',':'))
+
+    else :
+        res = {"success" : "false", "error" : error}
+        return gluon.contrib.simplejson.dumps(res, separators=(',',':'))
--- server/web2py/applications/Vidjil/controllers/standard.py
+++ server/web2py/applications/Vidjil/controllers/standard.py
@@ -29,3 +29,33 @@ def add_form():
     
     res = {"success": "true" }
     return gluon.contrib.simplejson.dumps(res, separators=(',',':'))
+
+def edit(): 
+    if request.env.http_origin:
+        response.headers['Access-Control-Allow-Origin'] = request.env.http_origin  
+        response.headers['Access-Control-Allow-Credentials'] = 'true'
+        response.headers['Access-Control-Max-Age'] = 86400
+    return dict(message=T('edit standard file'))
+
+def edit_form(): 
+    import gluon.contrib.simplejson, shutil, os.path
+    if request.env.http_origin:
+        response.headers['Access-Control-Allow-Origin'] = request.env.http_origin  
+        response.headers['Access-Control-Allow-Credentials'] = 'true'
+        response.headers['Access-Control-Max-Age'] = 86400
+        
+        error = ""
+        
+        if request.vars["standard_name"] == "" :
+            error += "standard name needed, "
+        
+        if error=="" :
+            db.standard_file[request.vars["id"]] = dict(name=request.vars['standard_name'],
+                                                        info=request.vars['standard_info'])
+            
+            res = {"success": "true" }
+            return gluon.contrib.simplejson.dumps(res, separators=(',',':'))
+        
+        else :
+            res = {"success" : "false", "error" : error}
+            return gluon.contrib.simplejson.dumps(res, separators=(',',':'))
