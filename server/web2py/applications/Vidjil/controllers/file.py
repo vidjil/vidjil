9d143584543505fb5774bdffd48b10395fe3e041
Marc Duez <marc.duez@lifl.fr>
2014-04-15 17:56:17 +0200

    server : add edit form for patient/ sequence file / config / standard file - add view file for each - controller function edit and edit_form for each - add "e" button nead editable data

--- server/web2py/applications/Vidjil/controllers/file.py
+++ server/web2py/applications/Vidjil/controllers/file.py
@@ -7,6 +7,7 @@ def add():
         response.headers['Access-Control-Max-Age'] = 86400
     return dict(message=T('add file'))
 
+
 #TODO check data
 def add_form(): 
     import gluon.contrib.simplejson, shutil, os.path
@@ -15,18 +16,35 @@ def add_form():
         response.headers['Access-Control-Allow-Credentials'] = 'true'
         response.headers['Access-Control-Max-Age'] = 86400
         
-    id = db.sequence_file.insert(data_file = request.vars.file )
-  
-    name = ""
-    query = db( ( db.sequence_file.id==id)).select() 
-    for row in query :
-        name = db.sequence_file.data_file.retrieve(row.data_file)[0]
-    
+    id = db.sequence_file.insert(data_file = request.vars.file)
     
     db.sequence_file[id] = dict(sampling_date=request.vars['sampling_date'],
                                 info=request.vars['file_info'],
-                                patient_id=request.vars['patient_id'],
-                                filename=name)
+                                patient_id=request.vars['patient_id'])
     
+    res = {"success": "true" }
+    return gluon.contrib.simplejson.dumps(res, separators=(',',':'))
+
+
+def edit(): 
+    if request.env.http_origin:
+        response.headers['Access-Control-Allow-Origin'] = request.env.http_origin  
+        response.headers['Access-Control-Allow-Credentials'] = 'true'
+        response.headers['Access-Control-Max-Age'] = 86400
+    return dict(message=T('edit file'))
+
+
+#TODO check data
+def edit_form(): 
+    import gluon.contrib.simplejson, shutil, os.path
+    if request.env.http_origin:
+        response.headers['Access-Control-Allow-Origin'] = request.env.http_origin  
+        response.headers['Access-Control-Allow-Credentials'] = 'true'
+        response.headers['Access-Control-Max-Age'] = 86400
+  
+    db.sequence_file[request.vars["id"]] = dict(sampling_date=request.vars['sampling_date'],
+                                                info=request.vars['file_info'],
+                                                )
+
     res = {"success": "true" }
     return gluon.contrib.simplejson.dumps(res, separators=(',',':'))
--- server/web2py/applications/Vidjil/controllers/patient.py
+++ server/web2py/applications/Vidjil/controllers/patient.py
@@ -63,3 +63,56 @@ def add_form():
         
     res2 = {"success" : "false", "error" : "connect error"}
     return gluon.contrib.simplejson.dumps(res2, separators=(',',':'))
+
+
+def edit(): 
+    if request.env.http_origin:
+        response.headers['Access-Control-Allow-Origin'] = request.env.http_origin  
+        response.headers['Access-Control-Allow-Credentials'] = 'true'
+        response.headers['Access-Control-Max-Age'] = 86400
+    return dict(message=T('edit patient'))
+
+def edit_form(): 
+    import gluon.contrib.simplejson
+    if request.env.http_origin:
+        response.headers['Access-Control-Allow-Origin'] = request.env.http_origin  
+        response.headers['Access-Control-Allow-Credentials'] = 'true'
+        response.headers['Access-Control-Max-Age'] = 86400
+        
+        error = ""
+        
+        if request.vars["first_name"] == "" :
+            error += "first name needed, "
+        if request.vars["last_name"] == "" :
+            error += "last name needed, "
+        if request.vars["birth"] == "" :
+            error += "birth date needed, "
+        if request.vars["id"] == "" :
+            error += "patient id needed, "
+        
+        if error=="" :
+            db.patient[request.vars["id"]] = dict(first_name=request.vars["first_name"],
+                                                   last_name=request.vars["last_name"],
+                                                   birth=request.vars["birth"],
+                                                   info=request.vars["info"]
+                                                   )
+            
+            res = {"success": "true" }
+            return gluon.contrib.simplejson.dumps(res, separators=(',',':'))
+        
+        else :
+            res = {"success" : "false", "error" : error}
+            return gluon.contrib.simplejson.dumps(res, separators=(',',':'))
+        
+        
+@cache.action()
+def download():
+    if request.env.http_origin:
+        response.headers['Access-Control-Allow-Origin'] = request.env.http_origin  
+        response.headers['Access-Control-Allow-Credentials'] = 'true'
+        response.headers['Access-Control-Max-Age'] = 86400
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request, db)
