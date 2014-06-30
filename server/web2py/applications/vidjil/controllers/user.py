fe800f46ae4d2e05b070b2e8cc40c77c735d7ec5
Marc Duez <marc.duez@lifl.fr>
2014-06-30 16:23:54 +0200

    server: refactor controller

--- server/web2py/applications/vidjil/controllers/user.py
+++ server/web2py/applications/vidjil/controllers/user.py
@@ -1,21 +1,14 @@
-import toolbox
-
+import gluon.contrib.simplejson
+if request.env.http_origin:
+    response.headers['Access-Control-Allow-Origin'] = request.env.http_origin  
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+    response.headers['Access-Control-Max-Age'] = 86400
+        
 ## return user list
 def index():
-    import gluon.contrib.simplejson
-    if request.env.http_origin:
-        response.headers['Access-Control-Allow-Origin'] = request.env.http_origin  
-        response.headers['Access-Control-Allow-Credentials'] = 'true'
-        response.headers['Access-Control-Max-Age'] = 86400
-    
     return dict(message=T('user list'))
 
 ## return user information
 ## need ["id"]
 def info():
-    import gluon.contrib.simplejson
-    if request.env.http_origin:
-        response.headers['Access-Control-Allow-Origin'] = request.env.http_origin  
-        response.headers['Access-Control-Allow-Credentials'] = 'true'
-        response.headers['Access-Control-Max-Age'] = 86400
     return dict(message=T('user info'))
