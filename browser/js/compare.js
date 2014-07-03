88a81b7e1c33bcda56ad24c6890f812933fbbcf5
WebTogz <antonin.carette@etudiant.univ-lille1.fr>
2014-07-03 11:41:52 +0200

    js/compare.js: GPL

--- browser/js/compare.js
+++ browser/js/compare.js
@@ -1,3 +1,22 @@
+/*
+* This file is part of "Vidjil" <http://bioinfo.lifl.fr/vidjil>, V(D)J repertoire browsing and analysis
+* Copyright (C) 2014 by Carette Antonin <antonin.carette@etudiant.univ-lille1.fr> and the Vidjil Team
+* Bonsai bioinformatics at LIFL (UMR CNRS 8022, Université Lille) and Inria Lille
+*
+* "Vidjil" is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* "Vidjil" is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with "Vidjil". If not, see <http://www.gnu.org/licenses/>
+*/
+
 /* Fonction permettant de créer une fonction de comparaison, selon le schéma donné par celle ci-dessous
  * @param tab: Le tableau que l'on veut trier 
  * Ne renvoie rien - l'ancien tableau est écrasé par le tri que l'on vient de faire
