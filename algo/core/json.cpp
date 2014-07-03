0a2e7ef4f87a3b3a18ba9f22b9b47c6ff8650515
WebTogz <antonin.carette@etudiant.univ-lille1.fr>
2014-07-03 11:34:36 +0200

    core/ : more comments

--- algo/core/json.cpp
+++ algo/core/json.cpp
@@ -5,10 +5,15 @@
 
 using namespace std ;
 
-
+/*
+ Constructeur de JsonData
+ */
 JsonData::JsonData(){
 }
 
+/*
+ Méthode permettant de retourner le contenu d'un tampon, dans lequel on a "imprimé" un nom ainsi que ses données
+ */
 string JsonData::toString(){
   ostringstream stream;
   
@@ -16,11 +21,15 @@ string JsonData::toString(){
   return stream.str();;
 }
 
-
-
+/*
+ Constructeur de JsonList
+ */
 JsonList::JsonList(){
 }
 
+/*
+ Méthode permettant d'ajouter un nouvel élément Json composé d'un nom et de ses données
+ */
 void JsonList::add(string n, string d){
   JsonData elem;
   
@@ -29,16 +38,24 @@ void JsonList::add(string n, string d){
   l.push_back(elem);
 }
 
+/*
+ Méthode permettant d'ajouter un élèment Json composé d'un nom ainsi que d'une chaîne de caractères représentant le flottant donné en paramètre
+ */
 void JsonList::add(string n, float d){
   JsonData elem;
   ostringstream stream;
   
+  //Impression du flottant dans le buffer
   stream << d;
   elem.name=n;
+  //On prend la chaîne de caractères associée à l'impression du flottant dans le buffer
   elem.data=stream.str();
   l.push_back(elem);
 }
 
+/*
+ Méthode permettant d'ajouter un élèment Json composé d'un nom et d'une liste d'objets Json
+ */
 void JsonList::add(string n, JsonList &d){
   JsonData elem;
   
@@ -47,6 +64,9 @@ void JsonList::add(string n, JsonList &d){
   l.push_back(elem);
 }
 
+/*
+ Méthode permettant d'ajouter un élèment Json composé d'un nom et d'un tableau d'objets Json
+ */
 void JsonList::add(string n, JsonArray &d){
   JsonData elem;
   
@@ -55,10 +75,16 @@ void JsonList::add(string n, JsonArray &d){
   l.push_back(elem);
 }
 
+/*
+ Méthode permettant de concaténer 2 objets JsonList
+ */
 void JsonList::concat(JsonList &d){
 	l.insert(l.end(), d.l.begin(), d.l.end());
 }
 
+/*
+ Méthode permettant de retourner le contenu d'une JsonList
+ */
 string JsonList::toString(){
   ostringstream stream;
   
@@ -74,29 +100,45 @@ string JsonList::toString(){
   return stream.str();;
 }
 
-
-
+/*
+ Constructeur de JsonArray
+ */
 JsonArray::JsonArray(){
 }
 
+/*
+ Méthode permettant d'ajouter un nom d'objet
+ */
 void JsonArray::add(string d){
   l.push_back("\""+d+"\"");
 }
 
+/*
+ Méthode permettant d'ajouter une donnée 
+ */
 void JsonArray::add(float d){
   ostringstream stream;
   stream << d;
   l.push_back(stream.str());
 }
 
+/*
+ Méthode permettant d'ajouter un tableau d'objets Json (type: JsonArray)
+ */
 void JsonArray::add(JsonArray &d){
   l.push_back(d.toString());
 }
 
+/*
+ Méthode permettant d'ajouter une liste d'objets Json (type: JsonList)
+ */
 void JsonArray::add(JsonList &d){
   l.push_back(d.toString());
 }
 
+/*
+ Méthode permettant de retourner le contenu d'un objet JsonArray
+ */
 string JsonArray::toString(){
   ostringstream stream;
   
@@ -112,9 +154,9 @@ string JsonArray::toString(){
   return stream.str();
 }
 
-
-
-
+/*
+ Méthode permettant de retourner un tableau d'objets Json, contenant le nom des normalisations
+ */
 JsonArray json_normalization_names()
 {
   JsonArray result;
@@ -125,6 +167,9 @@ JsonArray json_normalization_names()
   return result;
 }
 
+/*
+ Méthode permettant de retourner un tableau d'objets Json, contenant des données concernant la normalisation d'un jeu, donné en paramètre
+ */
 JsonArray json_normalization( list< pair <float, int> > norm_list, int nb_reads, int nb_segmented)
 {
   JsonArray result;
