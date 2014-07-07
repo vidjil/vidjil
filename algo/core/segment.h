90cab056e0d17a89ecb1baf83a9710faacef210c
Mikael Salson <mikael.salson@lifl.fr>
2014-07-07 16:25:38 +0200

    Segment: Use KmerAffectAnalyser instead of CountKmerAffectAnalyser.
    While CountKmerAffectAnalyser is more flexible, the heuristic implemented
    in KmerAffectAnalyser is more efficient, that's why we use it in the KmerSegmenter

--- algo/core/segment.h
+++ algo/core/segment.h
@@ -127,7 +127,7 @@ class KmerSegmenter : public Segmenter
 {
  private:
   int because;                  
-  CountKmerAffectAnalyser<KmerAffect> *kaa;
+  KmerAffectAnalyser<KmerAffect> *kaa;
  protected:
   string affects;
 
@@ -151,7 +151,7 @@ class KmerSegmenter : public Segmenter
   /**
    * @return the KmerAffectAnalyser of the current sequence.
    */
-  CountKmerAffectAnalyser<KmerAffect> *getKmerAffectAnalyser() const;
+  KmerAffectAnalyser<KmerAffect> *getKmerAffectAnalyser() const;
 
   /**
    * @return the status of the segmentation. Tells if the Sequence has been segmented
