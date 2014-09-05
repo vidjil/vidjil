vidjil/vidjil

commit 92519fc99ce05119ac035eedc41388e8c577590d

fuse.py: .cut() uses OtherWindows, aggregating discarded windows in 'other-' pseudo-windows

server/fuse.py
@@ -342,17 +342,22 @@ class ListWindows:
         return result
         
     
-    def cut(self, limit):
-        '''Delete all information from sequence/windows who never enter in the most represented sequences.'''
+    def cut(self, limit, nb_points):
+        '''Remove information from sequence/windows who never enter in the most represented sequences. Put this information in 'other' windows.'''
 
         length = len(self.d["windows"])
         w=[]
-    
-        for index in range(length): 
-            if (int(self.d["windows"][index].d["top"]) < limit or limit == 0) :
-                w.append(self.d["windows"][index])
 
-        self.d["windows"]=w
+        others = OtherWindows(nb_points)
+
+        for index in range(length): 
+            win = self.d["windows"][index]
+            if (int(win.d["top"]) < limit or limit == 0) :
+                w.append(win)
+            else:
+                others += win
+
+        self.d["windows"] = w + list(others) 
         self.d["germline"]=self.d["germline"][0]
 
         print "### Cut merged file, keeping window in the top %d for at least one point" % limit
@@ -736,7 +741,7 @@ def main():
         jlist_fused.d["point"] = ll
     
     print
-    jlist_fused.cut(args.top)
+    jlist_fused.cut(args.top, len(l))
     print "\t", jlist_fused 
     print
 

commit 3d3819814bea3eed660305d032602b12bffff42a

fuse.py: --merge works again
Corrects bug introduced by 92519fc9

server/fuse.py
@@ -773,7 +773,7 @@ def main():
     
     print
     jlist_fused.build_stat()
-    jlist_fused.cut(args.top, len(l))
+    jlist_fused.cut(args.top, len(jlist_fused.d["point"]))
     print "\t", jlist_fused 
     print
 