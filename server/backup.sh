c29429411d4c5153d3279189c9c5af66b579a306
Mikael Salson <mikael.salson@lifl.fr>
2014-09-09 18:56:46 +0200

    Backup: We can provide a path that specify where to save the backup

--- server/backup.sh
+++ server/backup.sh
@@ -1,11 +1,31 @@
 #!/bin/sh
 
+COMPLETE=0
+DIR=
+
+if [ $# -ge 1 -a "$1" = "-h" ]; then
+    echo "$0: [-c] [path]
+
+-c:   Backup everything
+path: Where to save the file" >&2
+    exit 1
+fi
+
+if [ $# -ge 1 -a "$1" = "-c" ]; then
+    COMPLETE=1
+    shift
+fi
+
+if [ $# -ge 1 ]; then
+    DIR="$1"
+fi
+
 now=$(date +"%Y_%m_%d") 
-if [ "$1" = "-c" ]; then
-        filename="backup_"$now
+if [ $COMPLETE ]; then
+        filename="$DIR/backup_"$now
         zip -r $filename web2py/applications/vidjil/uploads/ web2py/applications/vidjil/databases/
 else
-        filename="backup_essentials_"$now
+        filename="$DIR/backup_essentials_"$now
         zip -r $filename web2py/applications/vidjil/databases/ web2py/applications/vidjil/uploads/data_file* web2py/applications/vidjil/uploads/fused_file*
 fi
 
