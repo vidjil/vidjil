750f2e83cd84ecc5d1d8b96e5d1d06bdd65ed077
Mikael Salson <mikael.salson@lifl.fr>
2014-09-09 19:03:03 +0200

    Backup: Trailing / must be put in the variable
    Otherwise we save in the root by default

--- server/backup.sh
+++ server/backup.sh
@@ -17,15 +17,15 @@ if [ $# -ge 1 -a "$1" = "-c" ]; then
 fi
 
 if [ $# -ge 1 ]; then
-    DIR="$1"
+    DIR="$1/"
 fi
 
 now=$(date +"%Y_%m_%d") 
 if [ $COMPLETE ]; then
-        filename="$DIR/backup_"$now
+        filename="${DIR}backup_"$now
         zip -r $filename web2py/applications/vidjil/uploads/ web2py/applications/vidjil/databases/
 else
-        filename="$DIR/backup_essentials_"$now
+        filename="${DIR}backup_essentials_"$now
         zip -r $filename web2py/applications/vidjil/databases/ web2py/applications/vidjil/uploads/data_file* web2py/applications/vidjil/uploads/fused_file*
 fi
 
