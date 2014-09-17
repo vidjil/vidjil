75e96632a0581ce90958baad4ad2000224a1a2ae
Marc Duez <marc.duez@lifl.fr>
2014-09-17 16:07:11 +0200

    server : check user for admin panel

--- server/web2py/applications/vidjil/controllers/admin.py
+++ server/web2py/applications/vidjil/controllers/admin.py
@@ -8,21 +8,24 @@ if request.env.http_origin:
 
 ## return admin_panel
 def index():
-    return dict(message=T(''))
+    if auth.has_membership("admin"):
+        return dict(message=T(''))
 
 
 def result():
-    return dict(message=T(''))
+    if auth.has_membership("admin"):
+        return dict(message=T(''))
 
 def run_all():
-    query = db(
-            (db.data_file.sequence_file_id==db.sequence_file.id)
-            & (db.data_file.config_id==db.config.id)
-        ).select()
-    
-    for row in query:
-        schedule_run(row.sequence_file.id, row.config.id)
-        
-    res = {"success" : "true",
-           "message" : "rerun all"}
-    return gluon.contrib.simplejson.dumps(res, separators=(',',':'))
+    if auth.has_membership("admin"):
+        query = db(
+                (db.data_file.sequence_file_id==db.sequence_file.id)
+                & (db.data_file.config_id==db.config.id)
+            ).select()
+
+        for row in query:
+            schedule_run(row.sequence_file.id, row.config.id)
+
+        res = {"success" : "true",
+               "message" : "rerun all"}
+        return gluon.contrib.simplejson.dumps(res, separators=(',',':'))
