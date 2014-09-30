7ef7aaf9bc1a9721a7cb5f1e62ca7c72ed0ca694
Mathieu Giraud <giraud@lifl.fr>
2014-10-01 00:00:30 +0200

    db.py: remove option 'remember me', default timeout to 1 week

--- server/web2py/applications/vidjil/models/db.py
+++ server/web2py/applications/vidjil/models/db.py
@@ -57,6 +57,10 @@ auth.settings.registration_requires_verification = False
 auth.settings.registration_requires_approval = False
 auth.settings.reset_password_requires_verification = True
 
+# auth.settings.login_email_validate = False
+auth.settings.expiration = 3600 * 24 * 7  # one week
+auth.settings.remember_me_form = False
+
 ## if you need to use OpenID, Facebook, MySpace, Twitter, Linkedin, etc.
 ## register with janrain.com, write your domain:api_key in private/janrain.key
 from gluon.contrib.login_methods.rpx_account import use_janrain
