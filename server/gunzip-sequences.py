3a44cbc16e1d28cc708e7a4e8394ff37d4263931
Mathieu Giraud <giraud@lifl.fr>
2014-10-30 12:44:31 +0100

    server/gunzip-sequences.py: unzip '.gz' sequence files and fool the DB.
    This may be a temporary hack.

---
+++ server/gunzip-sequences.py
@@ -0,0 +1,28 @@
+
+import sys
+import glob
+import os
+
+from subprocess import Popen, PIPE, STDOUT
+
+UPLOADS = '/mnt/upload/uploads/'
+
+for f in glob.glob(UPLOADS + '/*.gz'):
+
+    print "<==", f
+
+    ### Quick check
+    cmd = 'gunzip -l "%s"' % f
+    p = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE) #STDOUT, stderr=STDOUT, close_fds=True)
+    (stdoutdata, stderrdata) = p.communicate()
+    if 'not in gzip format' in stderrdata:
+        print stderrdata
+        continue
+
+    ### Unzip, then link to original name to fool the DB
+    f_base = f.replace('.gz', '')
+    cmd = 'gunzip "%s" ; ln -s "%s" "%s"' % (f, f_base, f)
+    print cmd
+    os.system(cmd)
+
+
