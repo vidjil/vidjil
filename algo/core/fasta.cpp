vidjil/vidjil

commit 59a1bcf6f7ef9431bf241e848b74415c0ff73c41

core/fasta.cpp: Fasta::add() also prints some message on stdout

algo/core/fasta.cpp
@@ -47,14 +47,14 @@ Fasta::Fasta(const string &input,
   if (!input.size()) // Do not open empty files (D germline if not segmentD)
     return ;
 
+  out << "  <== " << input ;
   add(input);  
-
-  out << "  <== " << input
-      << "\t" << setw(6) << total_size << " bp in " << setw(3) << size() << " sequences" << endl ;
 }
 
 void Fasta::add(istream &in) {
   in >> *this;
+
+  cout << "\t" << setw(6) << total_size << " bp in " << setw(3) << size() << " sequences" << endl ;
 }
 
 void Fasta::add(const string &filename) {
@@ -64,6 +64,8 @@ void Fasta::add(const string &filename) {
       cerr << "  !! Error in opening file: " << filename << endl ;
       exit(1);
     }
+
+  cout << " <== " << filename ;
   add(is);
   is.close();
 }

commit fa7f5a22999cbe0a2b5371f94bc36502489c4bce

core/fasta.cpp: remove duplicate stdout message
Corrects bug introduced by 59a1bcf.

algo/core/fasta.cpp
@@ -47,7 +47,6 @@ Fasta::Fasta(const string &input,
   if (!input.size()) // Do not open empty files (D germline if not segmentD)
     return ;
 
-  out << "  <== " << input ;
   add(input);  
 }
 