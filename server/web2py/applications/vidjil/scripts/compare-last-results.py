e8b0805f1316418af913fd5ee8c6876d5e1050a5
Mathieu Giraud <giraud@lifl.fr>
2014-11-13 18:24:42 +0100

    compare-last-results.py: compare .vidjil files across several launches
    This is a kind of generalized non-regression test, comparing
    the two last .vidjil results for *all* files on the server.

---
+++ server/web2py/applications/vidjil/scripts/compare-last-results.py
@@ -0,0 +1,25 @@
+
+DIR_SEQUENCES = '/mnt/upload/uploads'
+
+
+for seq in db(db.sequence_file).select():
+    print "===", "seq-%d" % seq.id, "\t", seq.sampling_date, "\t", seq.filename, seq.data_file
+    files = []
+
+    for res in db(db.results_file.sequence_file_id == seq.id).select():
+       print "   ", "sched-%d" % res.scheduler_task_id, "\t", res.run_date, "\t", res.data_file
+       if res.data_file:
+              files += [res.data_file]
+
+
+    if len(files) < 2:
+       continue
+
+    ### Compare .vidjil files across several launches
+    old = files[-2]
+    new = files[-1]
+    
+    com = 'python /home/vidjil/server/diff.py %s/%s %s/%s' % (DIR_SEQUENCES, old, DIR_SEQUENCES, new)
+    # print com
+    os.system(com)
+
