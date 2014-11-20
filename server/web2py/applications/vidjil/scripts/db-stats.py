a55c090cd0218464a9df3da3c5a667d4de4ef444
Mathieu Giraud <giraud@lifl.fr>
2014-11-20 08:38:35 +0100

    db-stats.py: reorder blocks, most signifiant first

--- server/web2py/applications/vidjil/scripts/db-stats.py
+++ server/web2py/applications/vidjil/scripts/db-stats.py
@@ -15,15 +15,17 @@ def ellipsis(what, max_size=30):
     return what
 
 print
-print
-print "=== Tables"
-
-for table in db.tables:
-    print "%10s %-20s" % (len(db(db[table]).select()), table)
 
 yesterday = datetime.datetime.now() - datetime.timedelta(hours=LAST_HOURS)
 
 RUN_CONFIG_SEQ_PATIENT = (db.results_file.sequence_file_id == db.sequence_file.id) & (db.patient.id == db.sequence_file.patient_id) & (db.config.id == db.results_file.config_id) & (db.scheduler_task.id == db.results_file.scheduler_task_id)
+
+print 
+print "=== Recent not completed tasks, last %d hours" % LAST_HOURS
+
+for res in db((db.scheduler_task.last_run_time >= yesterday) & (db.scheduler_task.status != 'COMPLETED')).select():
+    print "   ", "sch-%04d" % res.id, "\t", res.status, "   ", "\t", res.start_time, "\t", res.args,
+    print "\t", tmp_dir(res.args)
 
 
 print 
@@ -36,12 +38,12 @@ for res in db((db.results_file.run_date >= yesterday) & RUN_CONFIG_SEQ_PATIENT).
     print "\t", "pat-%04d (%s %s)" % (res.patient.id, res.patient.first_name, res.patient.last_name),
     print "\t", tmp_dir(res.scheduler_task.args), ellipsis(res.results_file.data_file)
 
-print 
-print "=== Recent not completed tasks, last %d hours" % LAST_HOURS
 
-for res in db((db.scheduler_task.last_run_time >= yesterday) & (db.scheduler_task.status != 'COMPLETED')).select():
-    print "   ", "sch-%04d" % res.id, "\t", res.status, "   ", "\t", res.start_time, "\t", res.args,
-    print "\t", tmp_dir(res.args)
+print
+print "=== Tables"
+
+for table in db.tables:
+    print "%10s %-20s" % (len(db(db[table]).select()), table)
 
 
-
+print
