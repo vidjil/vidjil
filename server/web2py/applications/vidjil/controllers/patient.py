e84cc19d758a09cd54f62d18f40401f24cf2f099
Vidjil Team <contact@vidjil.org>
2014-12-10 08:22:22 +0100

    patient.py: access 'anon' to the creator of the patient

--- server/web2py/applications/vidjil/controllers/patient.py
+++ server/web2py/applications/vidjil/controllers/patient.py
@@ -216,9 +216,10 @@ def add_form():
             user_group = auth.user_group(auth.user.id)
             admin_group = db(db.auth_group.role=='admin').select().first().id
             
-            #patient creator automaticaly have all rights 
+            #patient creator automaticaly has all rights 
             auth.add_permission(user_group, 'admin', db.patient, id)
             auth.add_permission(user_group, 'read', db.patient, id)
+            auth.add_permission(user_group, 'anon', db.patient, id)
             
             #tmp: share rights with users of the same group
             for g in auth.user_groups :
