4987283a3867994e81ec0d8c4afd2b26280077b5
Mathieu Giraud <giraud@lifl.fr>
2015-01-08 10:28:09 +0100

    scripts/links.py: links to last results for the initial sample by patient

--- server/web2py/applications/vidjil/scripts/links.py
+++ server/web2py/applications/vidjil/scripts/links.py
@@ -18,8 +18,8 @@ for res in db(db.patient.id == db.sequence_file.patient_id).select():
 print "### Results"
 
 
-
-for seq in db(db.sequence_file).select():
+def last_result_by_file():
+  for seq in db(db.sequence_file).select():
     print "===", "seq-%d" % seq.id, "\t", seq.sampling_date, "\t", seq.filename, seq.data_file
     res_with_file = []
 
@@ -32,13 +32,34 @@ for seq in db(db.sequence_file).select():
         continue
 
     # Takes the last result
-    res = res_with_file[-1]
+    yield (res_with_file[-1], seq)
+
+
+def last_result_by_first_point_by_patient():
+    res_by_pat = {}
+
+    for res in db((db.patient.id == db.sequence_file.patient_id)
+                  & (db.results_file.sequence_file_id == db.sequence_file.id)).select(#groupby=db.patient.id,
+                                                                                      orderby=db.patient.id|db.sequence_file.sampling_date|~db.results_file.run_date):
+
+        # Remeber only the first element
+        if not str(res.patient.id) in res_by_pat:
+            res_by_pat[str(res.patient.id)] = (res, res.sequence_file)
+
+    for pat in res_by_pat:
+        yield res_by_pat[pat]
+
+
+
+
+# for (res, seq) in last_result_by_file():
+for (res, seq) in last_result_by_first_point_by_patient():
     our_id = "pat-%04d--%s--sched-%04d--seq-%04d--%s" % (res.patient.id, res.patient.last_name[:3],
                                                          res.results_file.scheduler_task_id,
                                                          seq.id, seq.sampling_date)
 
     print "ln -s %s/%-20s %5s.vidjil" % (defs.DIR_RESULTS, res.results_file.data_file, our_id),
-    print "\t", "# seq-%04d" % seq.id, "%-20s" % seq.filename, "%-10s" % seq.sampling_date
+    print "\t", "# seq-%04d" % seq.id, "%-20s" % seq.filename, "%-10s" % seq.sampling_date,
     print "\t", "# pat-%04d (%s %s)" % (res.patient.id, res.patient.first_name, res.patient.last_name)
 
 
