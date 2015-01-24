171e541bd25826125ff7280d055705ecee617f0e
Mathieu Giraud <giraud@lifl.fr>
2015-01-24 23:42:11 +0100

    create-git-version-h.sh: short date

--- algo/create-git-version-h.sh
+++ algo/create-git-version-h.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-git log -1 --pretty=format:'#define GIT_VERSION "%h (%ci)"' --abbrev-commit > git-version.h 2> /dev/null 
+git log -1 --pretty=format:'#define GIT_VERSION "%h (%cd)"' --date=short --abbrev-commit > git-version.h 2> /dev/null 
 
