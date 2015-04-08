501f720d2051bf1934f481662852a13e3e157819
Mathieu Giraud <mathieu@vidjil.org>
2015-04-08 15:25:07 +0200

    models/db.py: autodelete to False (for maintenance)
    init_from_csv() may remove some uploaded files.
    See https://github.com/web2py/pydal/issues/130

--- server/web2py/applications/vidjil/models/db.py
+++ server/web2py/applications/vidjil/models/db.py
@@ -3,6 +3,8 @@
 import defs
 from gluon import current
 
+AUTODELETE = False
+
 #########################################################################
 ## This scaffolding model makes your app work on Google App Engine too
 ## File is released under public domain and you can use without limitations
@@ -89,8 +91,6 @@ use_janrain(auth, filename='private/janrain.key')
 ## >>> for row in rows: print row.id, row.myfield
 #########################################################################
 
-autodelete = True
-
 db.define_table('patient',
                 Field('first_name','string'),
                 Field('last_name','string'),
@@ -120,7 +120,7 @@ db.define_table('sequence_file',
                 Field('provider','reference auth_user'),
                 Field('data_file', 'upload', 
                       uploadfolder=defs.DIR_SEQUENCES,
-                      length=4294967295, autodelete=autodelete))
+                      length=4294967295, autodelete=AUTODELETE))
 
 
 
@@ -130,7 +130,7 @@ db.define_table('standard_file',
                 Field('info','text'),
                 Field('data_file', 'upload',
                       uploadfolder=defs.DIR_SEQUENCES,
-                      autodelete=autodelete, length=4294967295))
+                      autodelete=AUTODELETE, length=4294967295))
 
 
 
@@ -149,7 +149,7 @@ db.define_table('results_file',
                 Field('scheduler_task_id', 'integer'),
                 Field('data_file', 'upload', 
                       uploadfolder=defs.DIR_RESULTS,
-                      length=4294967295, autodelete=autodelete))
+                      length=4294967295, autodelete=AUTODELETE))
 
 db.define_table('fused_file',
                 Field('patient_id', 'reference patient'),
@@ -159,7 +159,7 @@ db.define_table('fused_file',
                 Field('sequence_file_list', 'string'),
                 Field('fused_file', 'upload', 
                       uploadfolder=defs.DIR_RESULTS,
-                      length=4294967295, autodelete=autodelete))
+                      length=4294967295, autodelete=AUTODELETE))
 
 db.define_table('analysis_file',
                 Field('patient_id', 'reference patient'),
@@ -168,7 +168,7 @@ db.define_table('analysis_file',
                 Field('status', 'string'),
                 Field('analysis_file', 'upload', 
                       uploadfolder=defs.DIR_RESULTS,
-                      length=4294967295, autodelete=autodelete))
+                      length=4294967295, autodelete=AUTODELETE))
 
 
 
