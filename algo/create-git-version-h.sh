ab1e8ea43d23d42ad7163bbaff900f25f86bee4e
Mathieu Giraud <mathieu@vidjil.org>
2015-05-12 11:29:02 +0200

    create-git-version-h.sh: do not overwrite the .h file when the content is the same
    Before this commit, vidjil.o was always recompiled.

--- algo/create-git-version-h.sh
+++ algo/create-git-version-h.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-git log -1 --pretty=format:'#define GIT_VERSION "%h (%cd)"' --date=short --abbrev-commit > git-version.h 2> /dev/null 
+GITV_H=git-version.h
+GITV_H_TMP=git-version.h.tmp
+
+git log -1 --pretty=format:'#define GIT_VERSION "%h (%cd)"' --date=short --abbrev-commit > ${GITV_H_TMP} 2> /dev/null
+
+# Replace the file only when the new file is different
+diff ${GITV_H} ${GITV_H_TMP} || mv ${GITV_H_TMP} ${GITV_H}
 
