c31f2ef04938bf980f3b1b1167a9ca287688a5d7
florian.thonier <florian.thonier@inserm.fr>
2015-05-29 10:05:46 +0200

    germline + segment changements

--- browser/js/clone.js
+++ browser/js/clone.js
@@ -356,11 +356,18 @@ Clone.prototype = {
         }
     },
 
-	getRevCompSequence : function () {
+    getRevCompSequence : function () {
         if (typeof (this.sequence) != 'undefined' && this.sequence != 0){
-            var dict_comp  = {"A":"T","T":"A","C":"G","G":"C"}
+            var dict_comp  = {
+	      'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C',
+	      'Y': 'R', 'R': 'Y', // pyrimidine (CT) / purine (AG)
+	      'W': 'S', 'S': 'W', // weak (AT) / strong (GC)
+	      'K': 'M', 'M': 'K', // keto (TG) / amino (AC)
+	      'B': 'V', 'V': 'B', 'D': 'H', 'H': 'D',
+	      'N': 'N'
+	      }
 			var revcompSeq = ""
-			for (var i = this.sequence.length; i >= 0; i--) {
+			for (var i = this.sequence.length -1 ; i > 0; i--) {
 				revcompSeq += dict_comp[this.sequence[i]]
 			}
 			return revcompSeq
@@ -504,6 +511,86 @@ Clone.prototype = {
         }
     },
     
+	getPosOfthisClone: function (cloneID) {
+		var pos = "unknow";
+		for (var i=0; i< m.clones.length; i++) {
+			if (m.clones[i].id == cloneID) {
+				return i	
+			}
+		}
+		return pos
+	},
+
+    createLocusList: function () {
+    	var list_germline = ["TRA", "TRB", "TRG", "TRD", "IGH", "IGK", "IGL", "VdJa", "TRD+", "IGH+", "IGK+", "undefined"];
+		// TODO ask for text
+		var content = "manual chgmt: <form name='germ'><select NAME='LocusForm' id='germSelector', onChange='m.clones["+ this.getPosOfthisClone(this.id) +"].changeLocus(this.form);'>";
+		content += "<option value="+ this.germline + ">" + this.germline + "</option>";
+		
+		for (var i in germline_data) {
+			if (i.indexOf("_") ==-1 ){
+				content += "<option value=" + i +">" + i + "</option>";
+			}
+		}
+		content += "</select></form>";
+		return content;
+	},
+	changeLocus: function(form) {
+		// TODO add chgmt of germline in data analysis
+		// TODO change the germlines stats 
+		this.germline = form.LocusForm.value;
+		var segments  = ["Vsegment", "Dsegment", "Jsegment"];
+		
+		for (var i = 0; i < segments.length; i++) {
+			var myDiv = document.getElementById('list'+segments[i]);
+			var content = this.createSegmentList(segments[i], form.LocusForm.value);
+			myDiv.innerHTML = content;
+		};
+		m.analysisHasChanged = true;
+	},
+	createSegmentList: function (segment, locus) {
+		var segments = {"Vsegment": ["5", "V"], "Dsegment": ["4", "D"], "Jsegment": ["3", "J"]}
+		var nLocus = locus + segments[segment][1]
+		var content = "<form name="+ segment  +"><select NAME="+segment+" onChange='m.clones["+ this.getPosOfthisClone(this.id) +"].changeSegment(this.form, " + segments[segment][0] + ");'>";
+		// TODO create changeSegment function
+		content += "<option value="+ this.getGene(segments[segment][0]) + ">" + this.getGene(segments[segment][0]) + "</option>";		
+
+		if( typeof(locus) == 'undefined' ){
+			nLocus = this.germline + segments[segment][1]
+		};
+		for (var i in germline) {
+			if (i.indexOf(nLocus) !=-1 ){
+				for (seg in germline[i]) {
+					content += "<option value=" + seg +">" + seg + "</option>";
+				};
+			};
+		};
+		content += "</select></form>";
+		return content;
+	},
+	changeSegment: function (form, segment) {
+		// TODO add chgmt of germline in data analysis
+		var segments = {"5": ["Vsegment", "V", "germlineV"], "4" : ["Dsegment", "D", "germlineD"], "3": ["Jsegment", "J", "germlineJ"]}
+		if ( segment == "5") {
+			this.seg[segment]         = form["Vsegment"].value
+			this.seg[segment+"start"] = 0 //form["Vsegment"].value
+			this.seg[segment+"end"]   = 0 //form["Vsegment"].value			
+		} else if ( segment == "4") {
+			this.seg[segment]         = form["Dsegment"].value
+			this.seg[segment+"start"] = 0 //form["Vsegment"].value
+			this.seg[segment+"end"]   = 0 //form["Vsegment"].value
+		} else if ( segment == "3") {
+			this.seg[segment]         = form["Jsegment"].value
+			this.seg[segment+"start"] = 0 //form["Vsegment"].value
+			this.seg[segment+"end"]   = 0 //form["Vsegment"].value
+		}
+		// TODO : insert real value for stats (start, end, evalue, ...)
+		this.seg["_evalue"]       = 0 //form["Vsegment"].value
+		this.seg["_evalue_left"]  = 0 //form["Vsegment"].value
+		this.seg["_evalue_right"] = 0 //form["Vsegment"].value
+		m.analysisHasChanged = true;
+	}, 
+	
     
     /* return info about a sequence/clone in html 
      *
@@ -613,10 +700,10 @@ Clone.prototype = {
         
         html += "<tr><td> sequence </td><td colspan='" + time_length + "'>" + this.sequence + "</td></tr>"
         html += "<tr><td> id </td><td colspan='" + time_length + "'>" + this.id + "</td></tr>"
-        html += "<tr><td> locus </td><td colspan='" + time_length + "'>" + this.m.systemBox(this.germline).outerHTML + this.germline + "</td></tr>"
-        html += "<tr><td> V gene (or 5') </td><td colspan='" + time_length + "'>" + this.getGene("5") + "</td></tr>"
-        html += "<tr><td> (D gene) </td><td colspan='" + time_length + "'>" + this.getGene("4") + "</td></tr>"
-        html += "<tr><td> J gene (or 3') </td><td colspan='" + time_length + "'>" + this.getGene("3") + "</td></tr>"
+        html += "<tr><td> locus </td><td colspan='" + time_length + "'>" + this.m.systemBox(this.germline).outerHTML + this.germline + "<div id='listLocus' display='inline'>" + this.createLocusList() +"</div></td></tr>"
+        html += "<tr><td> V gene (or 5') </td><td colspan='" + time_length + "'>" + this.getGene("5") + "<div id='listVsegment' display='inline'>" + this.createSegmentList("Vsegment") + "</div></td></tr>"
+        html += "<tr><td> (D gene) </td><td colspan='" + time_length + "'>" + this.getGene("4") + "<div id='listDsegment' display='inline'>" + this.createSegmentList("Dsegment") + "</div></td></tr>"
+        html += "<tr><td> J gene (or 3') </td><td colspan='" + time_length + "'>" + this.getGene("3") + "<div id='listJsegment' display='inline'>" + this.createSegmentList("Jsegment") + "</div></td></tr>"
         
         
         //other info (clntab)
