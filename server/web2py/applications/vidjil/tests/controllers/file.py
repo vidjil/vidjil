841303636ea5cffe4e73713eb3bf3ad3428329be
Mikael Salson <mikael.salson@univ-lille1.fr>
2015-06-12 14:21:42 +0200

    tests/controllers/file.py: test update_name_of_sequence_file() and get_new_uploaded_filename()

--- server/web2py/applications/vidjil/tests/controllers/file.py
+++ server/web2py/applications/vidjil/tests/controllers/file.py
@@ -4,6 +4,7 @@ import unittest
 from gluon.globals import Request, Session, Storage, Response
 from gluon.contrib.test_helpers import form_postvars
 from gluon import current
+import base64
 
 class FileController(unittest.TestCase):
         
@@ -158,9 +159,22 @@ class FileController(unittest.TestCase):
         resp = producer_list()
         self.assertNotEqual(resp.find('"producer":['), -1, "producer_list() doesn't return a valid json")
             
-            
-            
-            
-            
-            
-            
+    def testUpdateNameOfSequenceFile(self):
+        sequence_id = self.createDumbSequenceFile()
+        data_file = db.sequence_file[sequence_id].data_file
+
+        update_name_of_sequence_file(sequence_id, 'toto.txt', 'LICENSE')
+
+        current_sequence = db.sequence_file[sequence_id]
+        self.assertEquals(current_sequence.size_file, os.path.getsize('LICENSE'))
+        self.assertEquals(current_sequence.data_file, 'LICENSE')
+        self.assertEquals(current_sequence.filename, 'toto.txt')
+
+    def testGetNewUploaddedFilename(self):
+        sequence_id = self.createDumbSequenceFile()
+        data_file = db.sequence_file[sequence_id].data_file
+
+        filename = get_new_uploaded_filename(data_file, "truc.def")
+
+        self.assertEquals(filename[-4:], ".def")
+        self.assertTrue(filename.find(base64.b16encode('truc.def').lower() + ".def") > -1)
