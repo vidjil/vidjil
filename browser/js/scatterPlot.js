236f38dfc99b8a520e109238bfb8e0b911dad6f2
Mathieu Giraud <mathieu@vidjil.org>
2015-06-25 18:33:31 +0200

    scatterPlot.js: temporarily hide tsne presets

--- browser/js/scatterPlot.js
+++ browser/js/scatterPlot.js
@@ -238,8 +238,8 @@ function ScatterPlot(id, model) {
     this.preset = { 
         "V/J (genes)" : { "mode": "plot", "x" : "gene_v", "y": "gene_j"},
         "V/J (alleles)" : { "mode": "plot", "x" : "allele_v", "y": "allele_j"},
-        "graph" : { "mode": "plot", "x" : "tsneX", "y": "tsneY"},
-        "graph_by_system" : { "mode": "plot", "x" : "tsneX_system", "y": "tsneY_system"},
+        // "graph" : { "mode": "plot", "x" : "tsneX", "y": "tsneY"},
+        // "graph_by_system" : { "mode": "plot", "x" : "tsneX_system", "y": "tsneY_system"},
         "V/N length" : { "mode": "plot", "x" : "gene_v", "y": "n"},
         "clone length / locus" : { "mode": "plot", "x": "sequenceLength", "y" : "locus"},
         "clone length / GC content " : { "mode": "plot", "x": "sequenceLength", "y" : "GCContent"},
