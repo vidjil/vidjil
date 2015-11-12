f1e4c9150a5469265f54776d39830f578e46ed4b
Ryan Herbert <ryan.herbert@inria.fr>
2015-11-12 10:17:56 +0100

    controllers/proxy.py added controller to serve as proxy for IMGT requests

---
+++ server/web2py/applications/vidjil/controllers/proxy.py
@@ -0,0 +1,24 @@
+import requests
+import gluon.contrib.simplejson
+
+if request.env.http_origin:
+    response.headers['Access-Control-Allow-Origin'] = request.env.http_origin
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+    response.headers['Access-Control-Max-Age'] = 86400
+
+
+def index():
+    return gluon.contrib.simplejson.dumps("index()")
+
+def imgt():
+    if request.env.request_method == "POST":
+        payload = dict(request.post_vars)
+        del payload['Session']
+        response = requests.post("http://www.imgt.org/IMGT_vquest/vquest", data=payload)
+        if response.status_code == requests.codes.ok:
+            return response
+        return gluon.contrib.simplejson.dumps("the site returned an invalid response")
+    return gluon.contrib.simplejson.dumps("improper method")
+   
+
+
