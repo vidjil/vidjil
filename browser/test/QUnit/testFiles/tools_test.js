95be4e0722df675b9ba70bae84098ae793afedce
Francois Dubiez <francois.dubiez@etudiant.univ-lille1.fr>
2016-02-22 10:06:03 +0100

    Updated Qunit test for - tools lib functions tests - highlight menu new options

---
+++ browser/test/QUnit/testFiles/tools_test.js
@@ -0,0 +1,79 @@
+//Framework assertion example
+module("tools", {
+//	setup:function(){alert("setup money individual test");},
+//	teardown:function(){alert("teardown money individual test");}
+});
+
+test("test nth_ocurrence", function () {
+        var str = "needle needle needle needle";
+        var m = nth_ocurrence(str, 'n', 3);
+        ok(m === 14, "Looking for the 3rd occurence of n in " + str);
+        var m = nth_ocurrence(str, 'E', 3);
+        ok(m === false, "search is casesensitive: no E in " + str);
+        var m = nth_ocurrence(str, 'z', 3);
+        ok(m === false, "No z in " + str);
+    }
+);
+
+test("test tsvToArray", function () {
+        var tabstring = "head1\thead2\thead3\thead4\nLine 1 data1\tLine 1 data2\tLine 1 data3\tLine 1 data4\n";
+        tabstring += "Line 2 data1\tLine 2 data2\tLine 2 data3\tLine 2 data4";
+        var aTest = tsvToArray(tabstring);
+        ok(aTest[0].head3 === "Line 1 data3", "Array[0].head3 =='Line 1 data3'");
+        ok(aTest[1].head2 === "Line 2 data2", "Array[1].head2=='Line 2 head2'");
+        ok(typeof(aTest[5]) === 'undefined', "Tis is only a 2 lines array, no line #5");
+    }
+);
+
+asyncTest("processImgtContents(IMGTresponse,tag)", function () {
+        expect(5);
+
+        var xhr = $.ajax({
+                url: 'testFiles/vquest_imgt.html',
+                dataType: 'html'
+            })
+            .done(function (html, status) {
+                ok(html.length > 0, "Test file was injected, html code received");
+                var imgtArray = processImgtContents(html, "pre");
+                ok(imgtArray[0]["Sequence number"] == "1", "first line is sequence 1");
+                ok(imgtArray[0]["CDR3-IMGT"] == "gcggcggaaactc", "CDR3-IMGT's seq 1 is gcggcggaaactc");
+                ok(imgtArray[3]["Sequence number"] == "4", "4th ligne is seq 4");
+                ok(imgtArray.length == 5, "5 sequences were identified");
+                QUnit.start();  // ...tell QUnit you're ready to go
+            });
+
+
+    }
+);
+
+test("computeStartStop(arrayToProcess,sequence)", function () {
+
+        var imgt2displayCheck = {
+            "3'V-REGION": {
+                "seq": "",
+                "tooltip": "Homsap TRGV3*01 F",
+                "start": 161,
+                "stop": 179
+            },
+            "5'J-REGION": {
+                "seq": "",
+                "tooltip": "Homsap TRGJP2*01 F",
+                "start": 185,
+                "stop": 203
+            },
+            "CDR3-IMGT": {
+                "seq": "",
+                "tooltip": "CDR3-IMGT",
+                "start": 164,
+                "stop": 200
+            }
+
+        };
+
+
+        var imgt2display = computeStartStop(json_data.clones[3].seg.imgt, json_data.clones[3].sequence);
+        ok(typeof imgt2display != 'undefined', "function returned a not null array");
+        deepEqual(imgt2display, imgt2displayCheck, "Processed array is similar to expected array");
+
+    }
+);
