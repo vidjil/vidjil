b1e7a124e417e353465b1bda6b847637e3ca4d0a
Mathieu Giraud <mathieu@vidjil.org>
2016-03-10 11:31:34 +0100

    js/database.js: DB_TIMEOUT_CALL to 10s

--- browser/js/database.js
+++ browser/js/database.js
@@ -1,5 +1,5 @@
 DB_ADDRESS = ""
-DB_TIMEOUT_CALL = 5000                // Regular call
+DB_TIMEOUT_CALL = 10000               // Regular call
 DB_TIMEOUT_GET_DATA = 15000           // Get patient/sample .data
 DB_TIMEOUT_GET_CUSTOM_DATA = 1200000  // Launch custum fused sample .data
 NOTIFICATION_PERIOD = 30000			  // Time interval to check for notifications periodically
