8dc9cd77fccfaeea1800b2f8e6cb165041227096
Ryan Herbert <ryan.herbert@inria.fr>
2016-03-17 17:22:03 +0100

    crossDomain.js isolated form input variables into init functions to prevent cross-contamination between calls

--- browser/js/crossDomain.js
+++ browser/js/crossDomain.js
@@ -1,89 +1,95 @@
 //parametre IMGT par defaut
-var imgtInput = {};
-imgtInput["callback"] = "jQuery17106713638880755752_1378825832820";
-imgtInput["livret"] = "1";
-imgtInput["Session"] = "&lt;session code=Â¤0Â¤ appliName=Â¤IMGTvquestÂ¤ time=Â¤3625396897Â¤/&gt;";
-imgtInput["l01p01c02"] = "Homo sapiens";
-imgtInput["l01p01c04"] = "TR";
-imgtInput["l01p01c03"] = "inline";
-imgtInput["l01p01c10"] = "";
-imgtInput["l01p01c07"] = "2. Synthesis";
-imgtInput["l01p01c05"] = "HTML";
-imgtInput["l01p01c09"] = "60";
-imgtInput["l01p01c60"] = "5";
-imgtInput["l01p01c12"] = "Y";
-imgtInput["l01p01c13"] = "Y";
-imgtInput["l01p01c06"] = "Y";
-imgtInput["l01p01c24"] = "N";
-imgtInput["l01p01c14"] = "Y";
-imgtInput["l01p01c15"] = "Y";
-imgtInput["l01p01c16"] = "Y";
-imgtInput["l01p01c41"] = "Y";
-imgtInput["l01p01c22"] = "Y";
-imgtInput["l01p01c17"] = "Y";
-imgtInput["l01p01c23"] = "Y";
-imgtInput["l01p01c19"] = "Y";
-imgtInput["l01p01c18"] = "Y";
-imgtInput["l01p01c20"] = "Y";
-imgtInput["l01p01c27"] = "Y";
-imgtInput["l01p01c28"] = "Y";
-imgtInput["l01p01c29"] = "Y";
-imgtInput["l01p01c30"] = "Y";
-imgtInput["l01p01c31"] = "Y";
-imgtInput["l01p01c32"] = "Y";
-imgtInput["l01p01c33"] = "Y";
-imgtInput["l01p01c34"] = "Y";
-imgtInput["l01p01c46"] = "N";
-imgtInput["l01p01c47"] = "N";
-imgtInput["l01p01c48"] = "N";
-imgtInput["l01p01c49"] = "N";
-imgtInput["l01p01c50"] = "Y";
-imgtInput["l01p01c51"] = "N";
-imgtInput["l01p01c52"] = "N";
-imgtInput["l01p01c53"] = "N";
-imgtInput["l01p01c54"] = "N";
-imgtInput["l01p01c55"] = "NO";
-imgtInput["l01p01c35"] = "F+ORF+ in-frame P";
-imgtInput["l01p01c36"] = "0";
-imgtInput["l01p01c40"] = "1";
-imgtInput["l01p01c25"] = "default";
-imgtInput["l01p01c37"] = "default";
-imgtInput["l01p01c38"] = "default";
-imgtInput["l01p01c39"] = "default";
-imgtInput["l01p01c08"] = "";
-imgtInput["l01p01c26"] = "";
-imgtInput["l01p01c10"] = ">a\nATGCGCAGATGC\n";
+function initImgtInput() {
+    var imgtInput = {};
+    imgtInput["callback"] = "jQuery17106713638880755752_1378825832820";
+    imgtInput["livret"] = "1";
+    imgtInput["Session"] = "&lt;session code=Â¤0Â¤ appliName=Â¤IMGTvquestÂ¤ time=Â¤3625396897Â¤/&gt;";
+    imgtInput["l01p01c02"] = "Homo sapiens";
+    imgtInput["l01p01c04"] = "TR";
+    imgtInput["l01p01c03"] = "inline";
+    imgtInput["l01p01c10"] = "";
+    imgtInput["l01p01c07"] = "2. Synthesis";
+    imgtInput["l01p01c05"] = "HTML";
+    imgtInput["l01p01c09"] = "60";
+    imgtInput["l01p01c60"] = "5";
+    imgtInput["l01p01c12"] = "Y";
+    imgtInput["l01p01c13"] = "Y";
+    imgtInput["l01p01c06"] = "Y";
+    imgtInput["l01p01c24"] = "N";
+    imgtInput["l01p01c14"] = "Y";
+    imgtInput["l01p01c15"] = "Y";
+    imgtInput["l01p01c16"] = "Y";
+    imgtInput["l01p01c41"] = "Y";
+    imgtInput["l01p01c22"] = "Y";
+    imgtInput["l01p01c17"] = "Y";
+    imgtInput["l01p01c23"] = "Y";
+    imgtInput["l01p01c19"] = "Y";
+    imgtInput["l01p01c18"] = "Y";
+    imgtInput["l01p01c20"] = "Y";
+    imgtInput["l01p01c27"] = "Y";
+    imgtInput["l01p01c28"] = "Y";
+    imgtInput["l01p01c29"] = "Y";
+    imgtInput["l01p01c30"] = "Y";
+    imgtInput["l01p01c31"] = "Y";
+    imgtInput["l01p01c32"] = "Y";
+    imgtInput["l01p01c33"] = "Y";
+    imgtInput["l01p01c34"] = "Y";
+    imgtInput["l01p01c46"] = "N";
+    imgtInput["l01p01c47"] = "N";
+    imgtInput["l01p01c48"] = "N";
+    imgtInput["l01p01c49"] = "N";
+    imgtInput["l01p01c50"] = "Y";
+    imgtInput["l01p01c51"] = "N";
+    imgtInput["l01p01c52"] = "N";
+    imgtInput["l01p01c53"] = "N";
+    imgtInput["l01p01c54"] = "N";
+    imgtInput["l01p01c55"] = "NO";
+    imgtInput["l01p01c35"] = "F+ORF+ in-frame P";
+    imgtInput["l01p01c36"] = "0";
+    imgtInput["l01p01c40"] = "1";
+    imgtInput["l01p01c25"] = "default";
+    imgtInput["l01p01c37"] = "default";
+    imgtInput["l01p01c38"] = "default";
+    imgtInput["l01p01c39"] = "default";
+    imgtInput["l01p01c08"] = "";
+    imgtInput["l01p01c26"] = "";
+    imgtInput["l01p01c10"] = ">a\nATGCGCAGATGC\n";
+    return imgtInput;
+}
 
 //parametre igBlast par defaut
-var igBlastInput = {};
-igBlastInput["queryseq"] = "GAAGGCCCCACAGCGTCTTCTGTACTATGACGTCTCCACCGCAAGGGATGTGTTGGAATCAGGACTCAGTCCAGGAAAGTATTATACTCATACACCCAGGAGGTGGAGCTGGATATTGAGACTGCAAAATCTAATTGAAAATGATTCTGGGGTCTATTACTGTGCCACCTTCTGACATAAGAAACCCTTTGGCAGTGGAACAACAC"
-igBlastInput["organism"] = "human"
-igBlastInput["germline_db_V"] = "IG_DB/imgt.TR.Homo_sapiens.V.f.orf.p";
-igBlastInput["germline_db_D"] = "IG_DB/imgt.TR.Homo_sapiens.D.f.orf";
-igBlastInput["germline_db_J"] = "IG_DB/imgt.TR.Homo_sapiens.J.f.orf.p";
-igBlastInput["program"] = "blastn";
-igBlastInput["min_D_match"] = 5;
-igBlastInput["D_penalty"] = -4;
-igBlastInput["num_alignments_V"] = 3;
-igBlastInput["num_alignments_D"] = 3;
-igBlastInput["num_alignments_J"] = 3;
-igBlastInput["translation="] = true;
-igBlastInput["domain"] = "imgt";
-igBlastInput["outfmt"] = 3;
-igBlastInput["additional_db"] = "";
-igBlastInput["v_focus"] = true;
-igBlastInput["num_alignments_additional"] = 10;
-igBlastInput["evalue"] = 1;
-igBlastInput["LINK_LOC"] = "";
-igBlastInput["SEARCH_TYPE"] = "TCR";
-igBlastInput["igsource"] = "new";
-igBlastInput["analyze"] = "on";
-igBlastInput["CMD"] = "request";
-igBlastInput["seqtype"] = "TCR";
+function initIgBlastInput() {
+    var igBlastInput = {};
+    igBlastInput["queryseq"] = "GAAGGCCCCACAGCGTCTTCTGTACTATGACGTCTCCACCGCAAGGGATGTGTTGGAATCAGGACTCAGTCCAGGAAAGTATTATACTCATACACCCAGGAGGTGGAGCTGGATATTGAGACTGCAAAATCTAATTGAAAATGATTCTGGGGTCTATTACTGTGCCACCTTCTGACATAAGAAACCCTTTGGCAGTGGAACAACAC"
+    igBlastInput["organism"] = "human"
+    igBlastInput["germline_db_V"] = "IG_DB/imgt.TR.Homo_sapiens.V.f.orf.p";
+    igBlastInput["germline_db_D"] = "IG_DB/imgt.TR.Homo_sapiens.D.f.orf";
+    igBlastInput["germline_db_J"] = "IG_DB/imgt.TR.Homo_sapiens.J.f.orf.p";
+    igBlastInput["program"] = "blastn";
+    igBlastInput["min_D_match"] = 5;
+    igBlastInput["D_penalty"] = -4;
+    igBlastInput["num_alignments_V"] = 3;
+    igBlastInput["num_alignments_D"] = 3;
+    igBlastInput["num_alignments_J"] = 3;
+    igBlastInput["translation="] = true;
+    igBlastInput["domain"] = "imgt";
+    igBlastInput["outfmt"] = 3;
+    igBlastInput["additional_db"] = "";
+    igBlastInput["v_focus"] = true;
+    igBlastInput["num_alignments_additional"] = 10;
+    igBlastInput["evalue"] = 1;
+    igBlastInput["LINK_LOC"] = "";
+    igBlastInput["SEARCH_TYPE"] = "TCR";
+    igBlastInput["igsource"] = "new";
+    igBlastInput["analyze"] = "on";
+    igBlastInput["CMD"] = "request";
+    igBlastInput["seqtype"] = "TCR";
+    return igBlastInput;
+}
 
 
 function imgtPost(data, system) {
-
+    var imgtInput = initImgtInput();
     imgtInput["l01p01c10"] = data;
     if (system[0] == "I") {
         imgtInput["l01p01c04"] = "IG";
@@ -118,7 +124,7 @@ function imgtPost(data, system) {
  * @param system
  */
 function imgtPostForSegmenter(data, system) {
-
+    var imgtInput = initImgtInput();
     var imgt4segButton= document.getElementById("toIMGTSeg");
     //limit #request to #
     var pos, nb = 1;
@@ -289,7 +295,7 @@ function imgtPostForSegmenter(data, system) {
 
 
 function igBlastPost(data, system) {
-
+    var igBlastInput = initIgBlastInput();
     igBlastInput["queryseq"] = data;
     if (system[0] == "I") {
         igBlastInput["germline_db_V"] = "IG_DB/imgt.Homo_sapiens.V.f.orf.p";
