81e90b05adf4ad00c9ac80bb95c1b0c8962402bd
Ryan Herbert <ryan.herbert@inria.fr>
2016-03-22 11:23:50 +0100

    builder.js js server logging for builder

--- browser/js/builder.js
+++ browser/js/builder.js
@@ -20,38 +20,45 @@ Builder.prototype = {
 
     init: function () {
         var self = this;
+        try {
+            d3.select("#visu-separator")
+                .on("mousedown", function () {
+                    self.dragSeparator();
+                });
+            d3.select("#visu-container")
+                .on("mouseup", function () {
+                    self.dropSeparator()
+                })
+            d3.select("#visu-container")
+                .on("mousemove", function () {
+                    self.updateSeparator()
+                })
+            d3.select("#vertical-separator")
+                .on("click", function () {
+                    self.toggle_left_container()
+                });
 
-        d3.select("#visu-separator")
-            .on("mousedown", function () {
-                self.dragSeparator();
-            });
-        d3.select("#visu-container")
-            .on("mouseup", function () {
-                self.dropSeparator()
-            })
-        d3.select("#visu-container")
-            .on("mousemove", function () {
-                self.updateSeparator()
-            })
-        d3.select("#vertical-separator")
-            .on("click", function () {
-                self.toggle_left_container()
-            });
+            this.build_top_container()
+            this.build_info_container()
+            this.build_clusterSelector()
+            this.initTag();
 
-        this.build_top_container()
-        this.build_info_container()
-        this.build_clusterSelector()
-        this.initTag();
-        
-        if (this.m.samples.order.length == 1) this.resizeGraph(0) 
-        else this.resizeGraph(50) 
+            if (this.m.samples.order.length == 1) this.resizeGraph(0) 
+            else this.resizeGraph(50)
+
+        } catch (err) {
+            this.db.error(err.stack)
+        }
     },
 
     update: function () {
-
-        this.build_top_container()
-        this.build_info_container()
-        this.updateTagBox();
+        try {
+            this.build_top_container()
+            this.build_info_container()
+            this.updateTagBox();
+        } catch(err) {
+            this.db.error(err.stack)
+        }
     },
     
     updateElem: function () {},
