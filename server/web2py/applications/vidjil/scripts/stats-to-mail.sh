086dac2a29339075d0428412b993dd2eed05e131
Vidjil Team <contact@vidjil.org>
2016-04-04 19:00:00 +0200

    scripts/stats.sh, scripts/stats-to-mail.sh: scripts launched by www-data

--- server/web2py/applications/vidjil/scripts/stats-to-mail.sh
+++ server/web2py/applications/vidjil/scripts/stats-to-mail.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-(echo "Subject: [rbx] Vidjil Morning" ; sh stats.sh ) | sendmail notifications@vidjil.org
+(echo "Subject: [rbx] Vidjil Morning" ; sh stats.sh ) | /usr/sbin/sendmail notifications@vidjil.org
 
 
--- server/web2py/applications/vidjil/scripts/stats.sh
+++ server/web2py/applications/vidjil/scripts/stats.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-sudo -u www-data python ../../../web2py.py -S vidjil -M -R applications/vidjil/scripts//db-stats.py 2> /dev/null
+python ../../../web2py.py -S vidjil -M -R applications/vidjil/scripts//db-stats.py 2> /dev/null
 
 echo -n "=== Server health"
-/etc/update-motd.d/50-landscape-sysinfo
+# /etc/update-motd.d/50-landscape-sysinfo
 /etc/update-motd.d/60-df
 /etc/update-motd.d/90-updates-available
 /etc/update-motd.d/98-reboot-required 
