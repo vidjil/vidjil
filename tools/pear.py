097cd37548c5faad1ccd2262a1fe695920fd601e
Marc Duez <marc7duez@gmail.com>
2016-04-13 16:52:40 +0100

    tools/pear.py: add small script for pear pre_process

---
+++ tools/pear.py
@@ -0,0 +1,42 @@
+from os import listdir, path, makedirs
+from os.path import isfile, join
+import sys
+import shutil
+import gzip
+import subprocess
+import argparse
+
+parser = argparse.ArgumentParser(description='Use pear read merger to make a new fastq file and keep unmerged reads')
+
+parser.add_argument("file_R1", help="reverse read file")
+parser.add_argument("file_R2", help="forward read file")
+parser.add_argument("output_file", help="output file")
+parser.add_argument("-r1", "--keep_r1", help="keep unmerged reverse reads", action="store_true")
+parser.add_argument("-r2", "--keep_r2", help="keep unmerged forward reads", action="store_true")
+
+
+args = parser.parse_args()
+f_r1 = args.file_R1
+f_r2 = args.file_R2
+f_out = args.output_file
+
+
+subprocess.call(["pear",
+ "-f", f_r1,
+ "-r", f_r2,
+ "-o", f_out,
+ "-n", "100",
+ "-m", "490",
+ "-j", "3"])
+
+
+
+with gzip.open(f_out, 'w') as outFile:
+    with open(f_out+'.assembled.fastq', 'rb') as f1:
+        shutil.copyfileobj(f1, outFile)
+    if (args.keep_r1):
+        with open(f_out+'.unassembled.reverse.fastq', 'rb') as f2:
+            shutil.copyfileobj(f2, outFile)
+    if (args.keep_r2):
+        with open(f_out+'.unassembled.forward.fastq', 'rb') as f3:
+            shutil.copyfileobj(f3, outFile)
