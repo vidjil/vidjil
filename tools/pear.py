c2d34887768313ce09e6fdf042cc57125dcd6cdf
Mathieu Giraud <mathieu@vidjil.org>
2016-04-21 13:23:43 +0200

    pear.py: remove hard-coded options, add --pear-options
    Specific PEAR options should be configured through pre-process configs.

--- tools/pear.py
+++ tools/pear.py
@@ -5,14 +5,16 @@ import shutil
 import gzip
 import subprocess
 import argparse
+import shlex
 
-parser = argparse.ArgumentParser(description='Use pear read merger to make a new fastq file and keep unmerged reads')
+parser = argparse.ArgumentParser(description='Use PEAR read merger to make a new fastq file and keep unmerged reads')
 
 parser.add_argument("file_R1", help="reverse read file")
 parser.add_argument("file_R2", help="forward read file")
 parser.add_argument("output_file", help="output file")
 parser.add_argument("-r1", "--keep_r1", help="keep unmerged reverse reads", action="store_true")
 parser.add_argument("-r2", "--keep_r2", help="keep unmerged forward reads", action="store_true")
+parser.add_argument("-p", "--pear-options", help="additional options passed to PEAR", default="")
 
 
 args = parser.parse_args()
@@ -25,9 +27,9 @@ subprocess.call(["pear",
  "-f", f_r1,
  "-r", f_r2,
  "-o", f_out,
- "-n", "100",
- "-m", "490",
- "-j", "1"])
+ "-j", "1"]
+                + shlex.split(args.pear_options)
+)
 
 
 
