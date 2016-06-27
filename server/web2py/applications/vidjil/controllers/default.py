7c3321662434c24533dff1efaa4e9b6be055b0c2
Ryan Herbert <ryan.herbert@inria.fr>
2016-06-27 14:25:35 +0200

    Log when saving analysis

--- server/web2py/applications/vidjil/controllers/default.py
+++ server/web2py/applications/vidjil/controllers/default.py
@@ -432,11 +432,18 @@ def get_analysis():
 # need patient admin permission
 def save_analysis():
     error = ""
+
+    initial_id = request.vars["sample_set_id"] if "sample_set_id" in request.vars else None
+    initial_type = 'sample_set'
     if "patient" in request.vars :
         request.vars["sample_set_id"] = db.patient[request.vars["patient"]].sample_set_id
+        initial_id = request.vars['patient']
+        initial_type = 'patient'
 
     if "run" in request.vars :
         request.vars["sample_set_id"] = db.run[request.vars["run"]].sample_set_id
+        initial_id = request.vars["run"]
+        initial_type = "run"
 
     if not "sample_set_id" in request.vars :
         error += "It is currently not possible to save an analysis on a comparison of samples, "
@@ -476,7 +483,7 @@ def save_analysis():
 
         res = {"success" : "true",
                "message" : "(%s): analysis saved" % (sample_set_id)}
-        log.info(res)
+        log.info(res, extra={'user_id': auth.user.id, 'record_id': initial_id, 'table_name': initial_type})
         return gluon.contrib.simplejson.dumps(res, separators=(',',':'))
     else :
         res = {"success" : "false",
