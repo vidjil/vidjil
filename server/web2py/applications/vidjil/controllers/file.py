b035dd952c8e110ff4bebeba4abed58d2a239b81
Ryan Herbert <ryan.herbert@inria.fr>
2016-07-08 15:33:47 +0200

    file.py fix undefined patient_sample_set_id

--- server/web2py/applications/vidjil/controllers/file.py
+++ server/web2py/applications/vidjil/controllers/file.py
@@ -160,9 +160,10 @@ def add_form():
             id_sample_set_membership_patient = db.sample_set_membership.insert(sample_set_id=patient_sample_set_id,
                                                                   sequence_file_id=id)
 
-        originating_id = patient_sample_set_id
         if request.vars['sample_type'] == 'run':
             originating_id = run_sample_set_id
+        else:
+            originating_id = patient_sample_set_id
 
         redirect_args = {"id" : originating_id}
         
