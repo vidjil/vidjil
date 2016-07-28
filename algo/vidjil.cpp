e6918bd2318125c57700cdb0da0a0487b11bcb83
Mathieu Giraud <mathieu@vidjil.org>
2016-07-28 08:56:53 +0200

    vidjil.cpp, tests: json output, do not output 'quality' when there is no quality

--- algo/vidjil.cpp
+++ algo/vidjil.cpp
@@ -1356,6 +1356,8 @@ int main (int argc, char **argv)
         json_clone["_coverage_info"] = {repComp.getCoverageInfo()};
         //From KmerMultiSegmenter
         json_clone["seg"] = kseg->toJson();
+
+        if (repComp.getQuality().length())
         json_clone["seg"]["quality"] = {
             {"start", 1},
             {"stop", kseg->getSequence().sequence.length()},
