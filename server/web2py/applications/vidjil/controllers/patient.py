daaf1ae3acc341272b734dd37fde03d771ef1f91
Ryan Herbert <ryan.herbert@inria.fr>
2016-08-17 09:30:13 +0200

    patient.py fix default selected group on create
    This hotfix resolves an issue with default group selection when creating
    a new patient.
    
    This issue affected users that belonged to multiple groups but did not
    have creation permissions on all groups.
    
    The default user group is now also only displayed if the user has create
    permissions on that group.

--- server/web2py/applications/vidjil/controllers/patient.py
+++ server/web2py/applications/vidjil/controllers/patient.py
@@ -253,23 +253,34 @@ def index():
 def add(): 
     if (auth.can_create_patient()):
         max_group = auth.user_group()
-        groups = []
+        group_dict = {}
         if (auth.is_admin()):
             group_list = db(db.auth_group).select(db.auth_group.id, db.auth_group.role)
         else:
-            groups.append({'name': 'Personnal Group', 'id': auth.user_group()})
-            group_list = auth.get_user_group_parents()
+            group_list = db((db.auth_group.id == db.auth_membership.group_id) &
+                            (db.auth_membership.user_id == auth.user_id) &
+                            (db.auth_group.role != 'public')
+                        ).select(db.auth_group.id, db.auth_group.role)
         max_elements = 0
         for group in group_list:
-            groups.append({'id': group.id, 'name': group.role})
-            elements = db(
-                (db.auth_permission.group_id == group.id) &
-                (db.auth_permission.table_name.belongs('patient', 'run', 'sample_set')) &
-                (db.auth_permission.record_id > 0)
-            ).count()
-            if (elements > max_elements):
-                max_elements = elements
-                max_group = group.id
+            if (auth.is_admin()
+                    or auth.has_permission(PermissionEnum.create.value, 'sample_set', 0, group_id = group.id)):
+                parents = auth.get_group_parent(group.id)
+                if (len(parents) > 0):
+                    tgt_group = parents[0]
+                else:
+                    tgt_group = group
+                group_dict[tgt_group.id] = {'id': tgt_group.id,
+                        'name': 'Personnal Group' if tgt_group.id == auth.user_group() else tgt_group.role}
+                elements = db(
+                    (db.auth_permission.group_id == tgt_group.id) &
+                    (db.auth_permission.table_name.belongs('patient', 'run', 'sample_set')) &
+                    (db.auth_permission.record_id > 0)
+                ).count()
+                if (elements >= max_elements and tgt_group.id != auth.user_group()):
+                    max_elements = elements
+                    max_group = tgt_group.id
+        groups = [{'id': group_dict[key]['id'], 'name': group_dict[key]['name']} for key in group_dict]
         return dict(message=T('add patient'), groups=groups, master_group=max_group)
     else :
         res = {"message": ACCESS_DENIED}
