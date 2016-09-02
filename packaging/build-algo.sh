61a9b712fcd9be4750f33429fcd84606363aaecb
Ryan Herbert <ryan.herbert@inria.fr>
2016-09-02 13:35:46 +0200

    Packaging vidjil algo automation
    Added automation scripts for building the vidjil algorithm debian
    package

---
+++ packaging/build-algo.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+. ./build-generic.sh
+
+MY_PWD="$PWD"
+
+source_files="../algo ../browser ../data ../doc ../germline ../Makefile ../tools"
+dest_files="vidjil"
+exec_dir="$dest_files"
+
+copy_files -s="$source_files" -d="$dest_files"
+
+cd "$MY_PWD/$dest_files/germline"
+make
+cd "$MY_PWD/$dest_files/data"
+
+cd "$MY_PWD/$exec_dir"
+bash "$MY_PWD/mkdeb"
+
+remove_files -s="$source_files" -d="$dest_files"
+exit 0
