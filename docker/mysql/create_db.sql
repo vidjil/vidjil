b12531e4e82bb9b5b020f9d40491abf96bec9bca
Ryan Herbert <ryan.herbert@inria.fr>
2016-10-13 17:10:54 +0200

    docker create_db.sql fix error in script

--- docker/mysql/create_db.sql
+++ docker/mysql/create_db.sql
@@ -1,4 +1,4 @@
 CREATE USER vidjil_user;
-SET PASSWORD FOR USER vidjil_user = PASSWORD('rootpass');
+SET PASSWORD FOR vidjil_user = PASSWORD('rootpass');
 CREATE DATABASE vidjil;
 GRANT ALL ON vidjil.* TO 'vidjil_user'@'%'; 
