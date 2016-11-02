f75c59627fb38f528ec73db782bc927d50a368f2
Mathieu Giraud <mathieu@vidjil.org>
2016-11-03 00:51:46 +0100

    algo/create-git-version-h.sh: do not complain when the .h file is not present
    This is the case when compiling Vidjil from a fresh clone.

--- algo/create-git-version-h.sh
+++ algo/create-git-version-h.sh
@@ -3,6 +3,8 @@
 GITV_H=git-version.h
 GITV_H_TMP=git-version.h.tmp
 
+touch ${GITV_H}
+
 git log -1 --pretty=format:'#define GIT_VERSION "%h (%cd)"' --date=short --abbrev-commit > ${GITV_H_TMP} 2> /dev/null
 
 # Replace the file only when the new file is different
