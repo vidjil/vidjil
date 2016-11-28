de13ed83f66d5c8db070fc4ed3e88de2206bd4a2
Armand (Tydax) Bour <tydaxx@gmail.com>
2016-11-28 16:48:24 +0100

    Update design of segmenter_page

--- browser/segmenter_page.js
+++ browser/segmenter_page.js
@@ -53,6 +53,19 @@ function loadVidjilFile() {
     return vidjilObj;
 }
 
+function displayVidjilViews() {
+  var views = document.getElementsByClassName('vidjil_view');
+
+  for (var i = 0; i < views.length; i++) {
+    var view = views[i];
+    console.log(views.length);
+    view.style.display = 'block';
+  }
+
+  var formPanel = document.getElementById('form_panel');
+  formPanel.className = 'panel twopanels_left';
+}
+
 function main() {
   // Get DOM elements
   var submitNode = document.getElementById("form_submit");
@@ -61,10 +74,11 @@ function main() {
   var model = new Model();
   // model.loadGermline();
   var segmenter = new Segment("segmenter_panel", model);
-
+  var scatter = new ScatterPlot("scatter_panel", model);
 
   // Parse sequences and add to segmenter
   submitNode.addEventListener("click", function () {
+    displayVidjilViews();
     var vidjil = loadVidjilFile();
     model.parseJsonData(vidjil, 100);
     model.initClones();
