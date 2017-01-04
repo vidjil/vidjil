1012e5fde0001116f0558596c77061ed70390a24
RyanHerb <rya.herb@gmail.com>
2017-01-04 11:45:01 +0100

    uwsgi-entrypoint.sh fix error in file paths

--- docker/vidjil/scripts/uwsgi-entrypoint.sh
+++ docker/vidjil/scripts/uwsgi-entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chown -R www-data:www-data /mnt/results
-chown -R www-data:www-data /mnt/uploads
+chown -R www-data:www-data /mnt/result
+chown -R www-data:www-data /mnt/upload
 chown -R www-data:www-data /usr/share/vidjil/server/web2py/applications/vidjil/databases
 /usr/local/bin/gosu www-data /usr/bin/uwsgi --ini /etc/uwsgi/apps-enabled/web2py.ini
