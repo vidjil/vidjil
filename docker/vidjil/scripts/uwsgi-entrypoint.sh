055b45da43054aa1953b2a1630b2b721faaa2642
Ryan Herbert <ryan@vidjil.org>
2017-01-18 17:46:37 +0100

    docker/vidjil/Dockerfile recreate vidjil log files
    vidjil log files were being removed by dockre volumes. recreating them
    in the uwsgi entrypoint script allows us to ensure they exist

--- docker/vidjil/scripts/uwsgi-entrypoint.sh
+++ docker/vidjil/scripts/uwsgi-entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+touch /var/vidjil/vidjil.log
+touch /var/vidjil/vidjil-debug.log
+chown -R www-data:www-data /var/vidjil/vidjil*
 chown -R www-data:www-data /mnt/result
 chown -R www-data:www-data /mnt/upload
 chown -R www-data:www-data /usr/share/vidjil/server/web2py/applications/vidjil/databases
