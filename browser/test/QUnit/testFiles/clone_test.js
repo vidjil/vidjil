505386fe5a77525e9d39f7d681ebdd36e16f53d1
Mathieu Giraud <mathieu@vidjil.org>
2017-02-18 11:26:25 +0100

    tests: move QUnit.assert.includes into a new file 'qunit-vidjil.js'
    We may use 'includes' in other tests.

--- browser/test/QUnit/testFiles/clone_test.js
+++ browser/test/QUnit/testFiles/clone_test.js
@@ -61,15 +61,6 @@
         }
     }
 
-
-QUnit.assert.includes = function(result, pattern, message ) {
-    // Checks that the result includes the pattern
-    // TODO: see and use qunit-regexp !
-    var res = result.indexOf(pattern) > -1
-
-    this.push(res, res, "{includes} " + pattern, message);
-}
-
 QUnit.test("clone : name, informations, getHtmlInfo", function(assert) {
     
     var m = new Model();
