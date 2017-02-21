fd4fa1eff1056aae2e5aeda64684cc6ae92b616f
Mathieu Giraud <mathieu@vidjil.org>
2017-02-21 12:55:20 +0100

    js/speed_test.js: dataWithManyClones()
    See #2196.

--- browser/js/speed_test.js
+++ browser/js/speed_test.js
@@ -1,5 +1,39 @@
 
 
+function dataWithManyClones(many) {
+
+    // json_data from test/QUnit/testFiles/data_test
+
+    json_data.clones = []
+
+    // Fill some clones
+    for (var i = 0; i < many; i += 1)
+    {
+        var seg5 = "V-" + (i % 10)
+        var seg3 = "J-" + (Math.round(i/10) % 5)
+        var clone =
+            {
+                "sequence" : "acgtacgtactgtacgt",
+                "name" : "test-" + i + " " + seg5 + " " + seg3,
+                "id" : "test-" + i,
+                "reads" : [i, i*2, i, i*2],
+                "top" : i,
+                "germline" : "TRG",
+                "seg" : {
+                    "5" : seg5,
+                    "3" : seg3,
+                    "3start" : 5,
+                    "5end" : 10,
+                }
+            }
+
+        json_data['clones'].push(clone);
+    }
+
+    return json_data
+}
+
+
 function speedTest(m) {
 
     var startTime = new Date()
@@ -9,14 +43,13 @@ function speedTest(m) {
     var log = '';
     
     for (var i = 20; i < 101; i = i + 20) {
-                
-        log += "</br> load top " + i + "</br>";
-        
+
+        log += "<br/> ==== .parseJsonData, " + i + " clones <br />"
         startTime = new Date()
             .getTime();
         elapsedTime = 0;
                 
-        m.parseJsonData(json_data, i)
+        m.parseJsonData(dataWithManyClones(100), i)
             .loadGermline();
         m.initClones()
         
@@ -38,10 +71,10 @@ function speedTest(m) {
             startTime = new Date()
                 .getTime();
             elapsedTime = 0;
-            m.updateElem([0, 2, 3]); // , 5, 8, 12]);
+            m.updateElem([1, 2, 3, 5, 8, 13, 14, 15, 16, 17]);
             elapsedTime = new Date()
                 .getTime() - startTime;
-            log += "  // 5 clones update time (display :" + j + ") : " + elapsedTime + "ms</br>";
+            log += "  //10 clones update time (display :" + j + ") : " + elapsedTime + "ms</br>";
             
         }
     }
