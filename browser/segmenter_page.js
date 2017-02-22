bbf6ec69736949d90df659d0fa15a15aa7dbfce2
Armand (Tydax) Bour <tydaxx@gmail.com>
2017-02-22 16:29:52 +0100

    segmenter_page.js: Remove example
    We'll use the server response instead

--- browser/segmenter_page.js
+++ browser/segmenter_page.js
@@ -1,56 +1,8 @@
 /**
-* TODO: Implement this function
-* Loads the .vidjil file produced by the program.
-* @return {object} - the vidjil object
-**/
-function loadVidjilFile() {
-  var vidjilObj = {
-    "vidjil_json_version" : "2014.10",
 
-    "samples" :  {
-  	"number" : 1,
-  	"original_names" :  [ "/some/file" ] ,
-  	"run_timestamp" :  [ "2015-02-19 16:37:06" ] ,
-  	"producer" :  [ "vidjil dev 0cf35de (2015-02-17)" ] ,
-  	"log" :  [ "Some log" ],
-  	"commandline" :  [ "Some commandline" ]
-    } ,
 
-    "reads" :  {
-    	"total" :  [ 2000 ] ,
-    	"segmented" :  [ 1000 ] ,
-    	"germline" :  {
-    	    "IGH" :  [ 950 ]
-    	}
-    },
-      "clones" :  [{
-  	    "id" : "id-1",
-  	    "name": "name-1",
-    "sequence": "ACGCTGGACGACTTGTATCTCTGTGCCAGCCGACCCGAAAAAGACAGTAGAAACTAATGAAAAACTGTTTTTTGGCAGT",
 
-  	    "reads" :  [ 800 ] ,
-  	    "top" : 1,
-  	    "germline" : "IGH"
-  	}, {
-  	    "id" : "id-2",
-  	    "name" : "name-2",
-    "sequence" : "AGCTGGACGACTCGGCCCTGTATCTCTGTGCCAGCACCCGAGGGGACAGTAGAAACTAATGAAAAACTGTTTTTTGGCAGT",
 
-  	    "reads" :  [ 100 ] ,
-  	    "top" : 2,
-  	    "germline" : "IGH"
-  	}
-    ] ,
-      "germlines" :  {
-  	     "custom" :  {
-  	        "shortcut" : "X",
-      	    "3" :  [ "/some/IGHV.fa" ],
-      	    "4" :  [ "/some/IGHD.fa" ],
-      	    "5" :  [ "/some/IGHJ.fa" ]
-  	      }
-        }
-    };
-    return vidjilObj;
 }
 
 function displayVidjilViews() {
