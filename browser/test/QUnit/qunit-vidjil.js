1cf4761d19d02734cb3d6cbf349ff2d6731435e6
Mathieu Giraud <mathieu@vidjil.org>
2017-04-03 11:49:49 +0200

    qunit-vidjil.js: assert.approx(), check float 'equality'

--- browser/test/QUnit/qunit-vidjil.js
+++ browser/test/QUnit/qunit-vidjil.js
@@ -8,3 +8,9 @@ QUnit.assert.includes = function(result, pattern, message ) {
     this.push(res, res, "{includes} " + pattern, message);
 }
 
+QUnit.assert.approx = function(result, expected, margin, message ) {
+    // Checks that two floats are about the same
+    var res = Math.abs(result-expected) <= margin
+
+    this.push(res, result, expected + "±" + margin, message);
+}
