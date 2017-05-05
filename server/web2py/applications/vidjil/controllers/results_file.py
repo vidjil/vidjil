11c1c10515a0e6dfc991d3506e7258349a77cdca
Ryan Herbert <ryan@vidjil.org>
2017-05-05 15:08:50 +0200

    results_file.py don't load files into variable
    When serving files, they were being loaded into a variable, which as
    mentionned by @mikael-s in #2405 can lead to a large amount of RAM being
    taken up for no good reason.
    
    Using the same web2py streaming facility, files should now be served in
    chunks and not be loaded into RAM all at once.

--- server/web2py/applications/vidjil/controllers/results_file.py
+++ server/web2py/applications/vidjil/controllers/results_file.py
@@ -120,9 +120,7 @@ def download():
         directory = defs.DIR_OUT_VIDJIL_ID % results_id
         filepath = directory + os.path.basename(request.vars['filename'])
         try:
-            with open(filepath) as f:
-                file_content = f.read()
-            return response.stream(StringIO.StringIO(file_content), attachment = True, filename = request.vars['filename'])
+            return response.stream(open(filepath), attachment = True, filename = request.vars['filename'], chunk_size=10**6)
         except IOError:
             return error_message("File could not be read")
     return error_message("access denied")
