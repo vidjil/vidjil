542ab79276b185da17cb4e6687fb3e48bc4d82c9
Mathieu Giraud <mathieu@vidjil.org>
2017-05-16 17:48:21 +0200

    tests: test shortcut.js

---
+++ browser/test/QUnit/testFiles/shortcut_test.js
@@ -0,0 +1,30 @@
+
+QUnit.module("Shortcut", {
+});
+
+function keyMock(d) {
+    d.preventDefault = function() {}
+    return d
+};
+
+
+
+QUnit.test("shortcuts on scatterplot", function(assert) {
+
+    var m = new Model(m)
+    m.parseJsonData(json_data, 100)
+    m.loadGermline()
+    m.initClones()
+
+    var sp = new ScatterPlot("visu", m)
+    sp.init()
+
+    var shortcut = new Shortcut()
+    
+    assert.equal(sp.mode, sp.MODE_GRID)
+    shortcut.checkKey(keyMock({'key': '#'}))
+    assert.equal(sp.mode, sp.MODE_BAR)
+})
+           
+
+    
