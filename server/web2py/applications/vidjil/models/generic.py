d1d32b1f09ed5dea7bc490a3c3cda0ca42e751b9
Ryan Herbert <ryan@vidjil.org>
2017-05-16 17:18:47 +0200

    generic.py remove duplicate method
    The parent class has the exact same implementation of this method and
    needs it in order for the other model classes to work properly.

--- server/web2py/applications/vidjil/models/generic.py
+++ server/web2py/applications/vidjil/models/generic.py
@@ -6,19 +6,6 @@ class Generic(SampleSet):
     def get_type_display(self):
         return 'set'
 
-    def get_config_urls(self, data):
-        configs = []
-        for conf in data.conf_list:
-            filename =  "(%s %s)" % (self.get_name(data), conf['name'])
-            if conf['fused_file'] is not None :
-                configs.append(
-                    str(A(conf['name'],
-                        _href="index.html?sample_set_id=%d&config=%d" % (data.sample_set_id, conf['id']), _type="text/html",
-                        _onclick="event.preventDefault();event.stopPropagation();if( event.which == 2 ) { window.open(this.href); } else { db.load_data( { 'sample_set_id' : '%d', 'config' :  %d }, '%s' ); }" % (data.sample_set_id, conf['id'], filename))))
-            else:
-                configs.append(conf['name'])
-        return XML(", ".join(configs))
-
     def filter(self, filter_str, data):
         for row in data:
             row['string'] = [row['name'], row['confs'], row['groups'], str(row['info'])]
