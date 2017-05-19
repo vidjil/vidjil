8161b869ed32bec3e1fbd3b7a04a85b927f7c49f
Téo Vasseur <teo.vasseur@univ-lille1.fr>
2017-05-19 14:24:16 +0200

    builder.js : add missing header

--- browser/js/builder.js
+++ browser/js/builder.js
@@ -1,3 +1,26 @@
+/*
+ * This file is part of Vidjil <http://www.vidjil.org>,
+ * High-throughput Analysis of V(D)J Immune Repertoire.
+ * Copyright (C) 2013-2017 by Bonsai bioinformatics
+ * at CRIStAL (UMR CNRS 9189, Université Lille) and Inria Lille
+ * Contributors: 
+ *     Marc Duez <marc.duez@vidjil.org>
+ *     The Vidjil Team <contact@vidjil.org>
+ *
+ * "Vidjil" is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * "Vidjil" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with "Vidjil". If not, see <http://www.gnu.org/licenses/>
+ */
+
 function Builder(model, database) {
     if (typeof model != "undefined"){
         this.m = model; //Model utilisé
