f84a5c4a43b8b3ed5a1d5a18efe30bc3a42fbd93
Téo Vasseur <teo.vasseur@univ-lille1.fr>
2017-05-19 14:30:37 +0200

    add new file info.js
    See #2446

---
+++ browser/js/info.js
@@ -0,0 +1,37 @@
+/*
+ * This file is part of Vidjil <http://www.vidjil.org>,
+ * High-throughput Analysis of V(D)J Immune Repertoire.
+ * Copyright (C) 2013-2017 by Bonsai bioinformatics
+ * at CRIStAL (UMR CNRS 9189, Université Lille) and Inria Lille
+ * Contributors:
+ *     Marc Duez <marc.duez@vidjil.org>
+ *     The Vidjil Team <contact@vidjil.org>
+ *
+ * "Vidjil" is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * "Vidjil" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with "Vidjil". If not, see <http://www.gnu.org/licenses/>
+ */
+
+function Info(model) {
+    View.call(this, model);
+}
+
+Info.prototype = {
+
+    init : function () {},
+
+    updateElem : function (list) {},
+
+    updateElemStyle : function () {},
+
+    resize : function () {},
+}
