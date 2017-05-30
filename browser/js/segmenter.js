4d9c44f43c335eba6515d407f94f348330af2da5
Téo Vasseur <teo.vasseur@univ-lille1.fr>
2017-05-30 15:08:52 +0200

    {list,segmenter}.js : use DOM instead of innerHTML (for pretty)
    See #2436

--- browser/js/segmenter.js
+++ browser/js/segmenter.js
@@ -627,7 +627,7 @@ Segment.prototype = {
         for (var i in this.selectedAxis) {
             var span = document.createElement('span');
             var axis = this.selectedAxis[i];
-            span.innerHTML = axis.pretty ? axis.pretty(axis.fct(clone)) : axis.fct(clone);
+            span.appendChild(axis.pretty ? axis.pretty(axis.fct(clone)) : document.createTextNode(axis.fct(clone)));
             span.setAttribute('title', this.selectedAxis[i].label);
             span.className = axOpts[i];
             axisBox.appendChild(span);
