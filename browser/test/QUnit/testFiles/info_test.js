b269dc7b939cfe8e71bc9d8150718ccc90032649
Mathieu Giraud <mathieu@vidjil.org>
2017-06-07 17:35:40 +0200

    tests: testing 'Info' view, system boxes
    See #2517.

--- browser/test/QUnit/testFiles/info_test.js
+++ browser/test/QUnit/testFiles/info_test.js
@@ -27,3 +27,32 @@ QUnit.test("container, boxes", function(assert) {
     assert.equal(sbnm.length, 2, 'two locus boxes')
 })
 
+
+QUnit.test("read details", function(assert) {
+
+    var m = new Model()
+    m.parseJsonData(json_data, 10)
+    m.loadGermline()
+    m.initClones()
+
+    var builder = new Builder(m)
+    var info = new Info("info", m, builder)
+    info.init()
+
+    var reads = document.getElementsByClassName('reads_details')[0]
+    assert.includes(reads.outerHTML, 'analyzed reads</span><span>200 (100.00%)', 'read details: analyzed reads')
+    assert.includes(reads.outerHTML, 'selected locus</span><span>200 (100.00%)', 'read details: selected locus')
+
+    var top_info = document.createElement('div');
+    top_info.id = "top_info"
+    document.getElementById('info').appendChild(top_info)
+
+    m.keep_one_active_system('IGH')
+    var sbnm = document.getElementsByClassName('systemBoxNameMenu')
+    assert.includes(sbnm[0].outerHTML, '<span class="systemBoxNameMenu IGH">', 'system box IGH is still active')
+    assert.includes(sbnm[1].outerHTML, '<span class="systemBoxNameMenu inactive TRG">', 'system box TRG is inactive')
+
+    var reads = document.getElementsByClassName('reads_details')[0]
+    assert.includes(reads.outerHTML, 'analyzed reads</span><span>200 (100.00%)', 'read details: analyzed reads (same than before)')
+    assert.includes(reads.outerHTML, 'selected locus</span><span>100 (50.00%)', 'read details: selected locus (only IGH now)')
+});
