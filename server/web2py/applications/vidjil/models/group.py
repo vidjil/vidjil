9c1c30435004416ff7abe7a2bb4c52de47754b51
Ryan Herbert <ryan@vidjil.org>
2017-07-12 15:04:56 +0200

    models/group.py add group model for default permissions

---
+++ server/web2py/applications/vidjil/models/group.py
@@ -0,0 +1,5 @@
+def add_default_group_permissions(auth, group_id):
+    auth.add_permission(group_id, PermissionEnum.create.value, 'sample_set', 0);
+    auth.add_permission(group_id, PermissionEnum.admin.value, 'sample_set', 0);
+    auth.add_permission(group_id, PermissionEnum.upload.value, 'sample_set', 0);
+    auth.add_permission(group_id, PermissionEnum.save.value, 'sample_set', 0);
