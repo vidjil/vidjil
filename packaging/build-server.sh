815dc9ec33892b916a066537673b0f882b99d802
Ryan Herbert <ryan@vidjil.org>
2017-10-18 14:34:37 +0200

    packaging+docker package web2py source code with vidjil-server
    The web2py package for debian is a little out-dated and we would like to
    use the latest version. As mentionned here web2py.com/init/default/download
    specifying in the license where web2py's code begins and our app's ends
    allows us to do this.
    
    This commit updates the copyright file in the package to provide these
    details and allow us to package our server with whichever version of
    web2py we need.

--- packaging/build-server.sh
+++ packaging/build-server.sh
@@ -10,6 +10,12 @@ exec_dir="server"
 
 copy_files -s="$source_files" -d="$dest_files"
 
+cd "$dest_files/server"
+wget https://mdipierro.pythonanywhere.com/examples/static/web2py_src.zip
+unzip web2py_src.zip
+
+unlink web2py_src.zip
+
 cd "$MY_PWD/$exec_dir"
 create_version_file "$MY_PWD/$dest_files/server"
 bash "$MY_PWD/mkdeb"
