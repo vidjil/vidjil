658ff73a88709f75fd37237f1c9f60d79e1e9ac3
Mathieu Giraud <mathieu@vidjil.org>
2017-11-21 11:56:46 +0100

    js/numerical_axis.js: nice_number_digits was already defined elsewhere
    See 62eefb1d and 71be5c3b, and #2731.

--- browser/js/numerical_axis.js
+++ browser/js/numerical_axis.js
@@ -279,7 +279,7 @@ function FloatAxis (model, reverse, can_undefined) {
 FloatAxis.prototype = Object.create(NumericalAxis.prototype);
 
     FloatAxis.prototype.getLabelText = function(value) {
-        return parseFloat(value).toFixed( this.nice_number_digits(this.min, this.max) )
+        return parseFloat(value).toFixed( nice_number_digits(this.min, this.max, 2) )
     }
 
     FloatAxis.prototype.insert_values = function() {
@@ -305,10 +305,3 @@ FloatAxis.prototype = Object.create(NumericalAxis.prototype);
        this.value_mapping = temp
     }
 
-    FloatAxis.prototype.nice_number_digits = function(min, max) { //max - min, 2){
-        var exposant = 0
-        while (max - min < Math.pow(10, exposant) ) {
-            exposant--;
-        } 
-        return exposant
-    }
