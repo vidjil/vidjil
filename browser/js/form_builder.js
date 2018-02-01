8c653fdc5c01d020f3ea4360df8de4e33e2ccfd4
Ryan Herbert <ryan@vidjil.org>
2018-02-01 18:16:15 +0100

    better date validation pattern

--- browser/js/form_builder.js
+++ browser/js/form_builder.js
@@ -113,7 +113,7 @@ FormBuilder.prototype.build_date = function(id, object, name, label) {
         var d = this.build_wrapper();
         d.appendChild(this.build_label(label, object, id));
         var i = this.build_input(id, 'date', name, 'text', object, false, 'yyyy-mm-dd');
-        i.pattern = "[0-9]{4}-[0-9]{2}-[0-9]{2}";
+        i.pattern = "(?:19|20)[0-9]{2}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1[0-9]|2[0-9])|(?:(?!02)(?:0[1-9]|1[0-2])-(?:30))|(?:(?:0[13578]|1[02])-31))";
         i.title = "yyyy-mm-dd"
         d.appendChild(i);
         return d;
