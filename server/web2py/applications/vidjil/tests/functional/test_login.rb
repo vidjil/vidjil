3821a9f4ef6b32047e309e792db974bc7b121f61
Ryan Herbert <ryan@vidjil.org>
2018-05-30 16:25:54 +0100

    test_login.rb another test
    See #2881

--- server/web2py/applications/vidjil/tests/functional/test_login.rb
+++ server/web2py/applications/vidjil/tests/functional/test_login.rb
@@ -15,7 +15,23 @@ class TestLogin < BrowserTest
     assert(login_form.present?)
     login_form.text_field(:id => "auth_user_email").set('foo@bar.com')
     login_form.text_field(:id => "auth_user_password").set('foobar')
-    login_form.tr(:id => 'submit_record_row').input(:type => 'submit').click
+    login_form.tr(:id => 'submit_record__row').input(:type => 'submit').click
+    Watir::Wait.until(timeout: 30) {$b.execute_script("return jQuery.active") == 0}
+    assert(login_form.present?)
+  end
+
+  def test_01_successful_login
+    login_form = $b.form(:id => 'login_form')
+    assert(login_form.present?)
+    login_form.text_field(:id => "auth_user_email").set('plop@plop.com')
+    login_form.text_field(:id => "auth_user_password").set('foobar')
+    login_form.tr(:id => 'submit_record__row').input(:type => 'submit').click
+    Watir::Wait.until(timeout: 30) {$b.execute_script("return jQuery.active") == 0}
+    assert(!login_form.present?)
+  end
+
+  def test_zz_close
+    close_everything
   end
 end
 
