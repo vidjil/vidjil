42d57f082066cfe64c0e349ebd569e6e6acb9c50
Mathieu Giraud <mathieu@vidjil.org>
2018-05-31 17:02:49 +0200

    tests: add test, compare two samples

--- browser/test/QUnit/testFiles/scatterPlot_test.js
+++ browser/test/QUnit/testFiles/scatterPlot_test.js
@@ -84,6 +84,35 @@ QUnit.test("node sizes", function(assert) {
 })
 
 
+QUnit.test("other plot", function(assert) {
+
+    var m = new Model(m);
+    m.parseJsonData(json_data, 100)
+    m.loadGermline()
+    m.initClones()
+
+    var sp = new ScatterPlot("visu", m);
+    sp.init();
+    sp.update();
+
+    assert.equal(sp.otherVisibility, false)
+    assert.equal(sp.nodes[2].s, 0.125, "node 0, size (main sample time 0)")
+
+    m.tOther = 1
+    var preset = sp.preset["compare two samples"]
+    sp.changeSplitMethod(preset.x, preset.y, preset.mode)
+
+    assert.equal(sp.splitY, "sizeOtherSample")
+        assert.equal(sp.otherVisibility, true)
+
+    assert.equal(sp.nodes[2].s, 0.25, "node 0, size (other sample time 1)")
+
+    m.tOther = 3
+    sp.updateClone(2)
+    assert.equal(sp.nodes[2].s, 0.125, "node 0, size (other sample time 3)")
+})
+
+
 QUnit.test("multiple selection", function(assert) {
 
     var m = new Model(m);
