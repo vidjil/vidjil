1283c7f0d143e3349a3e47d6318ae8d64088c4be
Ryan Herbert <ryan@vidjil.org>
2018-06-01 15:20:44 +0100

    docker use entrypoint script for nginx

--- docker/vidjil/scripts/nginx-entrypoint.sh
+++ docker/vidjil/scripts/nginx-entrypoint.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# sed cannot redirect output to the input file so rename the file and sed into the final desired name
-mv /etc/vidjil/conf.js /etc/vidjil/tmp
-sed s/#{my_url}/$UWSGI_PORT_433_TCP_ADDR/g /etc/vidjil/tmp > /etc/vidjil/conf.js
-rm /etc/vidjil/tmp
-ln -sf /etc/vidjil/conf.js /usr/share/vidjil/browser/js/conf.js
-nginx -g "daemon off;"
+spawn-fcgi -u www-data -s /var/run/fcgiwrap.socket /usr/sbin/fcgiwrap
+nginx -g 'daemon off;'
