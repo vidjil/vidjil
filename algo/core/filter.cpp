86d7dd15936ac66cf99924ec187323104319882f
Cyprien Borée <boreec@tuta.io>
2018-06-12 18:17:07 +0200

    filter.{h,cpp}: builds and uses a Aho-Corasick automaton to filter a BioReader
    See #920 and #3080.
    
    (squashed from earlier commits)

---
+++ algo/core/filter.cpp
@@ -0,0 +1,133 @@
+#include "filter.h"
+
+pair<vector<int>*, AbstractACAutomaton<KmerAffect>*>* buildACAutomatonToFilterBioReader
+  (BioReader &origin, string seed){
+  pair<vector<int>*, AbstractACAutomaton<KmerAffect>*>* result;
+  vector<int>* indexes;
+  PointerACAutomaton<KmerAffect>* aho;
+  char asciiChar;
+  int asciiNumber;
+  string currentLabel;
+  string previousLabel;
+
+  if(origin.size() < 1){
+    return nullptr;
+  }
+  result = new pair<vector<int>*,AbstractACAutomaton<KmerAffect>*>();
+  aho = new PointerACAutomaton<KmerAffect>(false, true);
+  indexes = new vector<int>();
+  aho->insert(origin.sequence(0),std::string("") + char(1), true, 0, seed);
+  asciiNumber = 1;
+  indexes->push_back(0);
+  previousLabel = extractGeneName(origin.label(0));
+  int i;
+  for(i = 1;i < origin.size(); ++i){
+    currentLabel = extractGeneName(origin.label(i));
+    if(currentLabel != previousLabel){
+      indexes->push_back(i);
+      asciiNumber++;
+      if(char(asciiNumber) == AFFECT_AMBIGUOUS_CHAR ||
+        char(asciiNumber) == AFFECT_UNKNOWN_CHAR){
+        asciiNumber++;
+      }
+    }
+    if(asciiNumber > 127){
+      delete result; delete aho; delete indexes;
+      return nullptr;
+    }
+    asciiChar = char(asciiNumber);
+    aho->insert(origin.sequence(i),std::string("") + asciiChar, true, 0, seed);
+    previousLabel = currentLabel;
+  }
+  indexes->push_back(origin.size());
+  aho->build_failure_functions();
+  result->first = indexes;
+  result->second = aho;
+  return result;
+}
+
+/*
+  Takes a built automaton and a vector of indexes and build a BioReader
+  based on it.
+*/
+BioReader filterBioReaderWithACAutomaton(
+    pair<vector<int>*, AbstractACAutomaton<KmerAffect>*>* idxAho,
+    BioReader &origin, seqtype &seq,
+    unsigned int kmer_threshold){
+
+  BioReader result;
+  AbstractACAutomaton<KmerAffect>* aho;
+  vector<int>* indexes;
+  map<KmerAffect, int> mapAho;
+  KmerAffect tmpKmer;
+  unsigned int asciiNum;
+  char asciiChar;
+  if(idxAho == nullptr){
+    return origin;
+  }
+  indexes =  idxAho->first;
+  aho = idxAho->second;
+  mapAho = aho->getMultiResults(seq);
+
+  //All k-mers selected : iterate over all map
+  if(kmer_threshold > mapAho.size()){
+    for(auto const mx: mapAho){
+      tmpKmer = mx.first;
+      asciiChar = tmpKmer.getLabel().at(0);
+      asciiNum = int(asciiChar);
+      if(asciiNum == int(AFFECT_UNKNOWN_SYMBOL[0]) || asciiNum == int(AFFECT_AMBIGUOUS_SYMBOL[0])){
+        asciiNum++;
+      }
+      if(asciiNum > indexes->size() - 1){
+        break;
+      }
+      if(asciiChar == AFFECT_AMBIGUOUS_SYMBOL[0] || asciiChar == AFFECT_UNKNOWN_SYMBOL[0]){
+        continue;
+      }
+      for(int i = indexes->at(asciiNum - 1); i < indexes->at(asciiNum); ++i){
+        result.add(origin.read(i));
+      }
+    }
+  /* The most significant k-mers selected : iterate over a portion of the
+    sorted map */
+  }else{
+    /* sort map */
+    typedef function<bool(pair<KmerAffect, int>, pair<KmerAffect, int>)> Comparator;
+    Comparator compFunctor = [](pair<KmerAffect, int> elem1 ,pair<KmerAffect, int> elem2){
+      return elem1.second > elem2.second;
+    };
+    // Use a set to use the comparator and sort function
+    set<pair<KmerAffect, int>, Comparator> setOfWords(mapAho.begin(), mapAho.end(), compFunctor);
+    // Iterate over the pair and not the map
+    unsigned int nbKmers = 0;
+    for(pair<KmerAffect, int> element : setOfWords){
+      // Add corresponding sequences to the BioReader
+      if(nbKmers < kmer_threshold){
+        nbKmers++;
+        tmpKmer = element.first;
+        asciiChar = tmpKmer.getLabel().at(0);
+        asciiNum = int(asciiChar);
+        if(asciiNum == int(AFFECT_UNKNOWN_SYMBOL[0]) || asciiNum == int(AFFECT_AMBIGUOUS_SYMBOL[0])){
+          asciiNum++;
+        }
+        if(asciiNum > indexes->size() - 1){
+          break;
+        }
+        if(asciiChar == AFFECT_AMBIGUOUS_SYMBOL[0] || asciiChar == AFFECT_UNKNOWN_SYMBOL[0]){
+          continue;
+        }
+        for(int i = indexes->at(asciiNum - 1); i < indexes->at(asciiNum); ++i){
+          result.add(origin.read(i));
+        }
+      }
+      else{
+        /* Enough K-mers used for filtering, no need to go further */
+        break;
+      }
+    }
+  }
+  if(result.size() <= 2){
+    return origin;
+  }
+  return result;
+}
