41f7403b7c6aa3512f246a7a6aab5f4ff9d38aa9
Ryan Herbert <ryan@vidjil.org>
2018-07-20 11:18:56 +0100

    default.py add missing pre_process access permission

--- server/web2py/applications/vidjil/controllers/default.py
+++ server/web2py/applications/vidjil/controllers/default.py
@@ -162,6 +162,7 @@ def init_db_helper(force=False, admin_email="plop@plop.com", admin_password="123
         auth.add_permission(id_admin_group, PermissionEnum.access.value, db.run, 0)
         auth.add_permission(id_admin_group, PermissionEnum.access.value, db.generic, 0)
         auth.add_permission(id_admin_group, PermissionEnum.access.value, db.config, 0)
+        auth.add_permission(id_admin_group, PermissionEnum.access.value, db.pre_process, 0)
         auth.add_permission(id_admin_group, PermissionEnum.access.value, db.auth_group, 0)
         auth.add_permission(id_admin_group, PermissionEnum.admin.value, db.sample_set, 0)
         auth.add_permission(id_admin_group, PermissionEnum.admin.value, db.patient, 0)
