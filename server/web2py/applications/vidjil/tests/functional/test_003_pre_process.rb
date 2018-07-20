3ae273d35067cebe666e06c062607b3e8b885faa
Ryan Herbert <ryan@vidjil.org>
2018-07-20 11:21:09 +0100

    test_003_pre_process.rb remove commented line.

--- server/web2py/applications/vidjil/tests/functional/test_003_pre_process.rb
+++ server/web2py/applications/vidjil/tests/functional/test_003_pre_process.rb
@@ -18,7 +18,6 @@ class TestPreProcess < BrowserTest
   end
 
   def go_to_list
-    #$b.a(:class => ["button", "button_token", "patient_token"], :text => "patients").click
     $b.a(:class => "button", :text => "pre-process").click
     Watir::Wait.until(30) {$b.execute_script("return jQuery.active") == 0}
     table = $b.table(:id => "table")
