d3b4db3cda0dad58243500b280ad79e5480bc069
Mikael Salson <mikael.salson@univ-lille1.fr>
2018-10-18 16:14:59 +0200

    segment.cpp: A KmerAffectAnalyser was already created

--- algo/core/segment.cpp
+++ algo/core/segment.cpp
@@ -523,7 +523,7 @@ KmerSegmenter::KmerSegmenter(Sequence seq, Germline *germline, double threshold,
       || (germline->seg_method == SEG_METHOD_MAX1U))
     { // Pseudo-germline, MAX12 and MAX1U
       pair <KmerAffect, KmerAffect> max12 ;
-      CountKmerAffectAnalyser ckaa(*(germline->index), sequence);
+      KmerAffectAnalyser ckaa = *kaa;
 
 
       set<KmerAffect> forbidden;
