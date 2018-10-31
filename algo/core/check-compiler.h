8ce297b2d591ef7faa1542506361be05dbc0edcb
Mathieu Giraud <mathieu@vidjil.org>
2018-10-31 08:45:29 +0100

    core/check-compiler.h: update links to doc

--- algo/core/check-compiler.h
+++ algo/core/check-compiler.h
@@ -4,10 +4,10 @@
 
 #if defined(__clang__)
     #if (__clang_major__ * 10000 + __clang_minor__ * 100 + __clang_patchlevel__) < 30300
-        #warning "Vidjil needs a C++11 compiler such as clang >= 3.3 - see http://vidjil.org/doc/algo.html"
+        #warning "Vidjil needs a C++11 compiler such as clang >= 3.3 - see http://vidjil.org/doc/vidjil-algo"
     #endif
 #elif defined(__GNUC__)
     #if (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__) < 40800
-        #warning "Vidjil needs a C++11 compiler such as gcc >= 4.8 - see http://vidjil.org/doc/algo.html"
+        #warning "Vidjil needs a C++11 compiler such as gcc >= 4.8 - see http://vidjil.org/doc/vidjil-algo"
     #endif
 #endif
