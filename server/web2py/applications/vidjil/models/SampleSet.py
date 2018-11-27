81d00ab9841496d94cbbcdd165d0d594c0f6ad67
Ryan Herbert <ryan@vidjil.org>
2018-11-27 16:22:36 +0000

    models/SampleSet.py adjust config url
    for an absolulte URL

--- server/web2py/applications/vidjil/models/SampleSet.py
+++ server/web2py/applications/vidjil/models/SampleSet.py
@@ -50,7 +50,7 @@ class SampleSet(object):
             if conf['fused_file'] is not None :
                 configs.append(
                     str(A(conf['name'],
-                        _href="index.html?sample_set_id=%d&config=%d" % (data['sample_set_id'], conf['id']), _type="text/html",
+                        _href="/browser/index.html?sample_set_id=%d&config=%d" % (data['sample_set_id'], conf['id']), _type="text/html",
                         _onclick="event.preventDefault();event.stopPropagation();if( event.which == 2 ) { window.open(this.href); } else { myUrl.loadUrl(db, { 'sample_set_id' : '%d', 'config' :  %d }, '%s' ); }" % (data['sample_set_id'], conf['id'], filename))))
             else:
                 configs.append(conf['name'])
