efbda870b92c6c542cbfde43c02fac37586601d1
Mikael Salson <mikael.salson@univ-lille1.fr>
2019-03-12 18:01:57 +0100

    test_multilocus.rb: Skip test on report with Chrome
    #3699 must be solved first

--- browser/test/functional/test_multilocus.rb
+++ browser/test/functional/test_multilocus.rb
@@ -219,6 +219,10 @@ class TestMultilocus < BrowserTest
 
   def test_14_export_sample_report
 
+    if ENV['WATIR_CHROME']
+      skip "Issue #3699 must be solved first"
+    end
+
     assert ($b.scatterplot_x_legend(0).text.include? 'TRB'), "Current system should be TRB"
 
     # Select a clone
