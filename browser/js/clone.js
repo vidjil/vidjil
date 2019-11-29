40ebbc7def4bccfabfc009957044ea07ff1a33d8
flothoni <florian.thonier.pro@gmail.com>
2019-11-29 10:07:49 +0100

    clone.js; add some comment on use of some variables of a clone Link to #4020

--- browser/js/clone.js
+++ browser/js/clone.js
@@ -50,6 +50,8 @@ function Clone(data, model, index, attributes) {
     this.split = false
     this.seg = {};
     this.segEdited = false
+    // Link to another clone that include this one in his cluster (after merge)
+    this.mergedId = undefined 
 
     this.attributes = attributes
     var key = Object.keys(data)
@@ -735,7 +737,8 @@ Clone.prototype = {
     },
 
     /**
-     * Define a list of compatible real clones
+     * Define a list of compatible real clones that share the same values for available axis
+     * The list is sample dependant and differ for each sample
      * This list is called at the creation of the clone
      */
     defineCompatibleClones: function(){
@@ -763,7 +766,9 @@ Clone.prototype = {
 
 
     /**
-     * Compute the current size of a distrib clone. SUbstract active clone that share same distribution values
+     * Compute the current size of a distrib clone. Substract active clone that share same distribution values
+     * This allow to get the current number of reads or clones that are represented by a distribution clone
+     * Use the list of compatible clones, define for each sample/timepoint to look at if they are filtered or not at the moment
      * Will be call at each change top, filter or search action
      * @return {[type]} [description]
      */
