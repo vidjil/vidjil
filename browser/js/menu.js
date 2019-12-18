68a42e00cb233ad8a2766f860bc38e207c53ebea
flothoni <florian.thonier.pro@gmail.com>
2019-12-18 17:40:04 +0100

    index.html: add an entrie in menu filter This one is use to switch the mode onyOneSample Link to #4104

--- browser/js/menu.js
+++ browser/js/menu.js
@@ -126,9 +126,6 @@ function showSelector(elem) {
             .css('display', 'none');
         $('#' + elem)
             .css('display', 'block')
-            .animate({
-                height: $('#' + elem).children(":first").height()
-            }, 100);
 }
 
 function hideSelector() {
