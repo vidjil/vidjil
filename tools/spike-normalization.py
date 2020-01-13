9d62dc60b796e8d3529a8f4c6c3e0e87239649d1
Joao @ backup server 228 <joao.m@boldrini.org.br>
2020-01-13 14:52:05 +0000

    spike normalization with R2, etc. and tests

--- tools/spike-normalization.py
+++ tools/spike-normalization.py
@@ -18,13 +18,25 @@ import math
 ############################################################
 ### constants
 
-version = 'S0.01'
-UNIVERSAL = 'universal'
+version = 'S0.05'
+UNI = 'UNI'                     # except Vidjil leaves cluster to user
+NG600 = 100000                  # number of cells for 600ng of DNA
+### maximum reads for a give spike-in allowed in diagnostic samples
+DIAGMAX = 20.0
 
 ############################################################
 ### routines
 
+############################################################
+### function to get family name from clone name
+### returns the longest alphanumeric prefix
+def family(name):
+    pos = 1
+    while pos < len(name) and name[pos].isalnum():
+        pos+=1
+    return (name[:pos])
 
+############################################################
 def linearRegression(y, x):
     '''
     function for curve fitting y = ax
@@ -55,100 +67,173 @@ def linearRegression(y, x):
     
     return lr
 
+############################################################
 
-def computeCopies(data):
+def prevalentGermline(germlines):
     reads = {}
-    copies = {}
+    prevalent = ""
+    for g in germlines:
+        germline = g[0:3]
+        if germline in reads:
+            reads[germline] += germlines[g][0]
+        else:
+            reads[germline] = germlines[g][0]
+        if prevalent == "" or reads[prevalent] < reads[germline]:
+            prevalent = germline
+    return prevalent, reads[prevalent]
+
+############################################################
+    
+def spikeTable(data):
+    reads = {}
+    fams = {}
     ## get config
-    for spike in data['config']['labels']:
-        copies[spike['name']] = spike['copies']
-    ## get reads
+    spikes = data['config']['labels']
+    
+    ## find spikes in data, stamp name, and augment table
     for clone in data['clones']:
         if 'label' in clone:
             ## spike-in clone
             label = clone['label']
-            ## show spike name
-            clone['name'] = label + ' ' + clone['name']
+            if 'name' in clone:
+                ## call family before changing clone name
+                fams[label] = family(clone['name'])
+                clone['name'] = label + ' ' + clone['name']
+            else:
+                ## add spike name to clone anyway
+                clone['name'] = label
             ## grab read data
             a = clone['reads']
             if len(a) > 1:
                 print('  *** reads array with many elements', file=sys.stderr)
-
             ## add reads
             if label not in reads:
                 reads[label] = 0.0
             reads[label] += a[0]
+    for spike in spikes:
+        spike['reads'] = reads[spike['name']] if spike['name'] in reads else 0
+        ## vfam stands for 'Vidjil family' 
+        spike['vfam'] = fams[spike['name']] if spike['name'] in fams else spike['family']
 
-    return copies, reads, data['reads']['segmented'][0]
+    return spikes
 
 ############################################################
 
-def computeUniversalCoefficient(copies, reads):
+def computeCoefficients(spikes):
     '''
     curve-fitting
-    skip computing f for each user family
-    will do just universal coefficient
+    computes f for each user family
+    plus UNI coefficient
+    if family not dense enough or Pearson < 0.8 use UNI
+    plus SPIKE coefficient, used for spikes
+    goal is to force sum of spikes = 100,000 - sum of non spikes
     '''
 
-    f = {}
+    coeff = {}
     r2 = {}
     perr = {}
 
-    ### universal coefficient
+    ### UNI coefficient (only if enough spikes)
     ### uses one point per copy number
-    ### readsList is the list or read counts for each copy #
-    readsList = {}
-    for label in reads:
-        nCopies = copies[label]
-        if nCopies not in readsList:
-            readsList[nCopies] = []
-        readsList[nCopies].append(reads[label])
-
-    ### x = copy number
-    ### y = mean of reads for this copy number
-    x = []
-    y = []
-    for nCopies in readsList:
-        nReadsList = readsList[nCopies]
-        s = sum(nReadsList)
-        c = len(nReadsList)
-        x.append(1.0*s/c)
-        y.append(1.0*int(nCopies))
-        ## print('{0:5} {1:10.1f}'.format(s/c, int(nCopies)))
-
     ### test for not enough items
-    if len(x) == 0 or max(x) <= 10.0:
+
+    copyNos = list(set([int(spike['copies']) for spike in spikes]))
+    avgReads = []
+    if msgs >= 1:
+        print('copyNos: ', copyNos)
+    for copyNo in copyNos:
+        reads = [ spike['reads'] for spike in spikes
+                    if int(spike['copies']) == copyNo ]
+        ## len(reads) is never zero because spike['copies'] is in copyNos
+        avgReads.append(sum(reads)/len(reads))
+    if msgs >= 1:
+        print('avgReads: ', avgReads)
+    if len(avgReads) == 0 or max(avgReads) <= DIAGMAX:
+        ## not enough spikes; probably diagnostic sample
         print('** Not enough spike-ins **', file=sys.stderr)
         print('** No normalization performed **', file=sys.stderr)
     else:
-        ### fit curve
-        lr = linearRegression(y, x)
-        f[UNIVERSAL] = lr['slope']
-        r2[UNIVERSAL] = lr['r2']
-        perr[UNIVERSAL] = lr['s']
+        ### universal coefficient
+        lr = linearRegression(copyNos, avgReads)
+        coeff[UNI] = lr['slope']
+        r2[UNI] = lr['r2']
+        perr[UNI] = lr['s']
         if msgs >= 1:
-            fmtStr = 'Universal coefficient estimation: {0:15.13f} s: {1:5.1f} r2: {2:15.13f}'
-            print(fmtStr.format(f[UNIVERSAL], perr[UNIVERSAL], r2[UNIVERSAL]), file=sys.stderr)
+            fmtStr = 'Uni coefficient estimation: {0:15.13f} s: {1:5.1f} r2: {2:15.13f}'
+            print(fmtStr.format(coeff[UNI], perr[UNI], r2[UNI]), file=sys.stderr)
 
-    return f
+        ### family coefficients
+        ### also use one point per copy number
+        ### test for not enough points and r2 <= 0.8
+
+        vfams = list(set([spike['vfam'] for spike in spikes]))
+        if msgs >= 1:
+            print('Vfams: {0}'.format(vfams))
+        for vfam in vfams:
+            copyNos = list(set([int(spike['copies']) for spike in spikes if spike['vfam'] == vfam]))
+            reads = [ sum([ spike['reads'] for spike in spikes
+                            if spike['vfam'] == vfam and int(spike['copies']) == copyNo])
+                      for copyNo in copyNos ]
+            if len(reads) <= 1 or max(reads) <= DIAGMAX:
+                ## not enough points; use UNI coefficient
+                coeff[vfam] = coeff[UNI]
+                r2[vfam] = r2[UNI]
+                perr[vfam] = perr[UNI]
+            else:
+                ### fit curve
+                lr = linearRegression(copyNos, reads)
+                coeff[vfam] = lr['slope']
+                r2[vfam] = lr['r2']
+                perr[vfam] = lr['s']
+                if lr['r2'] <= 0.8:
+                    ## r2 <= 0.8; use UNI cofficient
+                    coeff[vfam] = coeff[UNI]
+                    r2[vfam] = r2[UNI]
+                    perr[vfam] = perr[UNI]
+            if msgs >= 1:
+                fmtStr = '{0} coefficient estimation: {1:15.13f} s: {2:5.1f} r2: {3:15.13f}'
+                print(fmtStr.format(vfam, coeff[vfam], perr[vfam], r2[vfam]), file=sys.stderr)
+
+    return coeff, r2
 
 ############################################################
 ### add normalized reads
+###
+### data: entire .vidjil JSON structure
+### coeff: dict with normalization coefficients for each family
+### r2: Pearson's r2 for each family
+### spk: total number of spike-in reads
 
-def addNormalizedReads(data, coeff, totalReads):
+def addNormalizedReads(data, coeff, r2, spk):
     if msgs >= 1:
         print('Normalizing reads and prinitng output file', file=sys.stderr)
 
-    ### all clones will be normalized
+    data['coefficients'] = coeff
+    ## find prevalent germine and sum of its reads
+    prevalent, spg = prevalentGermline(data['reads']['germline'])
+    data['samples']['prevalent'] = [ prevalent ]
+    data['samples']['ampl_coeff'] = [ spg/spk ]
+    ### normalize just clones from the prevalent germline
     for clone in data['clones']:
         ## grab read data
         a = clone['reads']
         if len(a) > 1:
             print('  *** reads array with many elements', file=sys.stderr)
-
-        ## update counters for clones
         reads = a[0]
-        clone['normalized_reads'] = [ reads*coeff*totalReads/100000 ]
+        if 'name' in clone:
+            ## use UNI for unknown, unnamed or unsegmented families or reads
+            fam = family(clone['name'])
+            if fam not in coeff:
+                ## unknown family or unsegmented clone
+                fam = UNI
+        else:
+            ## unnamed clone
+            fam = UNI
+        if 'germline' in clone and clone['germline'][0:3] == prevalent:
+            clone['copy_number'] = reads * coeff[fam]
+            clone['normalized_reads'] = [ reads*coeff[fam]*spg/NG600 ]
+            clone['R2'] = [ r2[fam] ]
+            clone['family'] = [ fam ]
 
 ############################################################
 ### command line, initial msg
@@ -160,16 +245,17 @@ if __name__ == '__main__':
 
     # read input file
     if msgs >= 1:
-        print('Reading follow-up file', file=sys.stderr)
+        print('Reading input file', file=sys.stderr)
 
     with open(inf) as inp:
         data = json.load(inp)
 
     # process data
-    copies, reads, totalReads = computeCopies(data)
-    f = computeUniversalCoefficient(copies, reads)
-    if UNIVERSAL in f:
-        addNormalizedReads(data, f[UNIVERSAL], totalReads)
+    spikes = spikeTable(data)
+    spk = sum([spike['reads'] for spike in spikes])
+    coeff, r2 = computeCoefficients(spikes)
+    if coeff:
+        addNormalizedReads(data, coeff, r2, spk)
 
     # write output file
     with open(outf, 'w') as of:
