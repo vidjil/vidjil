439a79bf0e80a47f6c9133420dafd96110a12164
marc duez <replayskevitch@gmail.com>
2020-02-17 14:45:51 +0100

    url.js : detect space in url parameters
    needed for scatterplot parameters

--- browser/js/url.js
+++ browser/js/url.js
@@ -87,7 +87,7 @@ Url.prototype= {
         }
 
         if (typeof this.url_dict.clone !== "undefined") {
-            var clones = this.url_dict.clone.split(',');
+            var clones = this.url_dict.clone.replace(/%20/g, " ").split(',');
             for (var j = 0; j < clones.length; j++) {
                 var c = this.m.clone(clones[j]);
                 if (typeof c !== "undefined" && c.isInteractable()) {
@@ -96,7 +96,7 @@ Url.prototype= {
             }
         }
         if (typeof this.url_dict.plot !== "undefined") {
-            var sp_params = this.url_dict.plot.split(',');
+            var sp_params = this.url_dict.plot.replace(/%20/g, " ").split(',');
             if (sp_params.length == 2) {
                 sp_params.push(this.sp.mode);
             }
