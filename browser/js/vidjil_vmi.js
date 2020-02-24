6b7cfccc7fdd8719a72c84060e0386929fa37606
Ryan Herbert <rya.herb@gmail.com>
2020-02-24 13:24:41 +0000

    vidjil_vmi.js add wide_mode layout
    move the right-container div to the right of the visu-container, make
    it a panel and add a placeholder scatterplot view to it.
    Add scenario to swap to the wide layout
    
    See #4195

--- browser/js/vidjil_vmi.js
+++ browser/js/vidjil_vmi.js
@@ -5,6 +5,7 @@ function VidjilVMI() {
     this.vmi.setupDrawer();
     this.mid_id = 'mid-container';
     this.left_id = 'left-container';
+    this.right_id = 'right-container';
     this.visu_id = 'visu-container';
     this.bot_id = 'bot-container';
 }
@@ -14,11 +15,13 @@ VidjilVMI.prototype = {
         var mid_container = new Panel(this.mid_id, this.parent_id, mid_callback);
         var left_container = new Panel(this.left_id, this.mid_id);
         var visu_container = new Panel(this.visu_id, this.mid_id, visu_callback);
+        var right_container = new Panel(this.right_id, this.mid_id);
         var bot_container = new Panel(this.bot_id);
 
         this.vmi.add_panel(mid_container, false);
         this.vmi.add_panel(left_container, false);
         this.vmi.add_panel(visu_container, true);
+        this.vmi.add_panel(right_container, false);
         this.vmi.add_panel(bot_container, false);
 
         this.vmi.addView("data", this.left_id, "", []);
@@ -28,6 +31,7 @@ VidjilVMI.prototype = {
         this.vmi.addView("visu2", this.visu_id, "", []);
         var segmenter_view = this.vmi.addView("segmenter", this.bot_id, "", []);
         segmenter_view.setMutable(false);
+        this.vmi.addView("visu3", this.right_id, "", []);
         this.vmi.setOverlays([this.visu_id]);
 
         this.normal_mode();
@@ -76,6 +80,26 @@ VidjilVMI.prototype = {
         this.vmi.setView(this.vmi.views.info, this.left_id);
         this.vmi.setView(this.vmi.views.visu, this.visu_id);
         this.vmi.setView(this.vmi.views.visu2, this.visu_id);
+        this.vmi.setView(this.vmi.views.segmenter, this.bot_id);
+        this.reset_menu();
+    },
+
+    wide_mode : function() {
+        this.vmi.hideAllViews();
+        this.vmi.hideAllPanels();
+        var views = Object.values(this.vmi.views);
+        for(i in views) {
+            views[i].setMutable(false);
+        }
+        var panels = [{}, this.bot_id];
+        panels[0][this.mid_id] = [this.left_id, this.visu_id, this.right_id];
+        this.vmi.setupPanels(panels);
+        this.vmi.setView(this.vmi.views.data, this.left_id);
+        this.vmi.setView(this.vmi.views.list, this.left_id);
+        this.vmi.setView(this.vmi.views.info, this.left_id);
+        this.vmi.setView(this.vmi.views.visu, this.visu_id);
+        this.vmi.setView(this.vmi.views.visu2, this.visu_id);
+        this.vmi.setView(this.vmi.views.visu3, this.right_id);
         this.vmi.setView(this.vmi.views.segmenter, this.bot_id);
         this.reset_menu();
     }
