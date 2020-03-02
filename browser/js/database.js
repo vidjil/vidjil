a1fe0d620719b06340aaba5ab20962ad20615a52
Mikael Salson <mikael.salson@univ-lille1.fr>
2020-03-02 15:28:06 +0000

    browser/js/{database,form_builder}.js, views/partial/file/form.html: File fields are required
    But this need to be updated (Javascript side) when switching configuration.
    
    See #3907

--- browser/js/database.js
+++ browser/js/database.js
@@ -372,8 +372,14 @@ Database.prototype = {
         if ($option.attr('required_files') == "1"){
             $(".file_2").hide();
             $(".upload_file").val("");
+            $(".upload_field").each(function() {
+                $(this).prop("required", false);
+            });
         }else{
             $(".file_2").show();
+            $(".upload_field").each(function() {
+                $(this).prop("required", true);
+            });
         }
     },
 
