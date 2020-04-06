b0d8225d4161d82e133107d4c84561bd42a7f099
Mikael Salson <mikael.salson@univ-lille1.fr>
2020-04-06 10:56:03 +0200

    more sleep

--- docker/vidjil-server/scripts/uwsgi-entrypoint.sh
+++ docker/vidjil-server/scripts/uwsgi-entrypoint.sh
@@ -15,7 +15,7 @@ fi
 
 if [ "$1" == "--ci" ]; then
     cd /usr/share/vidjil/server/web2py/applications/vidjil/scripts
-    sleep 10
+    sleep 20
     echo "Loading SQL"
     /usr/local/bin/gosu $user bash launch_python_script.sh load-sql.py -n /usr/share/vidjil/docker/ci/ci.sql
     echo "SQL loaded"
