8f2c4cd63400730f59c893c9d8afd9b306ed9e95
marc duez <replayskevitch@gmail.com>
2020-06-15 11:50:26 +0200

    sample_set.py: absolute url > consider browser path to always be at the root directory
    fix bug with older browser who are not able to distinguish url and origin (i look at you stupid firefox32)

--- server/web2py/applications/vidjil/models/SampleSet.py
+++ server/web2py/applications/vidjil/models/SampleSet.py
@@ -53,7 +53,7 @@ class SampleSet(object):
             if conf['fused_file'] is not None :
                 configs.append(
                     str(A(conf['name'],
-                        _href=http_origin + defs.BROWSER_PATH + "?sample_set_id=%d&config=%d" % (data['sample_set_id'], conf['id']), _type="text/html",
+                        _href=http_origin + "?sample_set_id=%d&config=%d" % (data['sample_set_id'], conf['id']), _type="text/html",
                         _onclick="event.preventDefault();event.stopPropagation();if( event.which == 2 ) { window.open(this.href); } else { myUrl.loadUrl(db, { 'sample_set_id' : '%d', 'config' :  %d }, '%s' ); }" % (data['sample_set_id'], conf['id'], filename))))
             else:
                 configs.append(conf['name'])
