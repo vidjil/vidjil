3f4b6e1849e67e0d7a5c4c3b085ef28ccc333903
flothoni <--help>
2020-07-09 13:38:40 +0200

    test_info.rb; Add a function in vidjil_browser.rb to access div directly Link to #4397

--- browser/test/functional/test_info.rb
+++ browser/test/functional/test_info.rb
@@ -22,20 +22,10 @@ class TestClones < BrowserTest
   def after_tests
   end
 
-  ##############
-  ### CLONES ###
-  ##############
-  # id     0 --> biggest clone, IGHV1, IGHJ1, _average_read_length==162
-  # id 15/16 --> other clone (TRD, IGH)
-  # id    18 --> lenSeqAverage/_average_read_length == 162
-  # id    27 --> lenCDR3 (undefined), represent all clones
-  # id    29 --> seg5; seg3 (IGHV1; IGHJ1)
 
   def test_000_default
-    $info = $b.div(:id => "info").div(:class => "info_color")
-
-    assert (     $info.span(:id => "fastTag0").exist? ), "info colorMethod is on tag by default"
-    assert ( not $info.span(:text => "not productive").exist? ), "info colorMethod is NOT on productivity at init"
+    assert (     $b.info_colorBy.span(:id => "fastTag0").exist? ), "info colorMethod is on tag by default"
+    assert ( not $b.info_colorBy.span(:text => "not productive").exist? ), "info colorMethod is NOT on productivity at init"
   end
 
   def test_001_change_productivity
@@ -43,10 +33,9 @@ class TestClones < BrowserTest
     $b.select_list(:id, "color_menu_select").select_value("productive")
     $b.update_icon.wait_while(&:present?)
 
-    assert ( not $info.span(:id => "fastTag0").exist? ),     "info colorMethod is no more on tag after select change"
-    assert ( $info.span(:text => "not productive").exist? ), "info colorMethod is on productivity (span not prod)"
-    assert ( $info.span(:text => "productive").exist? ),     "info colorMethod is on productivity (span prod)"
-
+    assert ( not $b.info_colorBy.span(:id => "fastTag0").exist? ),     "info colorMethod is no more on tag after select change"
+    assert ( $b.info_colorBy.span(:text => "not productive").exist? ), "info colorMethod is on productivity (span not prod)"
+    assert ( $b.info_colorBy.span(:text => "productive").exist? ),     "info colorMethod is on productivity (span prod)"
   end
 
   def test_002_change_abundance
@@ -54,13 +43,12 @@ class TestClones < BrowserTest
     $b.select_list(:id, "color_menu_select").select_value("abundance")
     $b.update_icon.wait_while(&:present?)
 
-    assert ( not $info.span(:text => "not productive").exist? ), "info colorMethod is no more on productivity"
-    assert (     $info.span(:text => "abundance").exist? ),      "info colorMethod is on abundance (text)"
-    assert (     $info.span(:text => "0%").exist? ),             "info colorMethod is on abundance (0%)"
-    assert (     $info.span(:class => "gradient").exist? ),      "info colorMethod is on abundance (gradient class)"
-    assert (     $info.span(:text => "100%").exist? ),           "info colorMethod is on abundance (100%)"
+    assert ( not $b.info_colorBy.span(:text => "not productive").exist? ), "info colorMethod is no more on productivity"
+    assert (     $b.info_colorBy.span(:text => "abundance").exist? ),      "info colorMethod is on abundance (text)"
+    assert (     $b.info_colorBy.span(:text => "0%").exist? ),             "info colorMethod is on abundance (0%)"
+    assert (     $b.info_colorBy.span(:class => "gradient").exist? ),      "info colorMethod is on abundance (gradient class)"
+    assert (     $b.info_colorBy.span(:text => "100%").exist? ),           "info colorMethod is on abundance (100%)"
   end
-
 
 
   # Not really a test
