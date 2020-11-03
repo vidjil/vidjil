4b8e48a556be8e1bb347779cae7155a370ea5e16
Ryan Herbert <rya.herb@gmail.com>
2020-11-03 12:20:12 +0000

    my_account init permissions to empty string
    group with no permissions will now display an empty string instead of
    brackets

--- server/web2py/applications/vidjil/controllers/my_account.py
+++ server/web2py/applications/vidjil/controllers/my_account.py
@@ -104,7 +104,7 @@ def index():
                 result[r.auth_group.role][set_type]['statuses'] = ""
             result[r.auth_group.role]['fuses'] = []
             result[r.auth_group.role]['tags'] = []
-        result[r.auth_group.role]['permissions'] = [] if r._extra[group_permissions()] is None else r._extra[group_permissions()]
+        result[r.auth_group.role]['permissions'] = "" if r._extra[group_permissions()] is None else r._extra[group_permissions()]
 
     query = (base_query(group_list) &
             (db.auth_permission.name == 'access') &
