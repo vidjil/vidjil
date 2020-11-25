7cb951c995fb60a880a6a66f7d00620f946ced6d
flothoni <florian.thonier.pro@gmail.com>
2020-11-25 22:35:24 +0100

    Apply path change

--- browser/test/functional/issues/test_4541.rb
+++ browser/test/functional/issues/test_4541.rb
@@ -8,7 +8,7 @@ class TestGraph < BrowserTest
  def setup
     super
     if not defined? $b
-      # set_browser("/browser/test/data/bug_stock_order/4541.vidjil")
+      # set_browser("/browser/test/data/issues/issues_4541/4541.vidjil")
     end
   end
 
@@ -25,7 +25,7 @@ class TestGraph < BrowserTest
 
   ### Issue 4541; 
   def test_00_load_analysis_simple_reorder
-    set_browser("/browser/test/data/bug_stock_order/4541.vidjil", "/browser/test/data/bug_stock_order/4541_00_stock.analysis")
+    set_browser("/browser/test/data/issues/issues_4541/4541.vidjil", "/browser/test/data/issues/issues_4541/4541_00_stock.analysis")
     assert ( $b.span(:id => "visu2_title").text == "2 / 3" ), "Ratio show is correct at init"
     assert (    $b.graph_x_legend('0').text == 'file0_name')
     assert (not $b.graph_x_legend('1').present?)
@@ -35,7 +35,7 @@ class TestGraph < BrowserTest
 
   # Samples here haven't stock_order field (old one)
   def test_01_load_analysis_hidden_sample
-    set_browser("/browser/test/data/bug_stock_order/4541.vidjil", "/browser/test/data/bug_stock_order/4541_01.analysis")
+    set_browser("/browser/test/data/issues/issues_4541/4541.vidjil", "/browser/test/data/issues/issues_4541/4541_01.analysis")
     assert ( $b.span(:id => "visu2_title").text == "2 / 3" ), "Ratio show is correct at init"
     assert (    $b.graph_x_legend('0').text == 'file0_name')
     assert (not $b.graph_x_legend('1').present?)
@@ -44,7 +44,7 @@ class TestGraph < BrowserTest
 
 
   def test_02_load_analysis_one_more_sample
-    set_browser("/browser/test/data/bug_stock_order/4541_02.vidjil", "/browser/test/data/bug_stock_order/4541_01.analysis")
+    set_browser("/browser/test/data/issues/issues_4541/4541_02.vidjil", "/browser/test/data/issues/issues_4541/4541_01.analysis")
     assert ( $b.span(:id => "visu2_title").text == "3 / 4" ), "Ratio show is correct at init"
     assert (    $b.graph_x_legend('0').text == 'file0_name')
     assert (not $b.graph_x_legend('1').present?)
@@ -54,7 +54,7 @@ class TestGraph < BrowserTest
 
 
   def test_03_load_analysis_deleted_sample
-    set_browser("/browser/test/data/bug_stock_order/4541_03_deleted_sample.vidjil", "/browser/test/data/bug_stock_order/4541_00_stock.analysis")
+    set_browser("/browser/test/data/issues/issues_4541/4541_03_deleted_sample.vidjil", "/browser/test/data/issues/issues_4541/4541_00_stock.analysis")
     assert ( $b.span(:id => "visu2_title").text == "1 / 2" ), "Ratio show is correct at init"
     assert (    $b.graph_x_legend('0').text == 'file0_name')
     assert (not $b.graph_x_legend('1').present?)
@@ -62,7 +62,7 @@ class TestGraph < BrowserTest
 
 
   def test_04_load_analysis_all_new_samples
-    set_browser("/browser/test/data/bug_stock_order/4541.vidjil", "/browser/test/data/bug_stock_order/4541_04_all_new_samples.analysis")
+    set_browser("/browser/test/data/issues/issues_4541/4541.vidjil", "/browser/test/data/issues/issues_4541/4541_04_all_new_samples.analysis")
     assert ( $b.span(:id => "visu2_title").text == "3 / 3" ), "Ratio show is correct at init"
     assert ( $b.graph_x_legend('0').text == 'file0_name')
     assert ( $b.graph_x_legend('1').text == 'file1_name')
@@ -70,7 +70,7 @@ class TestGraph < BrowserTest
   end
 
   def test_05_load_analysis_duplicate_in_order
-    set_browser("/browser/test/data/bug_stock_order/4541.vidjil", "/browser/test/data/bug_stock_order/4541_05_duplicate_in_order.analysis")
+    set_browser("/browser/test/data/issues/issues_4541/4541.vidjil", "/browser/test/data/issues/issues_4541/4541_05_duplicate_in_order.analysis")
     assert ( $b.span(:id => "visu2_title").text == "2 / 3" ), "Ratio show is correct at init"
     assert (    $b.graph_x_legend('0').text == 'file0_name')
     assert (not $b.graph_x_legend('1').present?)
