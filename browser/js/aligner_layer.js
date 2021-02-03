cd845886792ee51b47a771496257f9f4a8755864
Mathieu Giraud <mathieu@vidjil.org>
2021-02-03 22:45:25 +0100

    js/aligner_layer.js: css, bikeshed V/D/J and FR/CDR colors
    - FR/CDR: no hue
    - V/D/J: hue, but less saturated that the colors used for clones
    - native/IGMT: slight change of value

--- browser/js/aligner_layer.js
+++ browser/js/aligner_layer.js
@@ -36,7 +36,7 @@ LAYERS = {
         'start': function (s,c) { return c.getSegStart("5"); },
         'stop': function (s,c) { return c.getSegStop("5"); },
         'className': "seq_layer_highlight",
-        'style': { 'background': "green" },
+        'style': { 'background': "#4c4" },
         'enabled': true
     },
     'D':
@@ -45,7 +45,7 @@ LAYERS = {
         'start': function (s,c) { return c.getSegStart("4"); },
         'stop': function (s,c) { return c.getSegStop("4"); },
         'className': "seq_layer_highlight",
-        'style': { 'background': "red" },
+        'style': { 'background': "#c44" },
         'enabled': true
     },
     'J':
@@ -54,7 +54,7 @@ LAYERS = {
         'start': function (s,c) { return c.getSegStart("3"); },
         'stop': function (s,c) { return c.getSegStop("3"); },
         'className': "seq_layer_highlight",
-        'style': { 'background': "yellow" },
+        'style': { 'background': "#cc0" },
         'enabled': true
     },
     'D2':
@@ -63,7 +63,7 @@ LAYERS = {
         'start': function (s,c) { return c.getSegStart("4a"); },
         'stop': function (s,c) { return c.getSegStop("4a"); },
         'className': "seq_layer_highlight",
-        'style': { 'background': "red" },
+        'style': { 'background': "#f44" },
         'enabled': true
     },
     'D3':
@@ -72,7 +72,7 @@ LAYERS = {
         'start': function (s,c) { return c.getSegStart("4b"); },
         'stop': function (s,c) { return c.getSegStop("4b"); },
         'className': "seq_layer_highlight",
-        'style': { 'background': "red" },
+        'style': { 'background': "#c44" },
         'enabled': true
     },
     'CDR3':
@@ -81,7 +81,7 @@ LAYERS = {
         'start': function (s,c) { return c.getSegStart("cdr3"); },
         'stop': function (s,c) { return c.getSegStop("cdr3"); },
         'className': "seq_layer_bracket",
-        'style': { 'borderColor': "darkred" },
+        'style': { 'borderColor': "#444" },
         'enabled': false
     },
     
@@ -127,7 +127,7 @@ LAYERS = {
         'start': function (s,c) { return c.seg.imgt["V-REGION start"] - 1; },
         'stop': function (s,c) { return c.seg.imgt["V-REGION end"] - 1; },
         'className': "seq_layer_underline",
-        'style': { 'background': "green" },
+        'style': { 'background': "#484" },
         'enabled': true
     },
     'IMGT_J':
@@ -136,7 +136,7 @@ LAYERS = {
         'start': function (s,c) { return c.seg.imgt["J-REGION start"] - 1; },
         'stop': function (s,c) { return c.seg.imgt["J-REGION end"] - 1; },
         'className': "seq_layer_underline",
-        'style': { 'background': "gold" },
+        'style': { 'background': "#bb0" },
         'enabled': true
     },
     'IMGT_D':
@@ -145,7 +145,7 @@ LAYERS = {
         'start': function (s,c) { return c.seg.imgt["D-REGION start"] - 1; },
         'stop': function (s,c) { return c.seg.imgt["D-REGION end"] - 1; },
         'className': "seq_layer_underline",
-        'style': { 'background': "orange" },
+        'style': { 'background': "#f66" },
         'enabled': true
     },
     'IMGT_D1':
@@ -163,7 +163,7 @@ LAYERS = {
         'start': function (s,c) { return c.seg.imgt["D2-REGION start"] - 1; },
         'stop': function (s,c) { return c.seg.imgt["D2-REGION end"] - 1; },
         'className': "seq_layer_underline",
-        'style': { 'background': "orange" },
+        'style': { 'background': "red" },
         'enabled': true
     },
     'IMGT_D3':
@@ -181,7 +181,7 @@ LAYERS = {
         'start': function (s,c) { return c.seg.imgt["CDR1-IMGT start"] - 1; },
         'stop': function (s,c) { return c.seg.imgt["CDR1-IMGT end"] - 1; },
         'className': "seq_layer_bracket_top",
-        'style': { 'borderColor': "red" },
+        'style': { 'borderColor': "#888" },
         'enabled': false
     },
     'IMGT_CDR2':
@@ -190,7 +190,7 @@ LAYERS = {
         'start': function (s,c) { return c.seg.imgt["CDR2-IMGT start"] - 1; },
         'stop': function (s,c) { return c.seg.imgt["CDR2-IMGT end"] - 1; },
         'className': "seq_layer_bracket_top",
-        'style': { 'borderColor': "red" },
+        'style': { 'borderColor': "#444" },
         'enabled': false
     },
     'IMGT_CDR3':
@@ -199,7 +199,7 @@ LAYERS = {
         'start': function (s,c) { return c.seg.imgt["CDR3-IMGT start"] - 1; },
         'stop': function (s,c) { return c.seg.imgt["CDR3-IMGT end"] - 1; },
         'className': "seq_layer_bracket_top",
-        'style': { 'borderColor': "red" },
+        'style': { 'borderColor': "#444" },
         'enabled': false
     },
     'IMGT_FR1':
@@ -208,7 +208,7 @@ LAYERS = {
         'start': function (s,c) { return c.seg.imgt["FR1-IMGT start"] - 1; },
         'stop': function (s,c) { return c.seg.imgt["FR1-IMGT end"] - 1; },
         'className': "seq_layer_bracket_top",
-        'style': { 'borderColor': "blue" },
+        'style': { 'borderColor': "#aaa" },
         'enabled': false
     },
     'IMGT_FR2':
@@ -217,7 +217,7 @@ LAYERS = {
         'start': function (s,c) { return c.seg.imgt["FR2-IMGT start"] - 1; },
         'stop': function (s,c) { return c.seg.imgt["FR2-IMGT end"] - 1; },
         'className': "seq_layer_bracket_top",
-        'style': { 'borderColor': "blueviolet" },
+        'style': { 'borderColor': "#aaa" },
         'enabled': false
     },
     'IMGT_FR3':
@@ -226,7 +226,7 @@ LAYERS = {
         'start': function (s,c) { return c.seg.imgt["FR3-IMGT start"] - 1; },
         'stop': function (s,c) { return c.seg.imgt["FR3-IMGT end"] - 1; },
         'className': "seq_layer_bracket_top",
-        'style': { 'borderColor': "violet" },
+        'style': { 'borderColor': "#aaa" },
         'enabled': false
     }
 
