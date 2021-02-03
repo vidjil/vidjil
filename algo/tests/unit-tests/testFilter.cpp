bd553aff15fc8b71cf18f0057d90ff6234949e60
Mathieu Giraud <mathieu@vidjil.org>
2021-02-03 20:43:04 +0100

    tests: testFilter.cpp, create only once the filtered automaton for all IGHV tests

--- algo/tests/unit-tests/testFilter.cpp
+++ algo/tests/unit-tests/testFilter.cpp
@@ -351,11 +351,12 @@ void testGetNSignicativeKmers(){
   string SIZE_ERROR = "Filtered BioReader should be 10x smaller";
   string GENE_NOT_FOUND = "Filtering sequence not found after filter";
 
+  FilterWithACAutomaton *f = new FilterWithACAutomaton(seqV, "########");
+
+  // Check filter behaviour for each IGHV gene
   for(int i = 0; i < seqV.size(); ++i){
     Sequence seq = seqV.read(i);
-    FilterWithACAutomaton *f = new FilterWithACAutomaton(seqV, "########");
     filtered = f->filterBioReaderWithACAutomaton(seq.sequence, 1);
-    delete f;
     int j = 0;
     while(j < filtered.size()){
       if(extractGeneName(filtered.label(j)) == extractGeneName(seq.label)){
@@ -366,6 +367,7 @@ void testGetNSignicativeKmers(){
     TAP_TEST(j < filtered.size(), TEST_FILTER_BIOREADER_WITH_AC_AUTOMATON, GENE_NOT_FOUND);
     TAP_TEST(filtered.size() < seqV.size() / 10, TEST_FILTER_BIOREADER_WITH_AC_AUTOMATON, SIZE_ERROR + ", " + seq.label + ", " + string_of_int(filtered.size()) + "/" + string_of_int(seqV.size()));
   }
+  delete f;
 }
 
 /*
