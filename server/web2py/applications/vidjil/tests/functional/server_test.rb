d5f5443acf31ecd92179fa7c3d5093607c04dcc4
flothoni <--help>
2021-02-18 16:31:24 +0100

    server_test.rb; change the default user as plop@plop.com

--- server/web2py/applications/vidjil/tests/functional/server_test.rb
+++ server/web2py/applications/vidjil/tests/functional/server_test.rb
@@ -12,7 +12,7 @@ class ServerTest < BrowserTest
 
 
   def logins(user, field)
-    data = { "admin" => {"user" => "test@vidjil.org",  "password" => "test"},
+    data = { "admin" => {"user" => "plop@plop.com",    "password" => "foobartest"},
              "user0" => {"user" => "email0@local.org", "password" => "password0", "first" => "first_name0", "last" => "last_name0", "new_password" => "password0mod"},
              "user1" => {"user" => "email1@local.org", "password" => "password1", "first" => "first_name1", "last" => "last_name1"},
              "user2" => {"user" => "email2@local.org", "password" => "password2", "first" => "first_name2", "last" => "last_name2"}
