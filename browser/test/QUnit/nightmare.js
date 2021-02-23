401738d9f856a29aae47fba092e8e58fd9fc1969
marc duez <replayskevitch@gmail.com>
2021-02-23 17:16:42 +0100

    nightmare.js : wait for lib loading

--- browser/test/QUnit/nightmare.js
+++ browser/test/QUnit/nightmare.js
@@ -5,9 +5,11 @@ var nightmare = Nightmare({ show: false });
 var curdir = __dirname;
 var uri = 'file://' + curdir;
 
+
 nightmare
     .on('console', function(){ console.log(arguments[1]) })
     .goto(uri+"/test_Qunit.html")
+    .wait(1000)
     .wait(function() {
         return $('.qunit-fail').length > 0 || $('.qunit-pass').length > 0;
         })
