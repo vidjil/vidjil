a6171a9ce04bc76407af0e022d2241536c2e9f18
flothoni <--help>
2021-02-24 12:34:43 +0100

    issue 4070; add data and test Verify that analysis with null diversity value can be opened Link to #4070

---
+++ browser/test/functional/issues/test_4070.rb
@@ -0,0 +1,27 @@
+# coding: utf-8
+load 'vidjil_browser.rb'
+load 'browser_test.rb'
+
+#browser test suite
+class TestGraph < BrowserTest
+
+  def setup
+    super
+    if not defined? $b
+      set_browser("/browser/test/data/issues/4070_diversity_null.vidjil")
+    end
+  end
+
+  def test_00_open_sample_info
+    $b.info_point.i.click
+    info_timepoint = $b.div(id: "info_timepoint")
+    # timepoint information panel is open and show correct informations
+    assert( info_timepoint.text.include? "Ds_diversity")    
+    assert( info_timepoint.text.include? "null")    
+  end
+  
+  # Not really a test
+  def test_zz_close
+    close_everything
+  end
+end
