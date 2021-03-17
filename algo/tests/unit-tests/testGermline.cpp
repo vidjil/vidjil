c200f8cdc4a72c326fd3af3cbd2c5c3929965c37
Mathieu Giraud <mathieu@vidjil.org>
2021-03-17 12:30:45 +0100

    tests: update index load
    We act that's inflating.

--- algo/tests/unit-tests/testGermline.cpp
+++ algo/tests/unit-tests/testGermline.cpp
@@ -10,14 +10,14 @@ using namespace std;
 
 void testGermline1(Germline *g1)
 {
-    
+
   // Test metadata
   TAP_TEST_EQUAL(g1->code, "IGH", TEST_GERMLINE, "code") ;
   TAP_TEST_EQUAL(g1->shortcut, 'G', TEST_GERMLINE, "shortcut") ;
-                    
+
   // Test seeds
   TAP_TEST_EQUAL(g1->seed_5, "####-####", TEST_GERMLINE, "seed_5") ;
-  TAP_TEST_EQUAL(g1->seed_4, "###-###", TEST_GERMLINE, "seed_4") ;  
+  TAP_TEST_EQUAL(g1->seed_4, "###-###", TEST_GERMLINE, "seed_4") ;
   TAP_TEST_EQUAL(g1->seed_3, "#######", TEST_GERMLINE, "seed_3") ;
 }
 
@@ -25,10 +25,10 @@ void testIndexLoad(Germline *g1, IndexTypes index, float expected_index_load)
 {
   g1->new_index(index);
   g1->finish();
-  
+
   size_t seed_5_span = g1->seed_5.size();
   float index_load_5 = g1->index->getIndexLoad(KmerAffect(g1->affect_5, 1, seed_5_span));
-  
+
   TAP_TEST_APPROX(index_load_5, expected_index_load, 0.02, TEST_GERMLINE, "seed_5, index_load") ;
 }
 
@@ -45,12 +45,10 @@ void testGermline() {
      "4", {"seed", "10s", "code", "D", "build", "0"},
      "3", {"seed", "8s", "code", "J", "build", "0"}});
 
-  TAP_TEST_EQUAL(g1->code, "IGH", TEST_GERMLINE, "");
-  // testGermline1(g1);
-  // testIndexLoad(g1, KMER_INDEX, 0.24);
-  // delete g1->index;
-  // // TODO: Tests to really check the index load (with toy examples)
-  // testIndexLoad(g1, AC_AUTOMATON, 0.33);
-  
+  testGermline1(g1);
+  testIndexLoad(g1, KMER_INDEX, 0.40);
+  delete g1->index;
+  testIndexLoad(g1, AC_AUTOMATON, 0.20);
+
   delete g1;
 }
