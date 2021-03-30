b6e271e60ac6acb70235c125e95db17c40893a38
marc duez <replayskevitch@gmail.com>
2021-03-30 17:05:52 +0200

    aligner : external_test

--- browser/test/functional/external_test_imgt-igblast.rb
+++ browser/test/functional/external_test_imgt-igblast.rb
@@ -33,7 +33,7 @@ class ExternalTest < BrowserTest
         assert ($b.text.include? "Homsap TRBJ2-5*01"), ">> IMGT expected J not found"
       end
       
-      $b.window(:title => "").use
+      $b.window(:title => "").use 
       
       $b.window(:title => "IMGT/V-QUEST").close
 
@@ -49,6 +49,8 @@ class ExternalTest < BrowserTest
         assert ($b.text.include? "Homsap IGHJ6*02"), ">> IMGT expected J not found"
       end
       $b.window(:title => "").use
+      
+      $b.window(:title => "IMGT/V-QUEST").close
 
     end
   end
@@ -57,20 +59,21 @@ class ExternalTest < BrowserTest
     begin
       $b.clone_in_scatterplot('25').click
       $b.update_icon.wait_while(&:present?)
-      $b.span(:id => "toIMGTSeg" ).click
-      $b.until { $b.segmenter_checkbox_imgt_vdj.present? }
+      $b.div(:id => "align-imgt" ).hover
+      $b.input(:id => "aligner_checkbox_IMGT_CDR" ).click
+      clone_segmenter = $b.clone_in_segmenter('25')
+      clone_segmenter.span(:class => 'seq_layer_IMGT_CDR3').wait_while(&:present?) 
 
       clone_info = $b.clone_info_segmenter('25')
-      productive_title = clone_info[:axis].element(:class => ['productivity', 'IMGT']).title
+      productive_title = clone_info[:axis].element(:class => 'productivity').title
       assert (productive_title.include? 'productivity'), "IMGT should tell us the productivity of the sequence"
 
-      clone_segmenter = $b.clone_in_segmenter('25')
-      $b.segmenter_checkbox_imgt_vdj.click
-      highlights = clone_segmenter.spans(:class => 'highlight_border')
-      assert (highlights.size >= 2 && highlights.size <= 3), "We should have the V(D)J genes highlighted, we had %d highlights" % highlights.size
-      for h in highlights
-        assert (h.style('width').to_i >= 100), "Highlights should have a reasonable width, found to be %s" % h.style('width')
-      end
+      $b.div(:id => "align-imgt" ).hover
+      # $b.a(:id => "aligner_checkbox_IMGT_VDJ" ).click #checkbox already checked by default
+      $b.div(:id => "align-imgt" ).click
+      $b.update_icon.wait_while(&:present?)
+      highlight_V = $b.clone_in_segmenter('25').div(:class => 'seq_layer_IMGT_V')
+      assert (highlight_V.style('width').to_i >= 100), "V should have a reasonable width, found to be %s" % highlight_V.style('width')
 
       Watir::Wait.until(timeout: 10) { clone_info[:identity].span.exists? }
       clone_info[:identity].element(:text => "NaN%").wait_while(&:present?)
@@ -86,7 +89,8 @@ class ExternalTest < BrowserTest
       $b.clone_in_scatterplot('25').wait_until(&:present?)
       $b.clone_in_scatterplot('25').click
       $b.update_icon.wait_while(&:present?)
-      $b.span(:id => "toIgBlast" ).click
+      $b.div(:id => "align-external-tool" ).hover
+      $b.a(:id => "toIgBlast" ).click
       
       assert ( $b.window(:title => "IgBLAST Search Results").exists? ) , ">> fail opening igblast "
       $b.window(:title => "IgBLAST Search Results").use do
@@ -101,7 +105,8 @@ class ExternalTest < BrowserTest
 
       $b.clone_in_scatterplot('26').click
       $b.update_icon.wait_while(&:present?)
-      $b.span(:id => 'toIgBlast').click
+      $b.div(:id => "align-external-tool" ).hover
+      $b.a(:id => "toIgBlast" ).click
       assert ( $b.window(:title => "IgBLAST Search Results").exists? ) , ">> fail opening second igblast "
       $b.window(:title => "IgBLAST Search Results").use do
         assert ($b.text.include? "Length=318"), ">> igblast: was not launched on the correct sequence"
