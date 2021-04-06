3c6ed6762d72ce834a06dc59d9af1cbea9aacdc2
flothoni <--help>
2021-04-06 10:43:26 +0200

    server_test.rb; comment testing order function (to run in random order) Now tests will be run in writing order

--- server/web2py/applications/vidjil/tests/functional/server_test.rb
+++ server/web2py/applications/vidjil/tests/functional/server_test.rb
@@ -1,9 +1,10 @@
 load 'browser_test.rb'
 class ServerTest < BrowserTest
 
-  def self.test_order
-    :random
-  end
+  ## Comment to allow test to run in writing order; allow to debug error
+  # def self.test_order
+  #   :random
+  # end
 
 end
 
