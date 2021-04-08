620716f3d6d2c33f379c868e0f19096d801dbec3
flothoni <--help>
2021-04-08 07:46:23 +0200

    server-test.rb; add login table for testing purpose

--- server/web2py/applications/vidjil/tests/functional/server_test.rb
+++ server/web2py/applications/vidjil/tests/functional/server_test.rb
@@ -5,6 +5,21 @@ class ServerTest < BrowserTest
   # def self.test_order
   #   :random
   # end
+  
+  def datapath filename
+    File.expand_path(File.join(File.dirname(__FILE__), filename))
+  end
+
+
+  def logins(user, field)
+    data = { "admin" => {"user" => "test@vidjil.org",  "password" => "test"},
+             "user0" => {"user" => "email0@local.org", "password" => "password0", "first" => "first_name0", "last" => "last_name0", "new_password" => "password0mod"},
+             "user1" => {"user" => "email1@local.org", "password" => "password1", "first" => "first_name1", "last" => "last_name1"},
+             "user2" => {"user" => "email2@local.org", "password" => "password2", "first" => "first_name2", "last" => "last_name2"}
+           }
+    return data.fetch(user).fetch(field)
+  end
+
 
 end
 
