2aea085d5896c9aa11d568d68b8b55ddea8f9c6a
flothoni <--help>
2021-04-15 19:44:29 +0200

    bioseq; add and update tests

--- browser/test/QUnit/testFiles/lib/bioseq_test.js
+++ browser/test/QUnit/testFiles/lib/bioseq_test.js
@@ -6,22 +6,37 @@ QUnit.module("lib_bioseq", {
 
 QUnit.test("simple alignment", function(assert) {
 
-    var sequence = "catcatcatgatgctacg" // clone test5; pos 6 in list
-    var mismatch = "catcGtcaCCatgctacg" // 3x mismatch
+    var sequence    = "catcatcatgatgctacg" // clone test5; pos 6 in list
+    var mismatch_1  = "catcGtcatgatgctacg" // 1x mismatch
+    var mismatch_3  = "catcGtcaCgatcctacg" // 3x mismatch
+    var mismatch_3g = "catcGtcaCCCtgctacg" // 3x mismatch; 1gap
+
+    var matrix = [ 1, -2] //square score matrix or [match,mismatch] array
+    var gapsc  = [-2, -1] //[gap_open,gap_ext] array; k-length gap costs gap_open+gap_ext*k
 
     // identical sequences
-    var rst = bsa_align(true, sequence, sequence, [1, -2], [-2, -1]) // return [score, start pos, ~cigar]
-    var nb_match = bsa_cigar2match(rst[2])
-    var cigar    = bsa_cigar2str(rst[2])
-    assert.equal(18, nb_match, 'Correct alignment')
-    assert.equal("18M", cigar, 'Correct cigar')
+    var rst = bsa_align(true, sequence, sequence, matrix, gapsc) // return [score, start pos, ~cigar]
+    assert.equal("18M", bsa_cigar2str(rst[2]), 'Correct cigar')
+    assert.equal(18, rst[0], 'Correct score if perfect match') // +18*1
+    console.log( rst )
 
-    // // sequences with 3 mimsmatch
-    // var rst = bsa_align(true, sequence, mismatch, [1, -2], [-2, -1]) // return [score, start pos, ~cigar]
-    // var nb_match = bsa_cigar2match(rst[2])
-    // var cigar    = bsa_cigar2str(rst[2])
-    // assert.equal(15, nb_match, 'Correct alignment')
-    // assert.equal("5M1S3M2S8M", cigar, 'Correct cigar')
+
+    // sequences with 1 mimsmatch
+    var rst = bsa_align(true, sequence, mismatch_1, matrix, gapsc) // return [score, start pos, ~cigar]
+    assert.equal("18M", bsa_cigar2str(rst[2]), 'Correct cigar')
+    assert.equal(15, rst[0], 'Correct score if 1 mismatch') // +17*1 -2*1
+
+    // sequences with 3 mimsmatch
+    var rst = bsa_align(true, sequence, mismatch_3, matrix, gapsc) // return [score, start pos, ~cigar]
+    assert.equal("18M", bsa_cigar2str(rst[2]), 'Correct cigar')
+    assert.equal(9, rst[0], 'Correct score if 3 mismatch') // +15*1 -2*3
+    console.log( rst )
+
+    // sequences with 1 mimsmatch+1gap
+    var rst = bsa_align(true, sequence, mismatch_3g, matrix, gapsc) // return [score, start pos, ~cigar]
+    assert.equal("11S7M", bsa_cigar2str(rst[2]), 'Correct cigar')
+    assert.equal(7, rst[0], 'Correct score if 1 mismatch + 1gap (len3)') // +14*1 -2*1 -2-1-1
+    console.log( rst )
 
 })
 
