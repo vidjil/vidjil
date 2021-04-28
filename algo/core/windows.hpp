203f700101210ea7fced654abb60e54a0537ffe6
Mathieu Giraud <mathieu@vidjil.org>
2021-04-28 07:53:16 +0200

    core/windows.cpp: do not display W50 on stdout
    Such warnings are now collected once all the clones have been processed.
    It's too late to show them on stdout.
    
    see #4425

--- algo/core/windows.hpp
+++ algo/core/windows.hpp
@@ -295,7 +295,7 @@ void WindowsStorage<Tshortcut, Affect>::sortedWindowsToOutput(SampleOutput *outp
     CloneOutput *clone = output->getClone(it->first);
 
     if (status_by_window[it->first][SEG_CHANGED_WINDOW])
-      clone->add_warning(W50_WINDOW, "Short or shifted window", LEVEL_WARN);
+      clone->add_warning(W50_WINDOW, "Short or shifted window", LEVEL_WARN, false);
 
     clone->set("id", it->first);
     clone->set("reads", { it->second });
