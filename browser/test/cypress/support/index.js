b3944d539107fcbbc11672c20fa253521bbd36af
flothoni <florian.thonier.pro@gmail.com>
2021-07-30 14:07:17 +0200

    cypress server; add function to keep cookies and session between tests

--- browser/test/cypress/support/index.js
+++ browser/test/cypress/support/index.js
@@ -38,3 +38,22 @@ before(function() {
     cy.initDatabase(Cypress.env('host'))
 })
 
+
+afterEach(() => {
+  //Code to Handle the Sesssions in cypress.
+  //Keep the Session alive when you jump to another test
+  let str = [];
+  cy.getCookies().then((cook) => {
+      cy.log(cook);
+      for (let l = 0; l < cook.length; l++) {
+          if (cook.length > 0 && l == 0) {
+              str[l] = cook[l].name;
+              Cypress.Cookies.preserveOnce(str[l]);
+          } else if (cook.length > 1 && l > 1) {
+              str[l] = cook[l].name;
+              Cypress.Cookies.preserveOnce(str[l]);
+          }
+      }
+    }
+  )
+})
