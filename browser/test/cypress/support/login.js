623b8db850a32bba1d6f2d426cce5b6a46248965
Thonier Florian <florian@vidjil.org>
2021-11-17 11:28:06 +0100

    cypress; refactor login to remove duplicate code

--- browser/test/cypress/support/login.js
+++ browser/test/cypress/support/login.js
@@ -4,43 +4,30 @@ Cypress.Commands.add('login', (host) => {
   if (host=="local"){
       cy.visit('http://localhost/browser')
       cy.update_icon()
-      // log in
-      cy.get('#auth_user_email', { timeout: 10000 })
-        .should('exist')
-        .should('be.visible')
-        .type('plop@plop.com')
-      cy.get('#auth_user_password')
-        .type('foobartest')
-      cy.get('#submit_record__row > .w2p_fw > input').click()
-      cy.update_icon()
+      cy.fillLogin('plop@plop.com','foobartest')
   } else if (host=="review"){
     cy.visit(Cypress.env('URL'))
     cy.update_icon()
-    // log in
-    cy.get('#auth_user_email', { timeout: 10000 })
-      .should('exist')
-      .should('be.visible')
-      .type('test@vidjil.org')
-    cy.get('#auth_user_password')
-      .type(Cypress.env('CI_PASSWORD_TEST'))
-    cy.get('#submit_record__row > .w2p_fw > input').click()
-    cy.update_icon()
+    cy.fillLogin('test@vidjil.org',Cypress.env('CI_PASSWORD_TEST'))
   } else if (host=="app"){
     cy.visit('https://app.vidjil.org/')
     cy.update_icon()
-    
-    // log in
+    cy.fillLogin('demo@vidjil.org','demo')
+  }
+})
+
+
+
+Cypress.Commands.add('fillLogin', (user, password) => { 
     cy.get('#auth_user_email', { timeout: 10000 })
-      .should('exist')
-      .should('be.visible')
-      .type('demo@vidjil.org')
+      .should('exist').should('be.visible')
+      .type(user)
     cy.get('#auth_user_password')
-      .type('demo')
-      // .type('demo')
+      .type(password)
     cy.get('#submit_record__row > .w2p_fw > input').click()
     cy.update_icon()
-  }
-  cy.verifyLogin()
+
+    cy.verifyLogin()
 })
 
 
