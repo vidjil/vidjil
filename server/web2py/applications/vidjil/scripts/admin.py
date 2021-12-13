8fe35ccb4531a945485d54df20544b078ac92e29
Mikael Salson <mikael.salson@univ-lille1.fr>
2021-12-13 12:39:46 +0100

    admin.py: make reusable function

--- server/web2py/applications/vidjil/scripts/admin.py
+++ server/web2py/applications/vidjil/scripts/admin.py
@@ -1,12 +1,9 @@
 from __future__ import print_function
 import sys
 
-if len(sys.argv) == 1 or sys.argv[0] == "-h" or sys.argv[0] == "--help":
-    print("Usage: %s <admin_function> [parameters]\n\nExec a function from the admin controller)." % sys.argv[0])
-    exit(1)
 execfile("applications/vidjil/controllers/admin.py", globals())
 
-if __name__ == '__main__':
+def admin(function, args):
     auth.user = db.auth_user[1]
     auth.user.first_name = 'CLI'
     auth.user.last_name = 'admin.py'
@@ -14,9 +11,16 @@ if __name__ == '__main__':
     
     session.auth = []
     function = sys.argv[1]
-    if len(sys.argv) == 2:
+
+    if len(args) == 0:
         params = ''
     else:
-        params = ', '.join(sys.argv[2:])
-
+        params = ', '.join(args)
+    
     exec('{}({})'.format(function,params))
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1 or sys.argv[0] == "-h" or sys.argv[0] == "--help":
+        print("Usage: %s <admin_function> [parameters]\n\nExec a function from the admin controller)." % sys.argv[0])
+        exit(1)
+    admin(sys.argv[1], sys.argv[2:])
