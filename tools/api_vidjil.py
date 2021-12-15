3c6d60c5c7630f60ede4eb5df0d28f7a2dee5b71
Thonier Florian <florian@vidjil.org>
2021-12-15 21:20:27 +0100

    api; add a function to replace spec caracter

--- tools/api_vidjil.py
+++ tools/api_vidjil.py
@@ -14,6 +14,28 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 url = "https://localhost/vidjil/"
+def prettyUrl(string):
+
+    specChars = {
+        ' ': '%20',    '!': '%21',    '"': '%22',
+        '#': '%23',    '$': '%24',    '%': '%25',
+        '&': '%26',    '\'': '%27',   '(': '%28',
+        ')': '%29',    '*': '%2A',    '+': '%2B',
+        ',': '%2C',    '-': '%2D',    '.': '%2E',
+        '/': '%2F',    ':': '%3A',    ';': '%3B',
+        '<': '%3C',    '=': '%3D',    '>': '%3E',
+        '?': '%3F',    '@': '%40',    '[': '%5B',
+        '\\': '%5C',   ']': '%5D',    '^': '%5E',
+        '_': '%5F',    '`': '%60',    '{': '%7B',
+        '|': '%7C',    '}': '%7D',    '~': '%7E'
+    }
+    new_string = ""
+    for cara in string:
+        if cara in specChars.keys():
+            new_string += specChars[cara]
+        else:
+            new_string += cara
+    return new_string
 
 class Vidjil:
 
