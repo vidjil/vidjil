6eb6d3e7b64c57a6ff1a564a2022730f2e932b24
Thonier Florian <florian@vidjil.org>
2022-01-19 12:22:43 +0100

    model.js; change behavior to be able to get diversity splitted by locus also rename diversity key

--- browser/js/model.js
+++ browser/js/model.js
@@ -729,8 +729,18 @@ changeAlleleNotation: function(alleleNotation, update, save) {
             } else if (typeof this.diversity[key][time] != 'undefined') {
         	    // Diversity may not be stored in an Array for retrocompatiblitiy reasons
         	    // See #1941 and #3416
-        		if (this.diversity[key][time] != null) {
-                    return this.diversity[key][time].toFixed(3);
+        		if (this.diversity[key][time] != null){
+                    if (typeof this.diversity[key][time] == "number") {
+                        // old case, one global diversity
+                        return this.diversity[key][time].toFixed(3);
+                    } else {
+                        // case diversity by locus
+                        var diversity_by_locus = {}
+                        for (var locus in this.diversity[key][time]) {
+                            diversity_by_locus[locus] = this.diversity[key][time][locus].toFixed(3)
+                        }
+                        return diversity_by_locus
+                    }
                 } else {
                     return this.diversity[key][time]
                 }
@@ -1563,9 +1573,17 @@ changeAlleleNotation: function(alleleNotation, update, save) {
 
         // Sub-table diversity
         if ( typeof this.diversity != 'undefined') {
-            html += "<tr><td class='header' "+colspan_header+"> diversity </td></tr>"
+            html += "<tr><td class='header' "+colspan_header+"> Diversity indices </td></tr>"
             for (var key_diversity in this.diversity) {
-                html += "<tr><td> " + key_diversity.replace('index_', '') + "</td><td>" + this.getDiversity(key_diversity, timeID) + '</td></tr>'
+                var diversity = this.getDiversity(key_diversity, timeID)
+                if (typeof diversity == "string"){
+                    html += "<tr><td> " + translate_key_diversity(key_diversity) + "</td><td>" + diversity + '</td></tr>'
+                } else if (typeof diversity == "object"){
+                    html += "<tr><td "+colspan_header+">"+translate_key_diversity(key_diversity)+"</td></tr>"
+                    for (var locus in diversity) {
+                        html += "<tr><td> " + this.systemBox(locus).outerHTML + " "+locus+"</td><td>" + diversity[locus] + '</td></tr>'
+                    }
+                }
             }
         }
         if ( typeof this.samples.diversity != 'undefined' && typeof this.samples.diversity[timeID] != 'undefined') {
