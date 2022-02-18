585aa10cf12e8f918cb59496942f748f8b0bae67
marc duez <replayskevitch@gmail.com>
2022-02-18 14:31:45 +0100

    test_aligner.js : add external alignement tool tests IMGT / IgBlast / Blast / AssignSubset

--- browser/test/cypress/integration/test_aligner.js
+++ browser/test/cypress/integration/test_aligner.js
@@ -173,6 +173,122 @@ describe('Aligner', function () {
     return
   })
 
+
+  it('Aligner send to IMGT',{ browser: 'chrome' },  function() {
+    cy.openAnalysis("doc/analysis-example.vidjil")
+
+    //select clone
+    cy.get('#listElem_25').click()
+
+    //disable open in another tab
+    cy.get('#post_target_blank').click({force:true})
+
+    //toIMGT
+    cy.get('#toIMGT').click({force:true})
+
+    //check content
+    cy.title().should('eq', 'IMGT/V-QUEST')
+    cy.get('.sequences_number').contains("Number of analysed sequences: 1")
+    cy.get('.summary_synthesis').contains("Homsap TRBV28*01 F")
+    cy.get('.summary_synthesis').contains("Homsap TRBJ2-5*01 F")
+
+    cy.go('back')
+    cy.setBrowser(url)
+    cy.openAnalysis("doc/analysis-example.vidjil")
+
+    //select clone
+    cy.get('#listElem_26').click()
+
+    //disable open in another tab
+    cy.get('#post_target_blank').click({force:true})
+
+    //toIMGT
+    cy.get('#toIMGT').click({force:true})
+
+    //check content
+    cy.title().should('eq', 'IMGT/V-QUEST')
+    cy.get('.sequences_number').contains("Number of analysed sequences: 1")
+    cy.get('.summary_synthesis').contains("Homsap IGHV3-9*01")
+    cy.get('.summary_synthesis').contains("Homsap IGHJ6*02")
+
+    return
+  })
+
+
+  it('Aligner send to IgBlast',{ browser: 'chrome' },  function() {
+    cy.openAnalysis("doc/analysis-example.vidjil")
+
+    //select clone
+    cy.get('#listElem_25').click()
+
+    //disable open in another tab
+    cy.get('#post_target_blank').click({force:true})
+
+    //toIgBlast
+    cy.get('#toIgBlast').click({force:true})
+
+    cy.title().should('eq', 'IgBLAST Search Results')
+    cy.get('pre').contains("Length=180")
+    cy.get('pre').contains("TRBV28*01")
+    cy.get('pre').contains("TRBJ2-5*01")
+
+    cy.go('back')
+    cy.setBrowser(url)
+    cy.openAnalysis("doc/analysis-example.vidjil")
+
+    //select clone
+    cy.get('#listElem_26').click()
+
+    //disable open in another tab
+    cy.get('#post_target_blank').click({force:true})
+
+    //toIgBlast
+    cy.get('#toIgBlast').click({force:true})
+
+    cy.title().should('eq', 'IgBLAST Search Results')
+    cy.get('pre').contains("Length=318")
+    cy.get('pre').contains("IGHV3-9*01")
+    cy.get('pre').contains("IGHJ6*02")
+
+    return
+  })
+
+  it('Aligner send to Blast',{ browser: 'chrome' },  function() {
+    cy.openAnalysis("doc/analysis-example.vidjil")
+
+    //select clone
+    cy.get('#listElem_25').click()
+
+    //disable open in another tab
+    cy.get('#post_target_blank').click({force:true})
+
+    //toIgBlast
+    cy.get('#toBlast').click({force:true})
+
+    cy.title().should('eq', 'BLAST/BLAT search - Homo_sapiens - Ensembl genome browser 105')
+    cy.get('.seq-wrapper').contains('>25#TRBV29*01 -1/0/-0 TRBD1*01 -2/0/-5 TRBJ2-5*01')
+
+    return
+  })
+
+  it('Aligner send to Arrest',{ browser: 'chrome' },  function() {
+    cy.openAnalysis("doc/analysis-example.vidjil")
+
+    cy.get('#listElem_26').click()
+
+    // disable open in another tab
+    cy.get('#post_target_blank').click({force:true})
+
+    // send to Arrest/AssignSbset
+    cy.get('#toAssignSubsets').click({force:true})
+
+    cy.title().should('eq', 'ARResT/AssignSubsets @ the BAT cave | results')
+    cy.get('fieldset').contains("0 / 1 / 1 were assigned / 'healthy' / submitted")
+    cy.get('fieldset').contains('26#IGHV3-9*01_7/CCCGGA/17_IGHJ6*02')
+
+    return
+  })
+
   //menu in aligner top right corner (focus/hide/tag) are already tested in test_filter.js
 
 })
