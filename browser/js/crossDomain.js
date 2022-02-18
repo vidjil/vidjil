b19adf3deccd88e31807ed29ab361456ce39527b
marc duez <replayskevitch@gmail.com>
2022-02-18 14:30:14 +0100

    crossdomain.js: add post request target selector for cypress tests

--- browser/js/crossDomain.js
+++ browser/js/crossDomain.js
@@ -78,6 +78,15 @@ function initIgBlastInput() {
     return igBlastInput;
 }
 
+function postTarget(){
+    checkbox = $("#post_target_blank")[0]
+
+    if (checkbox && checkbox.checked)
+        return "_blank"
+    else 
+        return "_self"
+
+}
 
 function imgtPost(species, data, system) {
     var imgtInput = initImgtInput(species);
@@ -91,7 +100,7 @@ function imgtPost(species, data, system) {
     }
     var form = document.getElementById("form");
     form.removeAllChildren();
-    form.target = "_blank";
+    form.target = postTarget()
     form.action = "https://www.imgt.org/IMGT_vquest/analysis";
     form.method = "POST";
 
@@ -251,7 +260,7 @@ function igBlastPost(species, data, system) {
 
     var form = document.getElementById("form");
     form.removeAllChildren();
-    form.target = "_blank";
+    form.target = postTarget();
     form.action = "https://www.ncbi.nlm.nih.gov/igblast/igblast.cgi";
     form.method = "POST";
 
@@ -281,7 +290,7 @@ function arrestPost(species, data, system) {
 
     var form = document.getElementById("form");
     form.removeAllChildren();
-    form.target = "_blank";
+    form.target = postTarget();
     form.action = "http://tools.bat.infspire.org/cgi-bin/arrest/compile.junctions.online.pl";
     form.method = "POST";
 
@@ -329,7 +338,7 @@ function blastPost(species, data, system) {
 
     var form = document.getElementById("form");
     form.removeAllChildren();
-    form.target = "_blank";
+    form.target = postTarget();
     form.action = "http://www.ensembl.org/Multi/Tools/Blast?db=core";
     form.method = "POST";
 
@@ -353,7 +362,7 @@ function assignSubsetsPost(species, data, system) {
     } else {
         var form = document.getElementById("form");
         form.removeAllChildren();
-        form.target = "_blank";
+        form.target = postTarget();
         form.enctype = 'multipart/form-data';
         form.name = 'assignsubsets';
         form.action = getProxy()+"assign_subsets";
