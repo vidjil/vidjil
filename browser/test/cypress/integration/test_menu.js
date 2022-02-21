e313f1ff6ef3b941afc33eceee9f20780c5768a3
Thonier Florian <florian@vidjil.org>
2022-02-21 23:53:53 +0100

    trash watir; translate test_settings.rb

--- browser/test/cypress/integration/test_menu.js
+++ browser/test/cypress/integration/test_menu.js
@@ -5,6 +5,38 @@ console.log( Cypress.env('workdir') )
 var url = "./"+ Cypress.env('workdir')+"/browser/index.html"
 console.log( url )
 
+
+
+
+function get_names(pos, type){
+    // names in various format
+    if (pos == "0"){
+        if (type == "name") {return 'T8045-BC081-Diag'}
+        else if (type == "short_name" || type == "local_storage") {return 'T8045-BC'}
+        else if (type == "sampling_date") { return '2019-12-17' }
+        else if (type == "delta_date") { return '2019-12-17' }
+    } else if (pos == "1"){
+        if (type == "name") { return 'T8045-BC082-fu1' }
+        else if (type == "short_name" || type == "local_storage") { return 'T8045-BC' }
+        else if (type == "sampling_date") { return '2019-12-27' }
+        else if (type == "delta_date") { return '+10' }
+    }
+}
+
+
+function test_name_values(type_name){
+    cy.get('#visu2_menu').click()
+    // By default, 2 samples are present in timeline graph
+    cy.log("Test for: " + type_name)
+    // In graph label
+    cy.get('#time0').should("have.text", get_names("0", type_name), "incorrect name show for first sample (graph label)")
+    cy.get('#time1').should("have.text", get_names("1", type_name), "incorrect name show for second sample (graph label)")
+    // In graph list
+    cy.get('#visu2_listElem_text_0').should("have.text", get_names("0", type_name), "incorrect name show for first sample (graphList text)")
+    cy.get('#visu2_listElem_text_1').should("have.text", get_names("1", type_name), "incorrect name show for second sample (graphList text)")
+}
+
+
 // This script allow to make some action in a sandbox to quicly change made on the client when you code
 describe('Test sandbox', function () {
     beforeEach(function () {
@@ -51,5 +83,32 @@ describe('Test sandbox', function () {
 
   })
 
+
+
+
+  it('01-sample_name',  function() {
+    cy.openAnalysis("/doc/analysis-example2.vidjil")
+
+    // def test_01_names_sample
+    var type_name = "sampling_date"
+    cy.change_name_key(type_name)
+    test_name_values(type_name)
+
+    // def test_02_names_short
+    type_name = "short_name"
+    cy.change_name_key(type_name)
+    test_name_values(type_name)
+
+    // def test_03_names
+    type_name = "name"
+    cy.change_name_key(type_name)
+    test_name_values(type_name)
+
+    // def test_04_delta_date
+    type_name = "delta_date"
+    cy.change_name_key(type_name)
+    test_name_values(type_name)
+
+  })
 
 })
--- browser/test/cypress/support/menu.js
+++ browser/test/cypress/support/menu.js
@@ -8,3 +8,9 @@ Cypress.Commands.add('close_menu', () => {
   cy.get('body').click()
   cy.get('#patient_info_text').click()
 })
+
+Cypress.Commands.add('change_name_key', (type_name) => {
+  cy.open_menu_settings()
+  cy.get('#menuTimeForm_'+type_name).click()
+  cy.get('#visu2_menu').click()
+})
