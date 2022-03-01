f0b75a39cf652a98cfd3502fafb8933d06e489cd
Thonier Florian <florian@vidjil.org>
2022-03-01 17:20:06 +0100

    api_demo.py; invert complex example

--- tools/api_demo.py
+++ tools/api_demo.py
@@ -17,30 +17,29 @@ if  __name__ =='__main__':
     # Define a vidjil object with at least an url and ssl certificat file
     vidjil = Vidjil(url, ssl=certificat)
     # Login to the server by giving a user email and password
-    vidjil.login("user.email@society.com", "password")
+    vidjil.login("demo@vidjil.org", "demo")
 
-    ### Define a set of id and config
-    sample_set_id = 182
-    config_id     = 2
+    ### Define a set of id and config
+    sample_set_id = 25736  # Demo lil L3
+    config_id     = 2      # multi+inc+xxx
     
     
     ### Get a set from server by is id and set type
-    set182 = vidjil.getSamplesetById(sample_set_id, "patient")
-    print( set182 )
+    set25736 = vidjil.getSamplesetById(sample_set_id, "patient")
+    print( set25736 )
 
     ### Get status of sample of this set
     status        = vidjil.launchAnalysisOnSet(sample_set_id, config_id)
-    # {'set_182': {'sample_231': 'COMPLETED', 'sample_232': 'COMPLETED'}}
     print( status)
  
-    ### Same as previous, but on a list of sets
-    status        = vidjil.launchAnalysisBunchesSet(list_sets = [180, 182], config_id)
-    # {'set_180': {'sample_224': 'COMPLETED', 'sample_225': 'COMPLETED'}, 'set_182': {'sample_231': 'COMPLETED', 'sample_232': 'COMPLETED'}}
+    ### Same as previous, but on a list of sets
+    status        = vidjil.launchAnalysisBunchesSet(list_sets = [25736,  3296], config_id) # Lil-L3 et Lil-L4
     print( status)
 
 
     ### From API, you could create patient, run or generic set.
-    # Some parameters are mandatory (WARNING: These sets will be created on the server. Don't spam your server)
+    # Some parameters are mandatory
+    # WARNING: These sets will be created on the server. Don't spam your server
     vidjil.createPatient("first_name", "last_name", info="some informations and #tags")
     vidjil.createRun("run_name", run_date="2022-01-01")
     vidjil.createSet("set_api", info="Set to group sample with XXX protocol")
@@ -57,18 +56,37 @@ if  __name__ =='__main__':
 
 
 
-
     # ################################
     # #### More complex examples
     # ################################
     
-    ### Example 1; creating a set, upload data and launch analysis
+
+
+    ###################################################################
+    ### Example 1: download results from a set for a configuration id.
+    path_out = "output_dir/" # be sure to create this directory first
+    
+
+    # Get information from server about set and samples (we reuse here the set id previously created)
+    sampleset = vidjil.getSamplesetById(setid_set, "generic")[0]
+    samples   = vidjil.getSampleOfSet(setid_set, config_id)
+    # download result file from all samples if completed
+    for sample in samples["query"]:
+        if sample["results_file"]["status"] == "COMPLETED":
+            # compose a name as you need, here by a combinaison of ids of sets, seuqence file and config id.
+            filename = path_out+"set%s_sequence%s_config%s.vidjil" % (sample["sample_set_membership"]["sample_set_id"], sample["sample_set_membership"]["sequence_file_id"], config_id)  
+            # We also rename the sample in file, under original_names field, to reflet real original name. If don't, the field we be fill with hashed filename as store in the server.
+            vidjil.download(sample["results_file"]["data_file"], filename, sample["sequence_file"]["data_file"], sample["sequence_file"]["filename"])
+
+
+    ###################################################################
+    ### Example 2; creating a set, upload data and launch analysis
     
     # create set to use
     set_data  = vidjil.createSet("set for upload by api")
     setid_set = set_data["args"]["id"]
     ### Create sample in recently created patient, set and run
-    # set_ids filed take value in a specific format: :$set+$name+($id)
+    # set_ids filed take value in a specific format: :$set+$name+($id)
     # With :
     #   $set can be ans 's', 'p' or 'r' respectivly for generic set, patient of run
     #   $name is a part or complete name of set 
@@ -86,24 +104,7 @@ if  __name__ =='__main__':
                 sample_type="set")
     file_id  = sample["file_ids"][0]
 
-    # Launch analysis on the created sample, with a given configuration id
+    # Launch analysis on the created sample, with a given configuration id
     analysis = vidjil.launchAnalisysSample(setid_set, file_id, config_id)
-    # TODO: how to get configuration id ?
+    # TODO: how to get configuration id ?
 
-
-    ###################################################################
-    ### Example 2: download results from a set for a configuration id.
-    ###################################################################
-    path_out = "output_dir/"
-    
-    # Get information from server about set and samples (we reuse here the set id previously created)
-    sampleset = vidjil.getSamplesetById(setid_set, "generic")[0]
-    samples   = vidjil.getSampleOfSet(setid_set, config_id)
-    # download result file from all samples if completed
-    for sample in samples["query"]:
-        if sample["results_file"]["status"] == "COMPLETED":
-            # compose a name as you need, here by a combinaison of ids of sets, seuqence file and config id.
-            filename = path_out+"set%s_sequence%s_config%s.vidjil" % (sampleset["sample_set_id"], sample["sample_set_membership"]["sequence_file_id"], config_id)  
-            # We also rename the sample in file, under original_names field, to reflet real original name. If don't, the field we be fill with hashed filename as store in the server.
-            vidjil.download(sample["results_file"]["data_file"], filename, sample["sequence_file"]["data_file"], sample["sequence_file"]["filename"])
-    
