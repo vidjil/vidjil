cbb7119cad9f77ce15b39d50ad42507d61457123
marc duez <replayskevitch@gmail.com>
2022-06-02 15:12:24 +0200

    model.js : add some function to convert sample_timeID into sample_timeOrder or sample_original_name
    needed for report (will be usefull for template saving)

--- browser/js/model.js
+++ browser/js/model.js
@@ -2217,6 +2217,28 @@ changeAlleleNotation: function(alleleNotation, update, save) {
         return date_max
     },        
     
+    /*return TimeID of a given sample original_name
+    * return -1 if id does not exist
+    */
+    getTimeFromFileName(o_n){
+        for(var i in this.samples.original_names)
+            if (o_n == this.samples.original_names[i])
+                return i;
+
+        return -1
+    },
+
+    /*return TimeID of a given sample order value
+    * return -1 if id does not exist
+    */
+    getTimeFromOrder(o){
+        for(var i in this.samples.original_names)
+            if (o == parseInt(this.getStrTime(parseInt(i), "order")))
+                return parseInt(i);
+
+        return -1
+    },
+
     /**
      * return sample/time name in a specified format
      * @param {integer} timeID - sample/time index
