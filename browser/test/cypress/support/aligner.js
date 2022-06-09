0fc3ed98a75de84144982a04f60d93c2db17d30a
Thonier Florian <florian@vidjil.org>
2022-06-09 10:12:56 +0200

    cypress support; give a update_icon call after click on focus/reset buttons

--- browser/test/cypress/support/aligner.js
+++ browser/test/cypress/support/aligner.js
@@ -1,16 +1,21 @@
 
 
 Cypress.Commands.add('focusOnSelection', () => { 
-  cy.get('#focus_selected').click()
+  cy.get('#focus_selected').click({force: true})
+  cy.update_icon()
 })
 
 Cypress.Commands.add('hideSelection', () => { 
-  cy.get('#hide_selected').click()
+  cy.get('#hide_selected').click({force: true})
+  cy.update_icon()
 })
 
 
 Cypress.Commands.add('resetAllFilter', () => { 
-  cy.get('#reset_focus').click()
+  cy.get('#reset_focus').click({force: true})
+  cy.get('.icon-cancel-circled-outline')
+    .should('not.exist')
+  cy.update_icon()
 })
 
 
