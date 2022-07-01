fff34cbea85c6c4bb4a0f2cc6b8612e335989652
marc duez <replayskevitch@gmail.com>
2022-07-01 11:00:11 +0200

    py4web : convert proxy.py controller

--- server/py4web/apps/vidjil/controllers/proxy.py
+++ server/py4web/apps/vidjil/controllers/proxy.py
@@ -1,14 +1,35 @@
+# coding: utf8
+from sys import modules
+from .. import defs
+from ..modules import vidjil_utils
+from ..modules import tag
+from ..modules.stats_decorator import *
+from ..modules.sampleSet import SampleSet, get_set_group
+from ..modules.sampleSets import SampleSets
+from ..modules.sampleSetList import SampleSetList, filter_by_tags
+from ..modules.sequenceFile import get_associated_sample_sets, get_sequence_file_sample_sets
+from ..modules.controller_utils import error_message
+from ..modules.permission_enum import PermissionEnum
+from ..modules.zmodel_factory import ModelFactory
+from ..user_groups import get_default_creation_group, get_involved_groups
+from ..VidjilAuth import VidjilAuth
+from io import StringIO
+import json
 import requests
-import gluon.contrib.simplejson
+import time
+import os
+from py4web import action, request, abort, redirect, URL, Field, HTTP, response
+from collections import defaultdict
+import math
 
-if request.env.http_origin:
-    response.headers['Access-Control-Allow-Origin'] = request.env.http_origin
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
-    response.headers['Access-Control-Max-Age'] = 86400
+from ..common import db, session, T, flash, cache, authenticated, unauthenticated, auth, log
 
 
-def index():
-    return gluon.contrib.simplejson.dumps("index()")
+##################################
+# HELPERS
+##################################
+
+ACCESS_DENIED = "access denied"
 
 def proxy_request(url, headers={}):
     if request.env.request_method == "POST":
@@ -20,12 +41,22 @@ def proxy_request(url, headers={}):
         response = requests.post(url, headers = headers, data=payload)
         if response.status_code == requests.codes.ok:
             return response
-        return gluon.contrib.simplejson.dumps("the site returned an invalid response")
-    return gluon.contrib.simplejson.dumps("improper method")
+        return json.dumps("the site returned an invalid response")
+    return json.dumps("improper method")
 
+
+##################################
+# CONTROLLERS
+##################################
+@action("/vidjil/proxy/index", method=["POST", "GET"])
+def index():
+    return json.dumps("index()")
+
+@action("/vidjil/proxy/imgt", method=["POST", "GET"])
 def imgt():
     return proxy_request("https://www.imgt.org/IMGT_vquest/analysis")
 
+@action("/vidjil/proxy/asign_subset", method=["POST", "GET"])
 def assign_subsets():
     return proxy_request("http://tools.bat.infspire.org/cgi-bin/arrest/assignsubsets_html.pl",
                          {'referer': "http://tools.bat.infspire.org/arrest/assignsubsets/"})
