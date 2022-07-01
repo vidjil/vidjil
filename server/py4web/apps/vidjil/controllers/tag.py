8d3e2904ea289f9a5085886767ad45adf06725e2
marc duez <replayskevitch@gmail.com>
2022-07-01 10:59:13 +0200

    py4web : convert tag.py controller

--- server/py4web/apps/vidjil/controllers/tag.py
+++ server/py4web/apps/vidjil/controllers/tag.py
@@ -1,17 +1,32 @@
-from controller_utils import error_message
+# coding: utf8
+from sys import modules
+from .. import defs
+from ..modules import vidjil_utils
+from ..modules import tag
+from ..modules.stats_decorator import *
+from ..modules.controller_utils import error_message
 import json
+from py4web import action, request, abort, redirect, URL, Field, HTTP, response
+from collections import defaultdict
 
-if request.env.http_origin:
-    response.headers['Access-Control-Allow-Origin'] = request.env.http_origin  
-    response.headers['Access-Control-Allow-Credentials'] = 'true'
-    response.headers['Access-Control-Max-Age'] = 86400
 
+from ..common import db, session, T, flash, cache, authenticated, unauthenticated, auth, log
+
+
+##################################
+# HELPERS
+##################################
+
+ACCESS_DENIED = "access denied"
+
+@action("/vidjil/tag/auto_complete", method=["POST", "GET"])
+@action.uses(db, auth.user)
 def auto_complete():
-    if "keys" not in request.vars:
+    if "keys" not in request.params:
         return error_message("missing group ids")
 
-    prefix = get_tag_prefix()
-    group_ids = json.loads(request.vars["keys"])
-    tags = get_tags(db, group_ids)
+    prefix = tag.get_tag_prefix()
+    group_ids = json.loads(request.params["keys"])
+    tags = tag.get_tags(db, group_ids)
 
-    return tags_to_json(tags, group_ids)
+    return tag.tags_to_json(tags, group_ids)
