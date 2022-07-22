c8861e504e4b8544d3dc41128ee478a8027084e8
Thonier Florian <florian@vidjil.org>
2022-07-22 10:47:46 +0200

    view/multi-sample-stats.html; Add a line with checkbox to switch each columns of the statsQC table

--- browser/js/tools.js
+++ browser/js/tools.js
@@ -1149,10 +1149,10 @@ var toggleDisplay = function (element) {
 /**
  * Function that allow to call toogleDisplay on each cells of a table for a given column
  *  @param {string}  tableid     - Specific id of the table to use
- *  @param {integer} pos         - Specific pos of the collumn to switch
+ *  @param {integer} pos         - Specific pos of the column to switch
  *  @param {string}  checkbox_id - Specific id of the checkbox to switch (if action is called by a checkbox)
  */
-function toogleCollumn(tableid, pos, checkbox_id) {
+function toogleColumn(tableid, pos, checkbox_id) {
     document.querySelectorAll(`#${tableid} th:nth-child(${pos})`).forEach(toggleDisplay);
     document.querySelectorAll(`#${tableid} td:nth-child(${pos})`).forEach(toggleDisplay);
     if (checkbox_id != undefined){
