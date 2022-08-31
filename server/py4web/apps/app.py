28605edadc5ed3c3154bcd04e17abcd1a0342249
marc duez <replayskevitch@gmail.com>
2022-08-31 13:47:06 +0200

    py4web : uwsgi plugin python3.8

--- server/py4web/apps/app.py
+++ server/py4web/apps/app.py
@@ -8,6 +8,6 @@ if (DOMAIN_NAME is None):
     DOMAIN_NAME = "127.0.0.1"
 
 password_file = os.path.abspath(os.path.join(os.path.dirname(__file__),"password.txt"))
-application = wsgi(password_file = password_file, dashboard_mode = "full")
+application = wsgi(password_file = password_file, dashboard_mode = "full", apps_folder = "/usr/share/vidjil/server/py4web/apps")
 
-application.run(host = DOMAIN_NAME, port = 80 ) 
+application.run(host = DOMAIN_NAME, port = 80) 
