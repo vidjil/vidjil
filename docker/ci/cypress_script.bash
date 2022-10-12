fc3293fc377b5e8521c42c2f92110051726b42a3
Thonier Florian <florian@vidjil.org>
2022-10-12 18:12:34 +0200

    js/addons; make some test by cypress Build some function to load addons files for test

--- docker/ci/cypress_script.bash
+++ docker/ci/cypress_script.bash
@@ -10,6 +10,22 @@ ls /app/cypress/fixtures/tools/tests/data
 
 echo "==> PWD: `pwd`"
 
+echo "==> ls /app/vidjil/browser/test/data/addons: `ls /app/vidjil/browser/test/data/addons`"
+
+
+# Move addons to the correct path for test
+mv /app/vidjil/browser/test/data/addons/* /app/vidjil/browser/js/addons/
+mv /app/vidjil/browser/test/data/confs/conf_no_db.js /app/vidjil/browser/js/conf.js
+files=`printf "'%s'," /app/vidjil/browser/js/addons/*`
+echo -e "Copy addons file: $files"
+sed -i "s|\"addons\" : \[|\"addons\" : [$files|" "/app/vidjil/browser/js/conf.js"
+sed -i "s|,]|]|g" "/app/vidjil/browser/js/conf.js"
+sed -i "s|/app/vidjil/browser/||g" "/app/vidjil/browser/js/conf.js"
+
+
+echo "===== conf.js content ===\n"
+cat /app/vidjil/browser/js/conf.js
+echo "=====\n"
 
 echo -e "$ ./node_modules/cypress/bin/cypress run --browser $BROWSER --headless --spec $@ --env workdir=vidjil,host=$HOST,initiated_database=false"
 ./node_modules/cypress/bin/cypress run --browser $BROWSER --headless --spec $@ --env workdir=vidjil,host=$HOST,initiated_database=false
