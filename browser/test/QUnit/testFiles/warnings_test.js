d2b7104cffa230b0614b7664f9e66dcda0d0ddeb
Thonier Florian <florian@vidjil.org>
2022-11-30 12:43:54 +0100

    Qunit, minimal test for warnings view

--- browser/test/QUnit/testFiles/warnings_test.js
+++ browser/test/QUnit/testFiles/warnings_test.js
@@ -5,7 +5,20 @@ QUnit.module("Warnings", {
 QUnit.test("default menu", function(assert) {
     var m = new Model();
 
-    builder.build_warnings()
+    m.parseJsonData(json_data,100)
+    m.loadGermline()
+    m.initClones()
+    m.clones[0].warn  = [{"code": "W69", "msg": "Several genes with equal probability", "level": "warn"}]
+    m.clones[5].warn  = [{"code": "W82", "msg": "Merged clone has different productivities in some samples", "level": "warn"}]
 
-    
+
+    var warnings = new Warnings("warnings_list", m, false);
+    warnings.build_warnings()
+
+    var warnings_list  = document.getElementById("warnings_list")
+    var w69_sample     = document.getElementById("warn_span_W69_sample")
+    var w69_all_sample = document.getElementById("warn_span_W69_all_samples")
+
+    assert.includes(w69_sample.innerHTML, "1 (10)", "warnings W69; correct value")
+    assert.includes(w69_all_sample.innerHTML, "1 (50)", "warnings W69; correct value")
 });
