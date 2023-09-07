96ee1db6205fa700029a51abf2e1ec5ec46c7071
Thonier Florian <florian.thonier@inria.fr>
2023-09-07 09:42:28 +0200

    docker entrypoints; minor modifications

--- docker/vidjil-server/scripts/uwsgi-entrypoint.sh
+++ docker/vidjil-server/scripts/uwsgi-entrypoint.sh
@@ -8,14 +8,14 @@ echo -e "\e[34m=== `date +'%Y/%m/%d; %H:%M'`\e[0m"; echo
 user=$(get_user_of_results)
 
 echo "==== Setup apps of py4web ==="
-cd /usr/share/vidjil/server/py4web/apps
+cd /usr/share/vidjil/server/py4web/
 py4web setup apps --yes # Yes to all, so useless apps are "installed"
 # List of installed apps : _default, _minimal, _dahsboard, _documentation, _scaffold, showcase
-rm -r  _default  _minimal _scaffold showcase # clean useless default apps
+rm -r  apps/_default  apps/_minimal apps/_scaffold apps/showcase # clean useless default apps
 
 
 echo "==== Setup password"
-cd /usr/share/vidjil/server/py4web
+cd /usr/share/vidjil/server/py4web/apps/
 /usr/local/bin/gosu $user py4web set_password --password "$PY4WEB_ADMIN_PASSWORD"
 ls /usr/share/vidjil/server/py4web/apps/
 
