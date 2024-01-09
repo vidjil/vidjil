5b3f7fe3b283a1915857954912da63293504e9e2
Clement CHESNIN <clement.chesnin@inria.fr>
2024-01-09 16:05:28 +0100

    Fix log message and docker-compose version

--- docker/vidjil-server/scripts/uwsgi-entrypoint.sh
+++ docker/vidjil-server/scripts/uwsgi-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo -e "\n\e[34m=======================\e[0m"
-echo -e "\e[34m=== Prepare uwsgi\e[0m"
+echo -e "\e[34m=== Start service uwsgi\e[0m"
 echo -e "\e[34m=== `date +'%Y/%m/%d %H:%M:%S'`\e[0m"; echo
 
 mkdir -p /mnt/backup/ /mnt/data/ /mnt/result/ /mnt/upload/
@@ -13,13 +13,12 @@ cd /usr/share/vidjil/server/py4web/apps/
 gosu $user py4web set_password --password "$PY4WEB_ADMIN_PASSWORD"
 ls /usr/share/vidjil/server/py4web/apps/
 
-
 if [[ -v UWSGI_POOL ]]; then
     echo "==== Change number of threads: '$UWSGI_POOL'"
     sed -i "s/processes = 6/processes = $UWSGI_POOL/g" /etc/uwsgi/sites/uwsgi.ini
 fi
 
 echo -e "\n\e[34m=======================\e[0m"
-echo -e "\e[34m=== Start uwsgi script\e[0m"
+echo -e "\e[34m=== Start uwsgi\e[0m"
 echo -e "\e[34m=== `date +'%Y/%m/%d %H:%M:%S`\e[0m"; echo
 uwsgi /etc/uwsgi/sites/uwsgi.ini
