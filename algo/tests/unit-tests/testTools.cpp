45bbdd88e67801cbbafd11ddb4a07e246cf76c6d
Mikaël Salson <mikael.salson@univ-lille.fr>
2024-07-17 22:06:54 +0200

    tests: approximation is worse for small files
    This is due to gzip that reads the file by chunks (see previous commit)

--- algo/tests/unit-tests/testTools.cpp
+++ algo/tests/unit-tests/testTools.cpp
@@ -57,7 +57,7 @@ void testFastaNbSequences() {
   TAP_TEST(a1 >= 350 && a1 <= 370, TEST_FASTA_NB_SEQUENCES, "");
 
   int a2 = nb_sequences_in_file("data/Stanford_S22.fasta", true);
-  TAP_TEST(a2 >= 13100 && a2 <= 13200, TEST_FASTA_NB_SEQUENCES, "");
+  TAP_TEST(a2 >= 12800 && a2 <= 13200, TEST_FASTA_NB_SEQUENCES, "");
 }
 
 
