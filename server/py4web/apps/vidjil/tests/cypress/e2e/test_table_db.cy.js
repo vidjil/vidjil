4e0d5d5d892f78e413ffed21d668f9a2bf4e2db3
THONIER Florian <florian.thonier@inria.fr>
2024-07-24 08:52:03 +0200

    cypress typo, need to get element again before checking it

--- server/py4web/apps/vidjil/tests/cypress/e2e/test_table_db.cy.js
+++ server/py4web/apps/vidjil/tests/cypress/e2e/test_table_db.cy.js
@@ -215,7 +215,8 @@ describe('Manipulate patient, sample and launch analysis', function () {
 
         cy.get('#db_filter_input')
           .type("{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{backspace}{rightArrow}{enter}") // 11 calls to del + 1 space
-          .wait(['@getActivities'])
+        cy.wait(['@getActivities'])
+        cy.get('#db_filter_input')
           .click({force: true})
 
         cy.wait(['@getActivities'])
