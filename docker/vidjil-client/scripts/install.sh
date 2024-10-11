ec5857c43d17d1eeeb377b1dcc618c32b4205d7e
Clement CHESNIN <clement.chesnin@inria.fr>
2024-10-11 12:05:44 +0200

    Update client build process too

--- docker/vidjil-client/scripts/install.sh
+++ docker/vidjil-client/scripts/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 echo "${BUILD_ENV}"
 if [ "${BUILD_ENV}" = "TEST"  ]; then
-    ln -s /etc/vidjil/conf_http.js /usr/share/vidjil/browser/js/conf.js
-    ln -s /etc/vidjil/nginx_web2py_http /etc/nginx/conf.d/web2py.conf
+    ln -sf /etc/vidjil/conf_http.js /usr/share/vidjil/browser/js/conf.js
+    ln -sf /etc/vidjil/nginx_web2py_http /etc/nginx/conf.d/web2py.conf
 else
-    ln -s /etc/vidjil/conf.js /usr/share/vidjil/browser/js/conf.js
-    ln -s /etc/vidjil/nginx_web2py /etc/nginx/conf.d/web2py.conf
+    ln -sf /etc/vidjil/conf.js /usr/share/vidjil/browser/js/conf.js
+    ln -sf /etc/vidjil/nginx_web2py /etc/nginx/conf.d/web2py.conf
 fi;
 
