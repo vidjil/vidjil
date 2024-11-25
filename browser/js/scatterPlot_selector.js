40fbffdd6ee88523dfe6d1efe7709d4a11d9bfc9
THONIER Florian <florian.thonier@inria.fr>
2024-11-25 18:18:55 +0100

    typo

--- browser/js/scatterPlot_selector.js
+++ browser/js/scatterPlot_selector.js
@@ -136,7 +136,7 @@ ScatterPlot_selector.prototype = {
 
     getTooltip: function(d){
         var clone = this.m.clone(d.id)
-        return `${this.axisX["name"]}: ${this.axisX["fct"](clone)}<br>${this.axisY["name"]}: ${this.axisY["fct"](clone)}<br> `
+        return `${this.axisX["name"]}: ${this.axisX["fct"](clone)}<br>${this.axisY["name"]}: ${this.axisY["fct"](clone)}`
     },
 
     dragstarted: function(d)
