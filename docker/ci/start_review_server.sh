7591b4c7a31a8c63f0265314523acb65fdd6c447
Clement CHESNIN <clement.chesnin@inria.fr>
2024-12-13 15:35:59 +0100

    Fix review server startup

--- docker/ci/start_review_server.sh
+++ docker/ci/start_review_server.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+echo "Compose up"
 docker compose version
 ln -sf docker-compose-ci-review.yml docker-compose.override.yml
 docker compose config
 docker compose up -d
 echo "TIME - after start - $(date)"
-docker compose exec -w "/usr/share/vidjil/server/py4web/apps/vidjil/scripts/" uwsgi bash -c "source /usr/share/vidjil/venv/bin/activate && python3 load-sql.py -i /usr/share/vidjil/server/py4web/apps/vidjil/tests/functional/ci_erase.sql"
+
+echo "Load test data"
 docker compose exec -w "/usr/share/vidjil/server/py4web/apps/vidjil/tests/functional/" uwsgi bash -c "source /usr/share/vidjil/venv/bin/activate && python3 init_test_db.py"
