8c260f2e88cbc2fd07b6868e0096845ec99c8a93
Thonier Florian <florian.thonier@inria.fr>
2025-05-27 12:54:06 +0200

    browser/info.js: refactor event function in a dedicated function Update tests with a qunit test

--- browser/js/info.js
+++ browser/js/info.js
@@ -336,7 +336,23 @@ Info.prototype = {
         return container;
     },
 
+    /**
+     * Allow to update title attribute of a locus span in info panel to show size of locus (in system or globally)
+     */
+    makeMouseEnterHandlerOnLocusSpan: function(span, key, self) {
+        return function(event) {
+            var time = self.m.getTime();
+            let size_in_all = (self.m.systemSize(key)*100).toFixed(2);
+            let size_in_system = self.m.reads.germline[key][time] ?
+                 ((self.m.reads.germline[key][time]*100) / self.m.systemGroupSize(key)).toFixed(2) : (0).toFixed(2);
+            span.title = `Locus ${key}; ${size_in_system}% of system, ${size_in_all}% of total segmented reads`;
+        };
+    },
+
     build_multi_system: function () {
+        let self = this;
+
+
         var div = document.createElement('div');
         div.className = "info_line locus_line";
 
@@ -396,13 +412,9 @@ Info.prototype = {
             let span = document.createElement('span');
             span.className = "systemBoxNameMenu "+key;
             span.id = "toogleLocusSystemBox_"+key;
-            span.addEventListener('mouseenter', (event) => {
-                // On hover, update title content with sizes of current time
-                var time = self.m.getTime()
-                let size_in_all = (self.m.systemSize(key)*100).toFixed(2)
-                let size_in_system = self.m.reads.germline[key][time] ? (((self.m.reads.germline[key][time]*100) / self.m.systemGroupSize(key)).toFixed(2)) : (0).toFixed(2);
-                span.title = `Locus ${key}; ${size_in_system}% of system, ${size_in_all}% of total segmented reads`
-            });
+
+            span.addEventListener('mouseenter', self.makeMouseEnterHandlerOnLocusSpan(span, key, self));
+
 
             if (this.m.system_selected.indexOf(key) == -1)
                 span.className = "systemBoxNameMenu unchecked " + key;
