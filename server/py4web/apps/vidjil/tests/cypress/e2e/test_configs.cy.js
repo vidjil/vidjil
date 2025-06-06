404229454f2f8cc6a81af99474183a37d101af27
Thonier Florian <florian.thonier@inria.fr>
2025-06-06 11:19:21 +0200

    cypress; test copy/paste of configuration from clipboard

--- server/py4web/apps/vidjil/tests/cypress/e2e/test_configs.cy.js
+++ server/py4web/apps/vidjil/tests/cypress/e2e/test_configs.cy.js
@@ -58,4 +58,51 @@ describe("Manipulate configs", function () {
       cy.deletePreprocess(preprocess_id, pre_process_name_2);
     });
   });
+
+  it("03-clipboard-config", function () {
+    cy.goToConfigsPage();
+
+    if ((Cypress.browser.name === "chromium") && (parseInt(Cypress.browser.version.split(".")[0]) >= 81)) {
+      cy.wrap(Cypress.automation('remote:debugger:protocol', {
+        command: 'Browser.grantPermissions',
+        params: {
+          permissions: ['clipboardReadWrite', 'clipboardSanitizedWrite'],
+          origin: window.location.origin,
+        },
+      }))
+      cy.get('#copyPathClipboard_2 > .icon-newspaper')
+        .click()
+        .then(() => {
+          // check that the path is copied to clipboard
+          cy.window().then((win) => {
+            win.navigator.clipboard.readText().then((text) => {
+              expect(text).to.contain("'program': 'vidjil',");
+              expect(text).to.contain("'classification': '1',");
+              expect(text).to.contain("'name': 'multi+inc+xxx',");
+              expect(text).to.contain("'command': '-c clones -z 100 -r 1 -g germline/homo-sapiens.g -e 1 -2 -d -w 50 ',");
+              expect(text).to.contain("'fuse_command': '-t 100',");
+              expect(text).to.contain("'info': 'multi-locus, with some incomplete/unusual/unexpected recombinations'");
+            });
+          });
+
+          // check flash message is displayed
+          cy.get(".flash_1").should("be.visible").contains("Copied");
+        });
+
+        cy.get('#new_config_btn')
+          .click()
+
+        cy.get('#db_content > button')
+          .click()
+
+        cy.get('#config_name').should('have.value',"multi+inc+xxx");
+        cy.get('#config_classification').should('have.value',"1");
+        cy.get('#config_program').should('have.value',"vidjil");
+        cy.get('#config_command').should('have.value',"-c clones -z 100 -r 1 -g germline/homo-sapiens.g -e 1 -2 -d -w 50 ");
+        cy.get('#config_fuse_command').should('have.value',"-t 100");
+        cy.get('#config_info').should('have.value',"multi-locus, with some incomplete/unusual/unexpected recombinations");
+
+    }
+
+  });
 });
