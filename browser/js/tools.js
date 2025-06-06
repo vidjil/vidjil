59cfc710b70791e1289de134518ffb673a04b134
Thonier Florian <florian.thonier@inria.fr>
2025-06-06 11:17:17 +0200

    tools.js; add a function to paste clipboard content to configuration form

--- browser/js/tools.js
+++ browser/js/tools.js
@@ -950,6 +950,32 @@ function copyHard(obj){
     return JSON.parse(JSON.stringify(obj))
 }
 
+
+/**
+ * Allow to read content from clipboard to put it in configuration form 
+ */
+async function fillConfigFormFromClipboard() {
+  try {
+    const content = await navigator.clipboard.readText();
+
+    // Par exemple, on suppose que les champs sont séparés par des tabulations ou des sauts de ligne
+    const values = JSON.parse(content.replace(/'/g, '"'));
+    console.default.log( values );
+
+    // On remplit les champs un à un (ajustez selon vos besoins)
+    document.getElementById("config_name").value = values.name;
+    document.getElementById("config_classification").value = values.classification;
+    document.getElementById("config_program").value = values.program;
+    document.getElementById("config_command").value = values.command;
+    document.getElementById("config_fuse_command").value = values.fuse_command;
+    document.getElementById("config_info").value = values.info;
+
+  } catch (err) {
+    console.error("Error in clipboard reading.", err);
+  }
+}
+
+
 /**
  * Function that allow to make comparison between two arrays.
  */
