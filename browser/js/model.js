2a00d6f68f317c24077034e135d773278bc9586b
Thonier Florian <florian.thonier@inria.fr>
2025-11-02 19:33:32 +0100

    Model loader: change behavior in case of analysis loading from both vidjil file and analysis file. In this case, we keep only last analysis content loaded, so from analysis file.
    cypress, add test for clusters loading from analysis vidjil file + overwrite by analysis file

--- browser/js/model.js
+++ browser/js/model.js
@@ -584,6 +584,9 @@ changeAlleleNotation: function(alleleNotation, update, save) {
      * @param {integer[]} clusters - an array of list of clone_id
      * */
     loadCluster: function (clusters) {
+        
+        // reset cluster before applying new one
+        this.resetClusters(update=false);
 
     this.analysis_clusters = [] // will store unused clusters analysis
     
@@ -2279,8 +2282,9 @@ changeAlleleNotation: function(alleleNotation, update, save) {
 
     /**
      * break all clusters to default 1-clone clusters
+     * @param {boolean} update Apply update of model after reset. In case of analysis overwrite, model is not fully loaded and cannot update
      * */
-    resetClusters: function () {
+    resetClusters: function (update=true) {
         this.saveClusters()
 
         for (var i = 0; i < this.clones.length; i++) {
@@ -2288,7 +2292,9 @@ changeAlleleNotation: function(alleleNotation, update, save) {
             this.clone(i).mergedId = undefined
         }
 
-        this.update()
+        if (update){
+            this.update()
+        }
     },
 
 
