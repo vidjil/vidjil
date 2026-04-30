ffcd859791c4e751301717d58dae4dbf8f748423
Sami AMARA <sami.amara@inria.fr>
2026-04-30 14:04:59 +0200

    Added a macro and some comments to explicitely describe what V/J genes positions are

--- algo/core/locations_to_mark.h
+++ algo/core/locations_to_mark.h
@@ -2,11 +2,15 @@
 
 #define INVALID_POS ~0
 
+#define AMINO_ACID_1_BASED_POS_TO_FIRST_NUCLEOTIDE_0_BASED_POS(aa_pos) (3 * (aa_pos - 1))
+
 enum
 {
   // JUNCTION/CDR3 extraction from gapped V/J sequences
-  CYS104_IN_GAPPED_V        = 310 - 1, // First nucleotide of Cys104
-  PHE118_TRP118_IN_GAPPED_J = 38 - 1   // Last nucleotide of Phe118/Trp118
+  CYS104_IN_GAPPED_V = AMINO_ACID_1_BASED_POS_TO_FIRST_NUCLEOTIDE_0_BASED_POS(104),
+
+  // Special case: based on (W|P)G_G amino acid pattern position on J genes
+  PHE118_TRP118_IN_GAPPED_J = 38 - 1
 };
 
 enum
