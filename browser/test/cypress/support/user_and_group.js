9671665c06244ab6b679f251ad6f20d1598655de
Thonier Florian <florian.thonier@inria.fr>
2026-05-13 00:17:00 +0200

    cypress: fix a new position of user columns

--- browser/test/cypress/support/user_and_group.js
+++ browser/test/cypress/support/user_and_group.js
@@ -39,7 +39,7 @@ Cypress.Commands.add("goToCorrespondingUserGroup", (user_id) => {
   cy.get("#table_users")
     .contains("tr td:nth-child(1)", user_id)
     .parent()
-    .contains("td:nth-child(5) > a", "user_")
+    .contains("td:nth-child(6) > a", "user_")
     .dblclick()
   cy.wait("@getActivities")
 
