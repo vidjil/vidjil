d6cd682e39444f2f89dd19cdaef017d2d647c546
Mikaël Salson <mikael.salson@univ-lille.fr>
2026-06-11 19:55:31 +0200

    algo: Allow to customize the min coverage to compute the consensus.
    Fix #5419

--- algo/vidjil.cpp
+++ algo/vidjil.cpp
@@ -617,6 +617,9 @@ int main(int argc, char **argv) {
            "consensus sequence (instead of a random sample)")
         ->group(group)
         ->level();
+    app.add_option("--min-coverage-consensus", ratio_representative, "minimal percent of the maximal coverage that is admissible to build the consensus sequence")
+        ->group(group)
+        ->capture_default_str();    
 
     // ----------------------------------------------------------------------------------------------------------------------
     group = "Clone analysis (second pass), V/D/J designation, CDR3/JUNCTION analysis with "
