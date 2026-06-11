e112a0db6a68afdb3b043dfa5d3014f94b013001
Mikaël Salson <mikael.salson@univ-lille.fr>
2026-06-11 20:11:00 +0200

    algo: make option only available in the advanced help

--- algo/vidjil.cpp
+++ algo/vidjil.cpp
@@ -619,7 +619,8 @@ int main(int argc, char **argv) {
         ->level();
     app.add_option("--min-coverage-consensus", ratio_representative, "minimal percent of the maximal coverage that is admissible to build the consensus sequence")
         ->group(group)
-        ->capture_default_str();    
+        ->capture_default_str()
+        ->level();
 
     // ----------------------------------------------------------------------------------------------------------------------
     group = "Clone analysis (second pass), V/D/J designation, CDR3/JUNCTION analysis with "
